d7afe5a32d0dee32e7e24351d32c037a881d164d
aldrin <aldrin@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-06-07 10:55:20 +0000

    fix the logic for adding experiment
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBPWebService@3849 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/ibpworkbench/service/BreedingViewServiceImpl.java
+++ src/main/java/org/generationcp/ibpworkbench/service/BreedingViewServiceImpl.java
@@ -219,21 +219,26 @@ public class BreedingViewServiceImpl implements BreedingViewService {
             	}
             }
             
-            ExperimentValues experimentValues = null;
+   
             ArrayList<String> environments = traitsAndMeans.get(csvHeader[0]);
             for(int i = 0; i < environments.size(); i++) {
-                for(int j = 2; j < csvHeader.length; j++) {   //means and errors are in pair, so just get the word before _
-                    if(csvHeader[j] != null) {
-                        Stock stock = stocks.findOnlyOneByLocalName(csvHeader[1], traitsAndMeans.get(csvHeader[1]).get(i));    //???
-                        if (stock != null) {
-	                        stockId = stock.getId();      //germPlasmId
-	                        experimentValues = createExperimentValues(meansDataSet, csvHeader[j],
-	                                traitsAndMeans.get(csvHeader[j]).get(i), ndLocationId, stockId);
-	                        studyDataManagerV2.addExperiment(meansDataSet.getId(), ExperimentType.AVERAGE, experimentValues);
-	                        
-                        }
-                    }
-                }
+               
+            	Stock stock = stocks.findOnlyOneByLocalName(csvHeader[1], traitsAndMeans.get(csvHeader[1]).get(i));
+            	ExperimentValues experimentRow = new ExperimentValues();
+            	experimentRow.setGermplasmId(stock.getId());
+            	experimentRow.setLocationId(ndLocationId);
+            		
+	            	List<Variable> list = new ArrayList<Variable>();
+	                for(int j = 2; j < csvHeader.length; j++) {
+	                	Variable var = new Variable( meansDataSet.getVariableTypes().findByLocalName(csvHeader[j]), traitsAndMeans.get(csvHeader[j]).get(i));
+	                	list.add(var);
+	                }
+	            VariableList variableList1 = new VariableList();
+	            variableList1.setVariables(list);
+                experimentRow.setVariableList(variableList1);
+                
+                
+                studyDataManagerV2.addExperiment(meansDataSet.getId(), ExperimentType.AVERAGE, experimentRow);
             }
 
         } else {
