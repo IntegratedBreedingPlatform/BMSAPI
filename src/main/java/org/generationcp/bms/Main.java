668b01ac19810136aa1a6f9b3e22c5267aa0300b
Naymesh Mistry <naymesh@leafnode.io>
2014-07-03 14:42:19 +1200

    Added basic spring-boot web infrastructure with Thymeleaf, Bootstrap and jQuery.

--- src/main/java/org/generationcp/bms/Main.java
+++ src/main/java/org/generationcp/bms/Main.java
@@ -1,17 +1,41 @@
 package org.generationcp.bms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class Main {
-	  
-	public static void main(String[] args) throws Exception {
+public class Main extends WebMvcConfigurerAdapter {
 	
-		SpringApplication.run(Main.class, args);
-	    
-		System.out.println("Startup Complete!");
+	private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	  
+	public static void main(String[] args) throws Exception {	
+		SpringApplication.run(Main.class, args);	    
+		LOGGER.info("Startup Complete!");
 	}
+	
+    @Bean
+    public TemplateResolver templateResolver(){
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/html/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+        return templateResolver;
+    }
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+				.addResourceLocations("/WEB-INF/static/");
+	}
+
 }
---
+++ src/main/java/org/generationcp/bms/web/controller/WebContentController.java
@@ -0,0 +1,18 @@
+package org.generationcp.bms.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/web")
+public class WebContentController {
+
+    @RequestMapping("/")
+    public String home(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}
