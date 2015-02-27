3a6c28b2e930bc2c449b783d24adf8a1169cf499
srajwadi <sunny@leafnode.io>
2015-02-27 11:03:37 +0530

    BMS-356 List all data types API Service
    Adding Test method to get all data types

---
+++ src/test/java/org/generationcp/bms/ontology/OntologyDataTypeResourceTest.java
@@ -0,0 +1,78 @@
+package org.generationcp.bms.ontology;
+
+import org.generationcp.bms.ApiUnitTestBase;
+import org.generationcp.middleware.domain.oms.Term;
+import org.generationcp.middleware.service.api.OntologyService;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.is;
+import static com.jayway.jsonassert.impl.matcher.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+public class OntologyDataTypeResourceTest extends ApiUnitTestBase {
+
+    @Configuration
+    public static class TestConfiguration {
+
+        @Bean
+        @Primary
+        public OntologyService ontologyService() {
+            return Mockito.mock(OntologyService.class);
+        }
+    }
+
+    @Autowired
+    private OntologyService ontologyService;
+
+    @Before
+    public void reset(){
+        Mockito.reset(ontologyService);
+    }
+
+    @After
+    public void validate() {
+        validateMockitoUsage();
+    }
+
+    @Test
+    public void listAllDataTypes() throws Exception {
+
+        String cropName = "maize";
+
+        List<Term> termList = new ArrayList<>();
+        Term term = new Term(1, "Categorical", "");
+        termList.add(term);
+        term = new Term(2, "Numeric", "");
+        termList.add(term);
+        term = new Term(3, "Character", "");
+        termList.add(term);
+        term = new Term(4, "Date", "");
+        termList.add(term);
+
+        Mockito.doReturn(termList).when(ontologyService).getAllDataTypes();
+
+        mockMvc.perform(get("/ontology/{cropname}/datatype/list", cropName).contentType(contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(termList.size())))
+                .andExpect(jsonPath("$[0].id", is(termList.get(0).getId())))
+                .andExpect(jsonPath("$[0].name", is(termList.get(0).getName())))
+                .andDo(print());
+
+        verify(ontologyService, times(1)).getAllDataTypes();
+    }
+}
