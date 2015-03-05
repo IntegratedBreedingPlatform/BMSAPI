5ae054eb062ae0fe3f32718c80269f5089608370
keyurbhatt <keyur@leafnode.io>
2015-03-05 10:36:33 +0530

    Removed unused method from ContextResolver

--- src/main/java/org/generationcp/bms/context/ContextResolverImpl.java
+++ src/main/java/org/generationcp/bms/context/ContextResolverImpl.java
@@ -1,54 +1,24 @@
 package org.generationcp.bms.context;
 
-import org.generationcp.bms.Constants;
-import org.generationcp.middleware.exceptions.MiddlewareQueryException;
-import org.generationcp.middleware.manager.api.WorkbenchDataManager;
-import org.generationcp.middleware.pojos.workbench.Project;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
+@SuppressWarnings("unused")
 @Component
 public class ContextResolverImpl implements ContextResolver {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(ContextResolverImpl.class);
 
-	@Autowired
-	private WorkbenchDataManager workbenchDataManager;
-
-	@Override
-	public Project resolveProgram() throws ContextResolutionException {
-
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-		HttpSession session = request.getSession();
-		Project selectedProgram = (Project) session.getAttribute(Constants.PARAM_SELECTED_PROGRAM);
-
-		if (selectedProgram != null) {
-			LOGGER.debug("Resolved program from session: " + selectedProgram);
-			return selectedProgram;
-		}
-
-		try {
-			Project lastOpenedProgram = this.workbenchDataManager.getLastOpenedProjectAnyUser();
-			if (lastOpenedProgram != null) {
-				LOGGER.debug("Resolved program from workbench access history: " + lastOpenedProgram);
-				return lastOpenedProgram;
-			}
-		} catch (MiddlewareQueryException e) {
-			throw new ContextResolutionException("Data access error while trying to resolve program from workbench access history.", e);
-		}
-		throw new ContextResolutionException("Unable to resolve program in context.");
-	}
-
     @Override
     public String resolveDatabaseFromUrl() throws ContextResolutionException {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        LOGGER.debug("Request: " + request.getRequestURI());
+
         String requestURL = request.getRequestURI();
         int a = requestURL.indexOf("/");
         int b = requestURL.indexOf("/", a + 1);
