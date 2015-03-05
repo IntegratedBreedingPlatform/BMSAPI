8b52aeb62cb16bbc58e83d0a79e49a658966c264
Naymesh <naymesh@leafnode.io>
2015-03-05 16:26:39 +1300

    Moving Workbench data manager singleton creation into MiddlewareFactory. Removing the duplicated ibpworkbench_hib.cfg.xml.

--- src/main/java/org/generationcp/bms/dao/MiddlewareFactory.java
+++ src/main/java/org/generationcp/bms/dao/MiddlewareFactory.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.annotation.PreDestroy;
 
 import org.generationcp.bms.context.ContextResolver;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.hibernate.HibernateSessionPerRequestProvider;
 import org.generationcp.middleware.hibernate.SessionFactoryUtil;
 import org.generationcp.middleware.manager.DatabaseConnectionParameters;
@@ -19,6 +20,7 @@ import org.generationcp.middleware.manager.LocationDataManagerImpl;
 import org.generationcp.middleware.manager.OntologyDataManagerImpl;
 import org.generationcp.middleware.manager.StudyDataManagerImpl;
 import org.generationcp.middleware.manager.UserDataManagerImpl;
+import org.generationcp.middleware.manager.WorkbenchDataManagerImpl;
 import org.generationcp.middleware.manager.api.GenotypicDataManager;
 import org.generationcp.middleware.manager.api.GermplasmDataManager;
 import org.generationcp.middleware.manager.api.GermplasmListManager;
@@ -27,6 +29,7 @@ import org.generationcp.middleware.manager.api.LocationDataManager;
 import org.generationcp.middleware.manager.api.OntologyDataManager;
 import org.generationcp.middleware.manager.api.StudyDataManager;
 import org.generationcp.middleware.manager.api.UserDataManager;
+import org.generationcp.middleware.manager.api.WorkbenchDataManager;
 import org.generationcp.middleware.service.DataImportServiceImpl;
 import org.generationcp.middleware.service.FieldbookServiceImpl;
 import org.generationcp.middleware.service.OntologyServiceImpl;
@@ -158,5 +161,16 @@ public class MiddlewareFactory {
 				config.getDbUsername(), config.getDbPassword());
 		
 		return new JdbcTemplate(dataSource);
+	}
+	
+	@Bean
+	@Scope(value = "singleton")
+	public WorkbenchDataManager getWorkbenchDataManager() throws FileNotFoundException, MiddlewareQueryException {
+		DatabaseConnectionParameters workbenchConnectionParameters = new DatabaseConnectionParameters(
+				config.getDbHost(), config.getDbPort(), config.getWorkbenchDBName(), config.getDbUsername(), config.getDbPassword());		
+		SessionFactory sessionFactory = SessionFactoryUtil.openSessionFactory(workbenchConnectionParameters);
+		HibernateSessionPerRequestProvider sessionProvider = new HibernateSessionPerRequestProvider(sessionFactory);
+		WorkbenchDataManagerImpl workbenchDataManager = new WorkbenchDataManagerImpl(sessionProvider);
+		return workbenchDataManager;
 	}
 }
