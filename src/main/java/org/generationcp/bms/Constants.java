6fe557b8e24b71e7ecaf48a48c1dd1e6ab4659ca
keyurbhatt <keyur@leafnode.io>
2015-03-06 15:27:56 +0530

    Update Context Resolver  Issue: BMS-317

--- src/main/java/org/generationcp/bms/Constants.java
+++ src/main/java/org/generationcp/bms/Constants.java
@@ -4,5 +4,6 @@ public final class Constants {
 	
 	public static final String PARAM_SELECTED_PROGRAM = "selectedProgram";
 	public static final String PARAM_SELECTED_CENTRAL_CROP_DB = "selectedCentralCropDB";
+    public static final String DB_NAME_FORMAT = "ibdbv2_%s_merged";
 
 }
--- src/main/java/org/generationcp/bms/context/ContextResolverImpl.java
+++ src/main/java/org/generationcp/bms/context/ContextResolverImpl.java
@@ -1,28 +1,35 @@
 package org.generationcp.bms.context;
 
+import org.generationcp.bms.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Component;
+
 @SuppressWarnings("unused")
 @Component
 public class ContextResolverImpl implements ContextResolver {
 	
-	private static final Logger LOGGER = LoggerFactory.getLogger(ContextResolverImpl.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ContextResolverImpl.class);
 
     @Override
     public String resolveDatabaseFromUrl() throws ContextResolutionException {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        LOGGER.debug("Request: " + request.getRequestURI());
 
-        String requestURL = request.getRequestURI();
-        int a = requestURL.indexOf("/");
-        int b = requestURL.indexOf("/", a + 1);
-        String dbName = requestURL.substring(b + 1, requestURL.indexOf("/", b + 1));
-        return String.format("ibdbv2_%s_merged", dbName.trim().toLowerCase().replaceAll("\\s+", "_"));
+        if(request == null) throw new ContextResolutionException("Request is null");
+
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        LOG.debug("Request path: " + path);
+        String[] parts = path.trim().toLowerCase().split("/");
+        if(parts.length < 3){
+            LOG.error("BAD URL Request :" + path);
+            throw new ContextResolutionException("BAD URL:" + path, new Exception("Expecting crop name"));
+        }
+        LOG.debug("Crop Name: " + parts[2]);
+        return String.format(Constants.DB_NAME_FORMAT, parts[2]);
     }
 }
