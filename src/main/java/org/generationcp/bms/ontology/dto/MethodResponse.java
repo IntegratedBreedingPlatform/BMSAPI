e1a268d3f936229c6c5c23b8befcd7e6d73458fa
keyurbhatt <keyur@leafnode.io>
2015-03-16 12:50:48 +0530

    FIX : added generic methods inside interface and implement it for method and property response

--- src/main/java/org/generationcp/bms/ontology/dto/MethodResponse.java
+++ src/main/java/org/generationcp/bms/ontology/dto/MethodResponse.java
@@ -2,24 +2,28 @@ package org.generationcp.bms.ontology.dto;
 
 import java.util.List;
 
-public class MethodResponse extends MethodSummary {
+public class MethodResponse extends MethodSummary implements EditableDeletableFields {
 
     private List<String> editableFields;
-    
+
     private Boolean deletable;
 
+    @Override
     public List<String> getEditableFields() {
         return editableFields;
     }
 
+    @Override
     public void setEditableFields(List<String> editableFields) {
         this.editableFields = editableFields;
     }
 
+    @Override
     public Boolean getDeletable() {
         return deletable;
     }
 
+    @Override
     public void setDeletable(Boolean deletable) {
         this.deletable = deletable;
     }
