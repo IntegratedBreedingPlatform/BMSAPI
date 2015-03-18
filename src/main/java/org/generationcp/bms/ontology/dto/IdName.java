cf7b26c38b95bb54e815aaf92cdde8d8c8046548
keyurbhatt <keyur@leafnode.io>
2015-03-18 12:02:45 +0530

    Added Constructor to IdName class to resolve ModelMapper problem of passing enum DataType

--- src/main/java/org/generationcp/bms/ontology/dto/IdName.java
+++ src/main/java/org/generationcp/bms/ontology/dto/IdName.java
@@ -6,6 +6,14 @@ public class IdName {
     private Integer id;
     private String name;
 
+    public IdName() {
+    }
+
+    public IdName(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Integer getId() {
         return id;
     }
