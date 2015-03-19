a68c2d448dd61be7ba7dc9f42e1f65e3d851d783
srajwadi <sunny@leafnode.io>
2015-03-19 10:57:15 +0530

    Cleanup for unused imports and variables.

--- src/main/java/org/generationcp/bms/ontology/validator/IntegerValidator.java
+++ src/main/java/org/generationcp/bms/ontology/validator/IntegerValidator.java
@@ -1,16 +1,12 @@
 package org.generationcp.bms.ontology.validator;
 
 import org.generationcp.bms.ontology.dto.MethodRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.validation.Errors;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class IntegerValidator extends BaseValidator implements org.springframework.validation.Validator{
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(IntegerValidator.class);
 
     @Override
     public boolean supports(Class<?> aClass) {
