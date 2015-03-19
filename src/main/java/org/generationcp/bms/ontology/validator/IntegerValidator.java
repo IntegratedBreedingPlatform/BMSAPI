d64612d18435cac2f877e18b5d33c695e2b80493
srajwadi <sunny@leafnode.io>
2015-03-19 10:56:28 +0530

    Hooking IntegerValidator with BaseValidator
    Issue: BMS-369
    
    Reviewer: Naymesh

--- src/main/java/org/generationcp/bms/ontology/validator/IntegerValidator.java
+++ src/main/java/org/generationcp/bms/ontology/validator/IntegerValidator.java
@@ -1,24 +1,16 @@
 package org.generationcp.bms.ontology.validator;
 
 import org.generationcp.bms.ontology.dto.MethodRequest;
-import org.generationcp.bms.util.I18nUtil;
-import org.generationcp.middleware.service.api.OntologyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
+import org.springframework.stereotype.Component;
+
 @Component
-public class IntegerValidator implements org.springframework.validation.Validator{
+public class IntegerValidator extends BaseValidator implements org.springframework.validation.Validator{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IntegerValidator.class);
-
-    @Autowired
-    OntologyService ontologyService;
-    @Autowired
-    ResourceBundleMessageSource messageSource;
 
     @Override
     public boolean supports(Class<?> aClass) {
@@ -27,12 +19,7 @@ public class IntegerValidator implements org.springframework.validation.Validato
 
     @Override
     public void validate(Object target, Errors errors) {
-
         String id = (String) target;
-
-        if(! id.matches("^[0-9]+$")){
-            LOGGER.error("field should be numeric");
-            errors.rejectValue("id", I18nUtil.formatErrorMessage(messageSource, "should.be.numeric", null));
-        }
+        checkNumber(id, errors);
     }
 }
