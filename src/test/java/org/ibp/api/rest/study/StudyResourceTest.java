cc34214abec8a55261ac2ce02cba0080ca6b2270
Naymesh <naymesh@leafnode.io>
2015-04-10 13:20:35 +1200

    Fix the StudyResourceTest for changes in previous commit (hooking up to the new Middleware StudyService).
    Issue: BMS-591
    Reviewer: AkhilM

--- src/test/java/org/ibp/api/rest/study/StudyResourceTest.java
+++ src/test/java/org/ibp/api/rest/study/StudyResourceTest.java
@@ -3,11 +3,11 @@ package org.ibp.api.rest.study;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.generationcp.middleware.domain.oms.StudyType;
 import org.generationcp.middleware.manager.api.WorkbenchDataManager;
 import org.generationcp.middleware.pojos.workbench.CropType;
 import org.hamcrest.Matchers;
 import org.ibp.ApiUnitTestBase;
-import org.ibp.api.domain.study.StudySummary;
 import org.ibp.api.java.study.StudyService;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -37,6 +37,12 @@ public class StudyResourceTest extends ApiUnitTestBase {
 		public StudyService getStudyService() {
 			return Mockito.mock(StudyService.class);
 		}
+		
+		@Bean
+		@Primary
+		public org.generationcp.middleware.service.api.study.StudyService getStudyServiceMW() {
+			return Mockito.mock(org.generationcp.middleware.service.api.study.StudyService.class);
+		}
 	}
 
 	@Autowired
@@ -44,38 +50,41 @@ public class StudyResourceTest extends ApiUnitTestBase {
 
 	@Autowired
 	private StudyService studyService;
+	
+	@Autowired
+	private org.generationcp.middleware.service.api.study.StudyService studyServiceMW;
 
 	@Test
 	public void testListAllStudies() throws Exception {
 
-		List<StudySummary> summaries = new ArrayList<>();
-		StudySummary summary = new StudySummary();
-		summary.setId(1);
-		summary.setName("A Maizing Trial");
-		summary.setTitle("A Maizing Trial Title");
-		summary.setObjective("A Maize the world with new Maize variety.");
-		summary.setType("TRIAL");
-		summary.setStartDate("01012015");
-		summary.setEndDate("01012015");
-		summaries.add(summary);
+		List<org.generationcp.middleware.service.api.study.StudySummary> summariesMW = new ArrayList<>();
+		org.generationcp.middleware.service.api.study.StudySummary summaryMW = new org.generationcp.middleware.service.api.study.StudySummary();
+		summaryMW.setId(1);
+		summaryMW.setName("A Maizing Trial");
+		summaryMW.setTitle("A Maizing Trial Title");
+		summaryMW.setObjective("A Maize the world with new Maize variety.");
+		summaryMW.setType(StudyType.T);
+		summaryMW.setStartDate("01012015");
+		summaryMW.setEndDate("01012015");
+		summariesMW.add(summaryMW);
 
 		Mockito.doReturn(new CropType(cropName)).when(this.workbenchDataManager).getCropTypeByName(cropName);
-		Mockito.when(this.studyService.listAllStudies()).thenReturn(summaries);
-
+		Mockito.when(this.studyServiceMW.listAllStudies()).thenReturn(summariesMW);
+		
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/study/{cropname}/list", "maize")
 				.contentType(this.contentType))
 				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.jsonPath("$", IsCollectionWithSize.hasSize(summaries.size())))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['id']", Matchers.is(summary.getId())))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['name']", Matchers.is(summary.getName())))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['title']", Matchers.is(summary.getTitle())))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['objective']", Matchers.is(summary.getObjective())))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['type']", Matchers.is(summary.getType())))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['startDate']", Matchers.is(summary.getStartDate())))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['endDate']", Matchers.is(summary.getEndDate())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$", IsCollectionWithSize.hasSize(summariesMW.size())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['id']", Matchers.is(summaryMW.getId())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['name']", Matchers.is(summaryMW.getName())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['title']", Matchers.is(summaryMW.getTitle())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['objective']", Matchers.is(summaryMW.getObjective())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['type']", Matchers.is(summaryMW.getType().getName())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['startDate']", Matchers.is(summaryMW.getStartDate())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0]['endDate']", Matchers.is(summaryMW.getEndDate())))
 				.andDo(MockMvcResultHandlers.print());
 
-		Mockito.verify(this.studyService).listAllStudies();
+		Mockito.verify(this.studyServiceMW).listAllStudies();
 	}
 
 }
