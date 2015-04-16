86cc13af1c84d000abd601c227d20adce01b7d52
keyurbhatt <keyur@leafnode.io>
2015-04-16 08:12:07 +0530

    Demerge Method Summary (Method list response) class from Reques. Both are separate business objects and should not depend on each other.
    Issue: none
    
    Reviewer: Sunny

--- src/main/java/org/ibp/api/domain/ontology/MethodSummary.java
+++ src/main/java/org/ibp/api/domain/ontology/MethodSummary.java
@@ -1,5 +1,42 @@
 package org.ibp.api.domain.ontology;
 
-public class MethodSummary extends MethodRequest {
+public class MethodSummary {
+    private String id;
 
+    private String name;
+
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "MethodSummary{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
