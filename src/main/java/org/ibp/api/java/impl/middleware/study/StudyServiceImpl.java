21098aac65b01ddd6f350cdcd874f84b8a8794e7
Naymesh <naymesh@leafnode.io>
2015-04-17 12:32:04 +1200

    Updated mock trial observation data: 3 types of seeds (germplasm), planted in 2 environments (trial instances), replicated twice in each environment, measuring two traits : (Grain Color and 100 grain weight).

--- src/main/java/org/ibp/api/java/impl/middleware/study/StudyServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/study/StudyServiceImpl.java
@@ -43,24 +43,44 @@ public class StudyServiceImpl implements StudyService {
 	
 	@Override
 	public List<Observation> getObservations(Integer studyId) {
-		Observation obs1 = createMockMeasurements(800000110, "ICARDA_GCP H2", "11", "12");
-		Observation obs2 = createMockMeasurements(3829816, "WAXWING*2/CIRCUS", "21", "22");
-		return Lists.newArrayList(obs1, obs2);
+		//Environment 1, Replicate 1
+		Observation obs111 = createMockMeasurements(800000006, "Zhoumai18",       1, "Test Entry", 1, "Unknown", 1, 1, "100GW_g", "101", "GRNCOL", "Light Brown 1");
+		Observation obs112 = createMockMeasurements(800000008, "Chang4738",       2, "Test Entry", 2, "Unknown", 1, 1, "100GW_g", "102", "GRNCOL", "Light Brown 2");
+		Observation obs113 = createMockMeasurements(800000010, "Sokoll (CIMMYT)", 3, "Test Entry", 3, "Unknown", 1, 1, "100GW_g", "103", "GRNCOL", "Light Brown 3");
+		
+		//Environment 1, Replicate 2
+		Observation obs121 = createMockMeasurements(800000006, "Zhoumai18",       1, "Test Entry", 1, "Unknown", 2, 1, "100GW_g", "104", "GRNCOL", "Light Brown 4");
+		Observation obs122 = createMockMeasurements(800000008, "Chang4738",       2, "Test Entry", 2, "Unknown", 2, 1, "100GW_g", "105", "GRNCOL", "Light Brown 5");
+		Observation obs123 = createMockMeasurements(800000010, "Sokoll (CIMMYT)", 3, "Test Entry", 3, "Unknown", 2, 1, "100GW_g", "106", "GRNCOL", "Light Brown 6");
+	
+		//Environment 2, Replicate 1
+		Observation obs211 = createMockMeasurements(800000006, "Zhoumai18",       1, "Test Entry", 1, "Unknown", 1, 2, "100GW_g", "201", "GRNCOL", "Dark Brown 1");
+		Observation obs212 = createMockMeasurements(800000008, "Chang4738",       2, "Test Entry", 2, "Unknown", 1, 2, "100GW_g", "202", "GRNCOL", "Dark Brown 2");
+		Observation obs213 = createMockMeasurements(800000010, "Sokoll (CIMMYT)", 3, "Test Entry", 3, "Unknown", 1, 2, "100GW_g", "203", "GRNCOL", "Dark Brown 3");
+		
+		//Environment 2, Replicate 2
+		Observation obs221 = createMockMeasurements(800000006, "Zhoumai18",       1, "Test Entry", 1, "Unknown", 2, 2, "100GW_g", "204", "GRNCOL", "Dark Brown 4");
+		Observation obs222 = createMockMeasurements(800000008, "Chang4738",       2, "Test Entry", 2, "Unknown", 2, 2, "100GW_g", "205", "GRNCOL", "Dark Brown 5");
+		Observation obs223 = createMockMeasurements(800000010, "Sokoll (CIMMYT)", 3, "Test Entry", 3, "Unknown", 2, 2, "100GW_g", "206", "GRNCOL", "Dark Brown 6");
+
+		return Lists.newArrayList(obs111, obs112, obs113, obs121, obs122, obs123, obs211, obs212, obs213, obs221, obs222, obs223);
 	}
 
-	private Observation createMockMeasurements(Integer gid, String germplasm, String value1, String value2) {
-		Observation obs = new Observation();
-		obs.setGermplasmId(gid);
-		obs.setGermplasmDesignation(germplasm);
-		obs.setEnrtyNumber(95);
-		obs.setEntryType("Test Entry");
-		obs.setReplicationNumber(1);
-		obs.setPlotNumber(1);
-		obs.setEnvironmentNumber(1);
-		obs.setParentage("ICARDA_GCP H0/ICARDA_GCP H1");
+	private Observation createMockMeasurements(Integer germplasmId, String germplasmDesignation, Integer enrtyNumber, String entryType, Integer plotNumber, String parentage,
+			Integer replicationNumber, Integer environmentNumber, String trait1Name, String trait1Value, String trait2Name, String trait2Value) {
 		
-		Measurement measurement1 = new Measurement(1, "AGRSCR_0_9", value1);
-		Measurement measurement2 = new Measurement(2, "Plant_height", value2);
+		Observation obs = new Observation();
+		obs.setGermplasmId(germplasmId);
+		obs.setGermplasmDesignation(germplasmDesignation);
+		obs.setEnrtyNumber(enrtyNumber);
+		obs.setEntryType(entryType);
+		obs.setReplicationNumber(replicationNumber);
+		obs.setPlotNumber(plotNumber);
+		obs.setEnvironmentNumber(environmentNumber);
+		obs.setParentage(parentage);
+
+		Measurement measurement1 = new Measurement(1, trait1Name, trait1Value);
+		Measurement measurement2 = new Measurement(2, trait2Name, trait2Value);
 		obs.setMeasurements(Lists.newArrayList(measurement1, measurement2));
 		return obs;
 	}
