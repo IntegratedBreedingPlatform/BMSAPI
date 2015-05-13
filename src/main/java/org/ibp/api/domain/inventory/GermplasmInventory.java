cb280ad6335812079d6f97cb8eecf4f972ab1c97
Naymesh <naymesh@leafnode.io>
2015-05-13 19:01:20 +1200

    Initial Commit: Re-instated prototype APIs for Inventory fitted into new structure. Kept services and data structures as they were in prototype to use as a base. Needs designing, refactoring.

---
+++ src/main/java/org/ibp/api/domain/inventory/GermplasmInventory.java
@@ -0,0 +1,125 @@
+package org.ibp.api.domain.inventory;
+
+import com.wordnik.swagger.annotations.ApiModel;
+
+import org.generationcp.middleware.pojos.ims.LotStatus;
+import org.ibp.api.domain.ontology.TermSummary;
+
+@ApiModel("Germplasm Inventory Information")
+public class GermplasmInventory {
+
+	private Integer gid;
+	private Integer lotId;
+	private Double quantityAvailable;
+	private Double quantityReserved;
+	private Double quantityTotal;
+	private TermSummary quantityUnit;
+	private Integer userId;
+	private String userName;
+	private InventoryLocation location;
+	private LotStatus lotStatus;
+	private String comments;
+
+	public GermplasmInventory() { }
+
+	public GermplasmInventory(Integer gid) {
+		this.gid = gid;
+	}
+
+	public Integer getGid() {
+		return gid;
+	}
+	
+	public void setGid(Integer gid) {
+		this.gid = gid;
+	}
+
+	public Integer getLotId() {
+		return lotId;
+	}
+
+	public void setLotId(Integer lotId) {
+		this.lotId = lotId;
+	}
+
+	public Double getQuantityAvailable() {
+		return quantityAvailable;
+	}
+
+	public void setQuantityAvailable(Double quantityAvailable) {
+		this.quantityAvailable = quantityAvailable;
+	}
+
+	public Double getQuantityReserved() {
+		return quantityReserved;
+	}
+
+	public void setQuantityReserved(Double quantityReserved) {
+		this.quantityReserved = quantityReserved;
+	}
+	
+	public Double getQuantityTotal() {
+		return quantityTotal;
+	}
+
+	public void setQuantityTotal(Double quantityTotal) {
+		this.quantityTotal = quantityTotal;
+	}
+
+	public TermSummary getQuantityUnit() {
+		return quantityUnit;
+	}
+
+	public void setQuantityUnit(TermSummary quantityUnit) {
+		this.quantityUnit = quantityUnit;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public InventoryLocation getLocation() {
+		return location;
+	}
+
+	public void setLocation(InventoryLocation inventoryLocation) {
+		this.location = inventoryLocation;
+	}
+
+	public LotStatus getLotStatus() {
+		return lotStatus;
+	}
+
+	public void setLotStatus(LotStatus lotStatus) {
+		this.lotStatus = lotStatus;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	@Override
+	public String toString() {
+		return "GermplasmInventoryInfo [gid=" + gid + ", lotId=" + lotId + ", quantityAvailable="
+				+ quantityAvailable + ", quantityReserved=" + quantityReserved + ", quantityUnit="
+				+ quantityUnit + ", userId=" + userId + ", userName=" + userName + ", location="
+				+ location + ", lotStatus=" + lotStatus + ", comments=" + comments + "]";
+	}
+
+}
---
+++ src/main/java/org/ibp/api/rest/inventory/InventoryResource.java
@@ -0,0 +1,58 @@
+
+package org.ibp.api.rest.inventory;
+
+import java.util.List;
+
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.ibp.api.domain.inventory.GermplasmInventory;
+import org.ibp.api.java.Inventory.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+
+@Api(value = "Inventory Services")
+@RestController
+@RequestMapping("/inventory")
+public class InventoryResource {
+
+	@Autowired
+	private InventoryService inventoryService;
+
+	@RequestMapping(value = "/{cropname}/germplasm/{gid}", method = RequestMethod.GET)
+	@ApiOperation(value = "Get Inventory Information", notes = "Returns information about all inventory lots available for the given germplasm id (gid).")
+	public List<GermplasmInventory> getInventoryLotInfoForGermplasm(@PathVariable String gid, @PathVariable String cropname) {
+		return inventoryService.getInventoryLotInfoForGermplasm(gid);
+	}
+
+	@RequestMapping(value = "/{cropname}/germplasm/{gid}", method = RequestMethod.PUT)
+	@ApiResponses(value = {@ApiResponse(code = 201, message = "Created")})
+	public ResponseEntity<?> createInverntory(@PathVariable String cropname, @RequestBody GermplasmInventory germplasmInventory, @PathVariable String gid)
+			throws MiddlewareQueryException {
+		inventoryService.createInverntory(germplasmInventory, gid);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/{cropname}/germplasm/{gid}", method = RequestMethod.POST)
+	public ResponseEntity<?> updateInverntory(@PathVariable String cropname, @RequestBody GermplasmInventory germplasmInventory, @PathVariable String gid) {
+		inventoryService.updateInverntory(germplasmInventory, gid);
+		return new ResponseEntity<>(HttpStatus.OK);
+
+	}
+
+	@RequestMapping(value = "/{cropname}/germplasm/{gid}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteInverntory(@PathVariable String cropname, @PathVariable String gid) {
+		inventoryService.deleteInverntory(gid);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+}
