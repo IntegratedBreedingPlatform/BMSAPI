4ae7d3c6838cc2dc4d1767566b0586d0cbdddd95
srajwadi <sunny@leafnode.io>
2015-05-26 00:08:15 +0530

    Adding DataType pojo for ibp that replace generic usage of IdName class.
    Issue: NA
    
    Reviewer: Naymesh

---
+++ src/main/java/org/ibp/api/domain/ontology/DataType.java
@@ -0,0 +1,31 @@
+package org.ibp.api.domain.ontology;
+
+public class DataType {
+
+    private Integer id;
+    private String name;
+
+    public DataType(){
+    }
+
+    public DataType(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
