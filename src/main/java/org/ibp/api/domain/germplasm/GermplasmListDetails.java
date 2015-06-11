037fb93fadc426a576b0c604d405df4c93246b6c
Akhil <Akhil@leafnode.io>
2015-06-12 10:30:15 +1200

    Adding in equals and hashcode for the GermplasmListDetails.
    issue: BMS-591
    reviewer: NaymeshM

--- src/main/java/org/ibp/api/domain/germplasm/GermplasmListDetails.java
+++ src/main/java/org/ibp/api/domain/germplasm/GermplasmListDetails.java
@@ -4,6 +4,9 @@ package org.ibp.api.domain.germplasm;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class GermplasmListDetails extends GermplasmListSummary {
 
 	private final List<GermplasmListEntrySummary> germplasmEntries = new ArrayList<GermplasmListEntrySummary>();
@@ -16,6 +19,35 @@ public class GermplasmListDetails extends GermplasmListSummary {
 
 	public List<GermplasmListEntrySummary> getGermplasmEntries() {
 		return this.germplasmEntries;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object other) {
+		if (!(other instanceof GermplasmListDetails)) {
+			return false;
+		}
+		GermplasmListDetails castOther = (GermplasmListDetails) other;
+		return new EqualsBuilder().append(this.germplasmEntries, castOther.germplasmEntries).isEquals();
+	}
+
+	private transient int hashCode;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		if (this.hashCode == 0) {
+			this.hashCode = new HashCodeBuilder().append(this.germplasmEntries).toHashCode();
+		}
+		return this.hashCode;
 	}
 
 }
