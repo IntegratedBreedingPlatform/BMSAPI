b3d81168d8fe8a5d04a53228f5114e2870066456
keyurbhatt <keyur@leafnode.io>
2015-07-03 15:33:56 +0530

    Adding data type with system flag.
    Issue: BMS-1172
    
    Reviewer: Sunny

--- src/main/java/org/ibp/api/domain/ontology/DataType.java
+++ src/main/java/org/ibp/api/domain/ontology/DataType.java
@@ -8,13 +8,15 @@ public class DataType {
 
 	private String id;
 	private String name;
+	private boolean systemDataType;
 
 	public DataType() {
 	}
 
-	public DataType(String id, String name) {
+	public DataType(String id, String name, boolean systemDataType) {
 		this.id = id;
 		this.name = name;
+		this.systemDataType = systemDataType;
 	}
 
 	public String getId() {
@@ -33,18 +35,26 @@ public class DataType {
 		this.name = name;
 	}
 
+	public boolean isSystemDataType() {
+		return systemDataType;
+	}
+
+	public void setSystemDataType(boolean systemDataType) {
+		this.systemDataType = systemDataType;
+	}
+
 	@Override
 	public boolean equals(final Object other) {
 		if (!(other instanceof DataType)) {
 			return false;
 		}
 		DataType castOther = (DataType) other;
-		return new EqualsBuilder().append(this.id, castOther.id).append(this.name, castOther.name).isEquals();
+		return new EqualsBuilder().append(this.id, castOther.id).append(this.name, castOther.name).append(this.systemDataType, castOther.systemDataType).isEquals();
 	}
 
 	@Override
 	public int hashCode() {
-		return new HashCodeBuilder().append(this.id).append(this.name).toHashCode();
+		return new HashCodeBuilder().append(this.id).append(this.name).append(this.systemDataType).toHashCode();
 	}
 
 }
