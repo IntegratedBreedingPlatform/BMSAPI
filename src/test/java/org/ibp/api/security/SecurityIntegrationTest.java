918206332f54f6b83394dad93a137c1e465bf621
Naymesh <naymesh@leafnode.io>
2015-11-18 17:36:48 +1300

    Cleanup test: Extract constant string out. Comments. Additional assertions.
    Issue: BMS-459
    Reviewer: AkhilM

--- src/test/java/org/ibp/api/security/SecurityIntegrationTest.java
+++ src/test/java/org/ibp/api/security/SecurityIntegrationTest.java
@@ -5,6 +5,7 @@ import java.nio.charset.Charset;
 
 import org.ibp.Main;
 import org.ibp.api.security.xauth.Token;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,11 +14,13 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -46,6 +49,10 @@ public class SecurityIntegrationTest {
 	protected final MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(),
 			Charset.forName("utf8"));
 
+	private static final String TEST_USER = "testusername";
+	private static final String TEST_PASS = "testuserpassword";
+	private static final String TEST_ROLE = "ADMIN";
+
 	@Configuration
 	public static class TestConfiguration {
 
@@ -53,10 +60,9 @@ public class SecurityIntegrationTest {
 		// Higher priority to in memory user service for test only.
 		@Primary
 		public UserDetailsService userDetailsService() {
-			// Password is a BCrypt strting for "testuserpassword"
-			UserDetails testUser =
-					new User("testuser", "$2a$08$UURcoje1Fghoho7ylgCZuuAKPNZOkSTJfen1BJYZ7NxqgGuc1cMv2",
-							Lists.newArrayList(new SimpleGrantedAuthority("ADMIN")));
+			// Application context is using BCryptPasswordEncoder so setting encrypted password in memory test user store.
+			String bcryptPassword = new BCryptPasswordEncoder().encode(TEST_PASS);
+			UserDetails testUser = new User(TEST_USER, bcryptPassword, Lists.newArrayList(new SimpleGrantedAuthority(TEST_ROLE)));
 			return new InMemoryUserDetailsManager(Lists.newArrayList(testUser));
 		}
 	}
@@ -68,24 +74,25 @@ public class SecurityIntegrationTest {
 
 	@Test
 	public void testUnauthorizedRequest() throws Exception {
-		// Call one of the services requiring authentication (ontology datatype service chosen at random) without auth header.
+		// Call one of the services requiring authentication (ontology datatype service chosen at random) without auth header
 		// and expect 401 Unauthorized response
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/ontology/datatypes").contentType(this.contentType))
-				.andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isUnauthorized());
+		.andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isUnauthorized());
 	}
 
 	@Test
 	public void testAuthorizedRequest() throws Exception {
 
 		// Hit the /authentication resource to obtain token first.
-		final MvcResult authResult =
-				this.mockMvc.perform(MockMvcRequestBuilders.post("/authenticate?username=testuser&password=testuserpassword")).andReturn();
+		String tokenUrl = String.format("/authenticate?username=%s&password=%s", TEST_USER, TEST_PASS);
+		final MvcResult authResult = this.mockMvc.perform(MockMvcRequestBuilders.post(tokenUrl)).andReturn();
+		Assert.assertEquals("Was expecting a successful token retrieval.", HttpStatus.OK.value(), authResult.getResponse().getStatus());
 
 		// Parse the response to get token
 		Token token = new ObjectMapper().readValue(authResult.getResponse().getContentAsByteArray(), Token.class);
 
-		// Call one of the services requiring authentication (ontology datatype service chosen at random) with a valid auth header.
-		// and expect 200 OK response
+		// Call one of the services requiring authentication (ontology datatype service chosen at random) with a valid auth header
+		//   and expect 200 OK response
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/ontology/datatypes")
 				.contentType(this.contentType)
 				.header("X-Auth-Token", token.getToken()))
