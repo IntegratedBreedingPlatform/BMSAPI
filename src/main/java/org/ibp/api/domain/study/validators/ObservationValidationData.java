209c2c7f62cccf6faaf5dab12eb4526b4980d82f
Akhil Mehra <akhil@leafnode.io>
2015-11-30 17:28:47 +1300

    Data strucutres to enable validation
    Adding in data structures to make validating observations easier. Note
    these are not be be used any where else.
    
    issue: BMS-2066
    reviewer: NaymeshM

---
+++ src/main/java/org/ibp/api/domain/study/validators/ObservationValidationData.java
@@ -0,0 +1,66 @@
+
+package org.ibp.api.domain.study.validators;
+
+import java.util.Map;
+
+/**
+ * Data structure that holds all data needed to validate a observation.
+ */
+public class ObservationValidationData {
+
+	/**
+	 * Crop that the observation is being carried out for.
+	 */
+	private String cropName;
+	
+	/**
+	 * Program id of the observation 
+	 */
+	private String programId;
+	
+	
+	/**
+	 * Study id of the observation
+	 */
+	private String studyId;
+	
+	/**
+	 * The actual observation id.
+	 */
+	private Integer observationId;
+	
+	/**
+	 * Map of array index to measurement variable details.  
+	 */
+	private Map<Integer, MeasurementVariableDetails> measurementVariableDetailsList;
+
+	public ObservationValidationData(final String cropName, final String programId, final String studyId, final Integer observationId,
+			final Map<Integer, MeasurementVariableDetails> measurementVariableDetailsList) {
+		this.cropName = cropName;
+		this.programId = programId;
+		this.studyId = studyId;
+		this.observationId = observationId;
+		this.measurementVariableDetailsList = measurementVariableDetailsList;
+	}
+
+	public String getCropName() {
+		return this.cropName;
+	}
+
+	public String getProgramId() {
+		return this.programId;
+	}
+
+	public String getStudyId() {
+		return this.studyId;
+	}
+
+	public Integer getObservationId() {
+		return this.observationId;
+	}
+
+	public Map<Integer, MeasurementVariableDetails> getMeasurementVariableDetailsList() {
+		return this.measurementVariableDetailsList;
+	}
+
+}
