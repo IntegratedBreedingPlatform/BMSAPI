a9507acc537e9bfe51be31bcc3c90857f88a5050
keyurbhatt <keyur@leafnode.io>
2016-03-16 16:57:54 +0530

    Adding test for getGermplasm method, added test that handles exception thrown branches as well for getGermplasm and searchGermplasm method. Refactor existing method and created generalised methods that can be used at multiple places.
    Issue: BMS-2534
    Reviewer: Abhishek

--- src/test/java/org/ibp/api/java/impl/middleware/germplasm/GermplasmServiceImplTest.java
+++ src/test/java/org/ibp/api/java/impl/middleware/germplasm/GermplasmServiceImplTest.java
@@ -3,6 +3,7 @@ package org.ibp.api.java.impl.middleware.germplasm;
 
 import java.util.List;
 
+import com.google.common.collect.Lists;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.manager.GermplasmNameType;
 import org.generationcp.middleware.manager.Operation;
@@ -12,9 +13,11 @@ import org.generationcp.middleware.pojos.Germplasm;
 import org.generationcp.middleware.pojos.Location;
 import org.generationcp.middleware.pojos.Method;
 import org.generationcp.middleware.pojos.Name;
+import org.generationcp.middleware.pojos.UserDefinedField;
 import org.generationcp.middleware.service.api.PedigreeService;
 import org.generationcp.middleware.util.CrossExpansionProperties;
 import org.ibp.api.domain.germplasm.GermplasmSummary;
+import org.ibp.api.exception.ApiRuntimeException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,8 +25,6 @@ import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
-import com.google.common.collect.Lists;
 
 public class GermplasmServiceImplTest {
 
@@ -58,24 +59,25 @@ public class GermplasmServiceImplTest {
 		gp.setLocationId(1);
 
 		List<Germplasm> middlewareSearchResults = Lists.newArrayList(gp);
+		middlewareSearchResults.add(null);
+
 		Mockito.when(this.germplasmDataManager.searchForGermplasm("CML", Operation.LIKE, false, false)).thenReturn(middlewareSearchResults);
 		String gpPedigree = "CML1/CML2";
 		Mockito.when(this.pedigreeService.getCrossExpansion(Matchers.anyInt(), Matchers.any(CrossExpansionProperties.class))).thenReturn(
 				gpPedigree);
 
-		Name gpName = new Name();
-		gpName.setGermplasmId(gp.getGid());
-		gpName.setNval("CML1");
+		Name gpName = this.createName(gp.getGid());
 		List<Name> gpNames = Lists.newArrayList(gpName);
 		Mockito.when(this.germplasmDataManager.getNamesByGID(Matchers.anyInt(), Matchers.anyInt(), Matchers.any(GermplasmNameType.class)))
 				.thenReturn(gpNames);
 
-		Method gpMethod = new Method();
-		gpMethod.setMname("Backcross");
+		UserDefinedField udf = this.createUserDefinedField();
+		Mockito.when(this.germplasmDataManager.getUserDefinedFieldByID(gpName.getTypeId())).thenReturn(udf);
+
+		Method gpMethod = this.createMethod();
 		Mockito.when(this.germplasmDataManager.getMethodByID(Matchers.anyInt())).thenReturn(gpMethod);
 
-		Location gpLocation = new Location();
-		gpLocation.setLname("Mexico");
+		Location gpLocation = this.createLocation();
 		Mockito.when(this.locationDataManger.getLocationByID(Matchers.anyInt())).thenReturn(gpLocation);
 
 		List<GermplasmSummary> germplasmSummaries = this.germplasmServiceImpl.searchGermplasm("CML");
@@ -90,4 +92,87 @@ public class GermplasmServiceImplTest {
 		Assert.assertEquals(gpName.getNval(), germplasmSummaries.get(0).getNames().get(0).getName());
 	}
 
+	@Test(expected = ApiRuntimeException.class)
+	public void testSearchGermplasmThrowsException() throws MiddlewareQueryException {
+		Mockito.when(this.germplasmDataManager.searchForGermplasm("CML", Operation.LIKE, false, false)).thenThrow(MiddlewareQueryException.class);
+
+		List<GermplasmSummary> germplasmSummaries = this.germplasmServiceImpl.searchGermplasm("CML");
+	}
+
+	@Test
+	public void testGetGermplasm(){
+
+		Germplasm germplasm = new Germplasm();
+		germplasm.setGid(3);
+		germplasm.setGpid1(1);
+		germplasm.setGpid2(2);
+		germplasm.setMethodId(1);
+		germplasm.setLocationId(1);
+
+		Mockito.when(this.germplasmDataManager.getGermplasmByGID(germplasm.getGid())).thenReturn(germplasm);
+
+		String gpPedigree = "CML1/CML2";
+		Mockito.when(this.pedigreeService.getCrossExpansion(Matchers.anyInt(), Matchers.any(CrossExpansionProperties.class))).thenReturn(
+				gpPedigree);
+
+		Name gpName = this.createName(germplasm.getGid());
+
+		List<Name> gpNames = Lists.newArrayList(gpName);
+		Mockito.when(this.germplasmDataManager.getNamesByGID(Matchers.anyInt(), Matchers.anyInt(), Matchers.any(GermplasmNameType.class)))
+				.thenReturn(gpNames);
+
+		UserDefinedField udf = this.createUserDefinedField();
+		Mockito.when(this.germplasmDataManager.getUserDefinedFieldByID(gpName.getTypeId())).thenReturn(udf);
+
+		Method gpMethod = this.createMethod();
+		Mockito.when(this.germplasmDataManager.getMethodByID(Matchers.anyInt())).thenReturn(gpMethod);
+
+		Location gpLocation = this.createLocation();
+		Mockito.when(this.locationDataManger.getLocationByID(Matchers.anyInt())).thenReturn(gpLocation);
+
+		GermplasmSummary germplasmSummary = this.germplasmServiceImpl.getGermplasm(String.valueOf(germplasm.getGid()));
+
+		Assert.assertEquals(germplasm.getGid().toString(), germplasmSummary.getGermplasmId());
+		Assert.assertEquals(germplasm.getGpid1().toString(), germplasmSummary.getParent1Id());
+		Assert.assertEquals(germplasm.getGpid2().toString(), germplasmSummary.getParent2Id());
+		Assert.assertEquals(gpPedigree, germplasmSummary.getPedigreeString());
+		Assert.assertEquals(gpMethod.getMname(), germplasmSummary.getBreedingMethod());
+		Assert.assertEquals(gpLocation.getLname(), germplasmSummary.getLocation());
+		Assert.assertEquals(gpName.getNval(), germplasmSummary.getNames().get(0).getName());
+	}
+
+	@Test(expected = ApiRuntimeException.class)
+	public void testGetGermplasmThrowsException() throws MiddlewareQueryException {
+		Integer germplasmId = 1;
+		Mockito.when(this.germplasmDataManager.getGermplasmByGID(germplasmId)).thenThrow(MiddlewareQueryException.class);
+
+		GermplasmSummary germplasm = this.germplasmServiceImpl.getGermplasm(String.valueOf(germplasmId));
+	}
+
+	private UserDefinedField createUserDefinedField(){
+		UserDefinedField udf = new UserDefinedField();
+		udf.setFcode("FCode");
+		udf.setFname("FName");
+		return udf;
+	}
+
+	private Method createMethod(){
+		Method gpMethod = new Method();
+		gpMethod.setMname("Backcross");
+		return gpMethod;
+	}
+
+	private Location createLocation(){
+		Location gpLocation = new Location();
+		gpLocation.setLname("Mexico");
+		return gpLocation;
+	}
+
+	private Name createName(Integer germplasmId){
+		Name gpName = new Name();
+		gpName.setGermplasmId(germplasmId);
+		gpName.setNval("CML1");
+		gpName.setTypeId(1);
+		return gpName;
+	}
 }
