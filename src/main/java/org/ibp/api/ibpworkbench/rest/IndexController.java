3a4fe02366de12a7db1fff2a9907fdd92f7daadd
alejandroDroptek <amildiner@droptek.com>
2016-05-11 16:46:15 -0300

    Breading view webservice merged and Spring configuration setted

---
+++ src/main/java/org/ibp/api/ibpworkbench/rest/IndexController.java
@@ -0,0 +1,20 @@
+
+package org.ibp.api.ibpworkbench.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.sun.jersey.api.view.Viewable;
+
+@Path("/")
+public class IndexController {
+
+	@GET
+	@Path("/index")
+	@Produces("text/html")
+	public Viewable index() {
+		return new Viewable("/index");
+	}
+
+}
