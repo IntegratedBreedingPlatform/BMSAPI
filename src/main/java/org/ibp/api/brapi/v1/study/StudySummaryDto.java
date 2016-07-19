a1649f499b24bdcaa33a8c509f4995fbc141e755
Clarysabel Tovar <clarysabel@leafnode.io>
2016-07-19 12:51:53 -0300

    Adding Pojomatic dependency, defining DTOs and Resource to the List study summaries service Issue: BMS-3032

---
+++ src/main/java/org/ibp/api/brapi/v1/study/StudySummaryDto.java
@@ -0,0 +1,214 @@
+package org.ibp.api.brapi.v1.study;
+
+import com.fasterxml.jackson.annotation.*;
+import org.pojomatic.Pojomatic;
+import org.pojomatic.annotations.AutoProperty;
+
+import java.util.List;
+import java.util.Map;
+
+@AutoProperty
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"studyDbId", "name", "studyType", "years", "seasons", "locationDbId", "programDbId", "optionalInfo"})
+public class StudySummaryDto {
+
+    private Integer studyDbId;
+
+    private String name;
+
+    private String studyType;
+
+    private List<String> years;
+
+    private List<String> seasons;
+
+    private Integer locationDbId;
+
+    private Integer programDbId;
+
+    @JsonIgnore
+    private Map<String, Object> optionalInfo;
+
+    /**
+     *
+     * @return The study db id
+     */
+    public Integer getStudyDbId() {
+        return studyDbId;
+    }
+
+    /**
+     *
+     * @param studyDbId
+     * @return this
+     */
+    public StudySummaryDto setStudyDbId(Integer studyDbId) {
+        this.studyDbId = studyDbId;
+        return this;
+    }
+
+    /**
+     *
+     * @return The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name
+     * @return this
+     */
+    public StudySummaryDto setName(final String name) {
+        this.name = name;
+        return this;
+    }
+
+    /**
+     *
+     * @return The study type
+     */
+    public String getStudyType() {
+        return studyType;
+    }
+
+    /**
+     *
+     * @param studyType
+     * @return this
+     */
+    public StudySummaryDto setStudyType(final String studyType) {
+        this.studyType = studyType;
+        return this;
+    }
+
+    /**
+     *
+     * @return The list of years
+     */
+    public List<String> getYears() {
+        return years;
+    }
+
+    /**
+     *
+     * @param years
+     * @return this
+     */
+    public StudySummaryDto setYears(final List<String> years) {
+        this.years = years;
+        return this;
+    }
+
+    /**
+     *
+     * @return The list of seasons
+     */
+    public List<String> getSeasons() {
+        return seasons;
+    }
+
+    /**
+     *
+     * @param seasons
+     * @return this
+     */
+    public StudySummaryDto setSeasons(final List<String> seasons) {
+        this.seasons = seasons;
+        return this;
+    }
+
+    /**
+     *
+     * @return The location db id
+     */
+    public Integer getLocationDbId() {
+        return locationDbId;
+    }
+
+    /**
+     *
+     * @param locationDbId
+     * @return this
+     */
+    public StudySummaryDto setLocationDbId(final Integer locationDbId) {
+        this.locationDbId = locationDbId;
+        return this;
+    }
+
+    /**
+     *
+     * @return The program db id
+     */
+    public Integer getProgramDbId() {
+        return programDbId;
+    }
+
+    /**
+     *
+     * @param programDbId
+     * @return this
+     */
+    public StudySummaryDto setProgramDbId(final Integer programDbId) {
+        this.programDbId = programDbId;
+        return this;
+    }
+
+    /**
+     *
+     * @return The map with the optional info
+     */
+    @JsonAnyGetter
+    public Map<String, Object> getOptionalInfo() {
+        return optionalInfo;
+    }
+
+    /**
+     *
+     * @param name Key of the optional info
+     * @param value Value of the optional info
+     */
+    @JsonAnySetter
+    public void setOptionalInfo(final String name, final Object value) {
+        optionalInfo.put(name, value);
+    }
+
+    /**
+     *
+     * @param name Key of the optional info
+     * @param value Value of the optional info
+     * @return this
+     */
+    public StudySummaryDto addOptionalInfo(final String name, final Object value) {
+        optionalInfo.put(name, value);
+        return this;
+    }
+
+    /**
+     *
+     * @param optionalInfo
+     * @return this
+     */
+    public StudySummaryDto setOptionalInfo(final Map<String, Object> optionalInfo) {
+        this.optionalInfo = optionalInfo;
+        return this;
+    }
+
+    @Override
+    public int hashCode() {
+        return Pojomatic.hashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return Pojomatic.toString(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return Pojomatic.equals(this, o);
+    }
+
+}
+
---
+++ src/main/java/org/ibp/api/brapi/v1/study/StudySummaryResourceBrapi.java
@@ -0,0 +1,36 @@
+package org.ibp.api.brapi.v1.study;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import org.ibp.api.brapi.v1.location.Locations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * BMS implementation of the <a href="http://docs.brapi.apiary.io/">BrAPI</a> Study Summary services.
+ */
+@Api(value = "BrAPI Study Summary Services")
+@Controller
+public class StudySummaryResourceBrapi {
+
+    @ApiOperation(value = "List of study summaries", notes = "Get a list of study summaries.")
+    @RequestMapping(value = "/{crop}/brapi/v1/studies", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<StudySummariesDto> listLocations(@PathVariable final String crop,
+                                                           @ApiParam(value = "Program filter to only return studies associated with given program id.",
+                                                                   required = false) @RequestParam(value = "programDbId", required = false) Integer programDbId,
+                                                           @ApiParam(value = "Location filter to only return studies associated with given location id.",
+                                                                   required = false) @RequestParam(value = "locationDbId", required = false) Integer locationDbId,
+                                                           @ApiParam(value = "Season or year filter to only return studies associated with given season or year.",
+                                                                   required = false) @RequestParam(value = "seasonDbId", required = false) Integer seasonDbId,
+                                                           @ApiParam(value = "Page number to retrieve in case of multi paged results. Defaults to 1 (first page) if not supplied.",
+                                                                   required = false) @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                                           @ApiParam(value = "Number of results to retrieve per page. Defaults to 100 if not supplied. Max page size allowed is 200.",
+                                                                   required = false)
+                                                           @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+        return null;
+    }
+
+}
