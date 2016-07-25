e7deab8c3c497c1ac5ccee3a916574a1b56db626
Naymesh <naymesh@leafnode.io>
2016-07-25 17:01:05 -0400

    Minor name label change so that Swagger UI is presented properly.

--- src/main/java/org/ibp/api/brapi/v1/study/StudyResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v1/study/StudyResourceBrapi.java
@@ -1,8 +1,8 @@
 package org.ibp.api.brapi.v1.study;
 
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.generationcp.middleware.domain.dms.StudySummary;
 import org.generationcp.middleware.manager.api.StudyDataManager;
 import org.ibp.api.brapi.v1.common.Metadata;
@@ -18,15 +18,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 
 /**
- * BMS implementation of the <a href="http://docs.brapi.apiary.io/">BrAPI</a> Study Summary services.
+ * BMS implementation of the <a href="http://docs.brapi.apiary.io/">BrAPI</a> Study services.
  */
-@Api(value = "BrAPI Study Summary Services")
+@Api(value = "BrAPI Study Services")
 @Controller
 public class StudyResourceBrapi {
 
