0e84de46964aa19fde7f52c3905ab4c6105d9d47
Naymesh <naymesh@leafnode.io>
2016-09-09 11:52:50 +1200

    Cleaning up the old study services as they have moved to TrialResource now. Keeping placeholder methods with notes on what needs implementing. Keeping the stubs with "Not yet implemented" response in the mean time :)
    Issue: BMS-3125

--- src/main/java/org/ibp/api/brapi/v1/study/StudyResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v1/study/StudyResourceBrapi.java
@@ -1,19 +1,10 @@
 package org.ibp.api.brapi.v1.study;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.generationcp.middleware.domain.dms.StudySummary;
 import org.generationcp.middleware.manager.api.StudyDataManager;
 import org.ibp.api.brapi.v1.common.Metadata;
 import org.ibp.api.brapi.v1.common.Pagination;
 import org.ibp.api.brapi.v1.common.Result;
-import org.ibp.api.domain.common.PagedResult;
 import org.ibp.api.java.study.StudyService;
-import org.ibp.api.rest.common.PaginatedSearch;
-import org.ibp.api.rest.common.SearchSpec;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
@@ -36,9 +29,11 @@ import com.wordnik.swagger.annotations.ApiParam;
 @Controller
 public class StudyResourceBrapi {
 
+	@SuppressWarnings("unused") // temporary
 	@Autowired
 	private StudyDataManager studyDataManager;
 
+	@SuppressWarnings("unused") // temporary
 	@Autowired
 	private StudyService studyService;
 
@@ -46,84 +41,55 @@ public class StudyResourceBrapi {
 	@RequestMapping(value = "/{crop}/brapi/v1/studies", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<StudySummariesDto> listStudySummaries(@PathVariable final String crop,
-			@ApiParam(value = "Program filter to only return studies associated with given program id.", required = false) @RequestParam(value = "programDbId", required = false) final String programDbId,
-			@ApiParam(value = "Location filter to only return studies associated with given location id.", required = false) @RequestParam(value = "locationDbId", required = false) final String locationDbId,
-			@ApiParam(value = "Season or year filter to only return studies associated with given season or year.", required = false) @RequestParam(value = "seasonDbId", required = false) final String seasonDbId,
-			@ApiParam(value = "Page number to retrieve in case of multi paged results. Defaults to 1 (first page) if not supplied.", required = false) @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
-			@ApiParam(value = "Number of results to retrieve per page. Defaults to 100 if not supplied. Max page size allowed is 200.", required = false) @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+			@ApiParam(
+					value = "Studies are contained within a trial.  Provide the db id of the trial to list summary of studies within the trial. "
+							+ "Use <code>GET /{crop}/brapi/v1/trials</code> service to retrieve trial summaries first to obtain trialDbIds to supply here. ",
+					required = true) @RequestParam(value = "trialDbId", required = false) final String trialDbId,
+			@ApiParam(value = "Page number to retrieve in case of multi paged results. Defaults to 1 (first page) if not supplied.",
+					required = false) @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+			@ApiParam(value = "Number of results to retrieve per page. Defaults to 100 if not supplied. Max page size allowed is 200.",
+					required = false) @RequestParam(value = "pageSize", required = false) Integer pageSize) {
 
-		PagedResult<StudySummary> resultPage = new PaginatedSearch().execute(pageNumber, pageSize,
-				new SearchSpec<StudySummary>() {
+		/***
+		 * Study in BrAPI land = Environment/Instance in BMS/Middleware land. We need to build services in Middleware to list all
+		 * Environment/Instance metadata for a given trialDbId (required parameter). BrAPI does not yet have the trialDbId parameters but we
+		 * need it here so we are going to add it and implement anyway. In future we may support taking array of trial Ids to list studies
+		 * for. There is no point listing all instance/environment summaries across all trials.
+		 * 
+		 * studyDbId in BrAPI will map to nd_geolocation_id in Middleware.
+		 * 
+		 * For now, just returning an empty place holder message with status.
+		 */
 
-					@Override
-					public long getCount() {
-						return StudyResourceBrapi.this.studyDataManager.countAllStudies(programDbId, locationDbId,
-								seasonDbId);
-					}
-
-					@Override
-					public List<StudySummary> getResults(PagedResult<StudySummary> pagedResult) {
-						return StudyResourceBrapi.this.studyDataManager.findPagedProjects(programDbId, locationDbId,
-								seasonDbId, pagedResult.getPageSize(), pagedResult.getPageNumber());
-					}
-				});
-
-		List<StudySummaryDto> studies = new ArrayList<>();
-		PropertyMap<StudySummary, StudySummaryDto> studySummaryMapper = new PropertyMap<StudySummary, StudySummaryDto>() {
-			protected void configure() {
-				map(source.getLocationId(), destination.getLocationDbId());
-				map(source.getSeasons(), destination.getSeasons());
-				map(source.getYears(), destination.getYears());
-				map(source.getName(), destination.getStudyName());
-				map(source.getOptionalInfo(), destination.getOptionalInfo());
-				map(source.getProgramDbId(), destination.getProgramDbId());
-				map(source.getStudyDbid(), destination.getStudyDbId());
-				map(source.getType(), destination.getStudyType());
-			}
-		};
-
-		ModelMapper modelMapper = new ModelMapper();
-
-		modelMapper.addMappings(studySummaryMapper);
-
-		for (StudySummary mwStudy : resultPage.getPageResults()) {
-			StudySummaryDto studySummaryDto = modelMapper.map(mwStudy, StudySummaryDto.class);
-			studies.add(studySummaryDto);
-		}
-
-		Result<org.ibp.api.brapi.v1.study.StudySummaryDto> results = new Result<StudySummaryDto>().withData(studies);
-		Pagination pagination = new Pagination().withPageNumber(resultPage.getPageNumber())
-				.withPageSize(resultPage.getPageSize()).withTotalCount(resultPage.getTotalResults())
-				.withTotalPages(resultPage.getTotalPages());
-
-		Metadata metadata = new Metadata().withPagination(pagination);
+		Result<org.ibp.api.brapi.v1.study.StudySummaryDto> results = new Result<StudySummaryDto>();
+		Pagination pagination = new Pagination();
+		Metadata metadata =
+				new Metadata().withPagination(pagination)
+						.withStatus(Maps.newHashMap(ImmutableMap.of("message", "This call is not yet implemented.")));
 		StudySummariesDto studiesList = new StudySummariesDto().setMetadata(metadata).setResult(results);
 
 		return new ResponseEntity<>(studiesList, HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "List of study details", notes = "Get a list of the traits and values associated to a given study.")
+	@ApiOperation(value = "Get study observation details as table", notes = "Get study observation details as table")
 	@RequestMapping(value = "/{crop}/brapi/v1/studies/{studyDbId}/table", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<StudyDetailsDto> listStudyDetailsAsTable(@PathVariable final String crop,
+	public ResponseEntity<StudyDetailsDto> getStudyObservationsAsTable(@PathVariable final String crop,
 			@PathVariable final Integer studyDbId) {
 
+		/***
+		 * Study in BrAPI land = Environment/Instance in BMS/Middleware land. We need to build new services in Middleware to get
+		 * Environment/Instance level measurement details as table.
+		 * 
+		 * studyDbId in BrAPI will map to nd_geolocation_id in Middleware.
+		 * 
+		 * For now, just returning an empty place holder message with status.
+		 */
 		StudyDetailDto brapiStudyDetailDto = new StudyDetailDto();
 
-		org.generationcp.middleware.service.api.study.StudyDetailDto mwStudyDetailDto = this.studyService
-				.getStudyDetails(studyDbId);
-
-		int resultNumber = (mwStudyDetailDto == null) ? 0 : 1;
-
-		if (resultNumber != 0) {
-			ModelMapper modelMapper = new ModelMapper();
-			brapiStudyDetailDto = modelMapper.map(mwStudyDetailDto, StudyDetailDto.class);
-		}
-
-		Pagination pagination = new Pagination().withPageNumber(1).withPageSize(resultNumber)
-				.withTotalCount((long) resultNumber).withTotalPages(1);
-
-		Metadata metadata = new Metadata().withPagination(pagination);
+		Pagination pagination = new Pagination();
+		Metadata metadata = new Metadata().withPagination(pagination)
+				.withStatus(Maps.newHashMap(ImmutableMap.of("message", "This call is not yet implemented.")));
 		StudyDetailsDto studyDetailsDto = new StudyDetailsDto().setMetadata(metadata).setResult(brapiStudyDetailDto);
 		return new ResponseEntity<>(studyDetailsDto, HttpStatus.OK);
 	}
--- src/test/java/org/ibp/api/brapi/v1/study/StudyResourceBrapiTest.java
+++ src/test/java/org/ibp/api/brapi/v1/study/StudyResourceBrapiTest.java
@@ -1,168 +1,32 @@
 package org.ibp.api.brapi.v1.study;
 
-import static java.util.concurrent.ThreadLocalRandom.current;
-import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
-import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
-import static org.hamcrest.Matchers.hasKey;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Matchers.anyString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import java.util.List;
-
-import org.generationcp.middleware.domain.dms.StudySummary;
 import org.generationcp.middleware.manager.api.StudyDataManager;
-import org.generationcp.middleware.service.api.study.StudyDetailDto;
 import org.generationcp.middleware.service.api.study.StudyService;
 import org.ibp.ApiUnitTestBase;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import com.jayway.jsonassert.impl.matcher.IsCollectionWithSize;
 
 public class StudyResourceBrapiTest extends ApiUnitTestBase {
 
+	@SuppressWarnings("unused") // temporary
 	@Autowired
 	private StudyDataManager studyDataManager;
 
+	@SuppressWarnings("unused") // temporary
 	@Autowired
 	private StudyService studyServiceMW;
 
 	@Test
 	public void testListStudySummaries() throws Exception {
 
-		final int studyDbId = current().nextInt();
-		final String locationId = randomAlphabetic(5);
-		final String programDbId = randomAlphanumeric(5);
-		final String name = randomAlphabetic(5);
-		final String season = randomAlphabetic(5);
-		final String type = randomAlphabetic(5);
-		final String year = randomAlphabetic(5);
-		final String optionalInfoKey = randomAlphabetic(5);
-		final String optionalInfoValue = randomAlphabetic(5);
-
-		final StudySummary studySummary = new StudySummary().setName(name)
-				.setSeasons(ImmutableList.<String>builder().add(season).build()).setStudyDbid(studyDbId)
-				.setLocationId(locationId).setProgramDbId(programDbId).setType(type)
-				.setYears(ImmutableList.<String>builder().add(year).build()).setOptionalInfo(
-						ImmutableMap.<String, String>builder().put(optionalInfoKey, optionalInfoValue).build());
-
-		List<StudySummary> mwStudySummary = Lists.newArrayList(studySummary);
-		Mockito.when(this.studyDataManager.findPagedProjects(programDbId, String.valueOf(locationId), season, 10, 1))
-				.thenReturn(mwStudySummary);
-		Mockito.when(this.studyDataManager.countAllStudies(anyString(), anyString(), anyString())).thenReturn(200L);
-
-		UriComponents uriComponents = UriComponentsBuilder.newInstance().path("/maize/brapi/v1/studies")
-				.queryParam("programDbId", programDbId).queryParam("seasonDbId", season).queryParam("pageSize", 10)
-				.queryParam("pageNumber", 1).queryParam("locationDbId", String.valueOf(locationId)).build().encode();
-
-		this.mockMvc.perform(MockMvcRequestBuilders.get(uriComponents.toUriString()).contentType(this.contentType)) //
-				.andExpect(MockMvcResultMatchers.status().isOk()) //
-				.andDo(MockMvcResultHandlers.print()) //
-				.andExpect(jsonPath("$.result.data", IsCollectionWithSize.hasSize(mwStudySummary.size()))) //
-				.andExpect(jsonPath("$.result.data[0].studyDbId", is(studySummary.getStudyDbid()))) //
-				.andExpect(jsonPath("$.result.data[0].studyName", is(studySummary.getName()))) //
-				.andExpect(jsonPath("$.result.data[0].studyType", is(studySummary.getType()))) //
-				.andExpect(jsonPath("$.result.data[0].locationDbId", is(studySummary.getLocationId()))) //
-				.andExpect(jsonPath("$.result.data[0].programDbId", is(studySummary.getProgramDbId()))) //
-				.andExpect(jsonPath("$.result.data[0].years",
-						IsCollectionWithSize.hasSize(studySummary.getYears().size()))) //
-				.andExpect(jsonPath("$.result.data[0].years[0]", is(studySummary.getYears().get(0)))) //
-				.andExpect(jsonPath("$.result.data[0].seasons",
-						IsCollectionWithSize.hasSize(studySummary.getSeasons().size()))) //
-				.andExpect(jsonPath("$.result.data[0].seasons[0]", is(studySummary.getSeasons().get(0)))) //
-				.andExpect(jsonPath("$.result.data[0].optionalInfo", hasKey(optionalInfoKey)))
-				.andExpect(jsonPath("$.metadata.pagination.pageNumber", is(1))) //
-				.andExpect(jsonPath("$.metadata.pagination.pageSize", is(10))) //
-				.andExpect(jsonPath("$.metadata.pagination.totalCount", is(200))) //
-				.andExpect(jsonPath("$.metadata.pagination.totalPages", is(20))) //
-				.andExpect(jsonPath("$.metadata.status", is(IsCollectionWithSize.hasSize(0)))) //
-		;
-
+		// TODO with StudyResourceBrapi implementation
 	}
 
-	@Test
-	public void testListStudyDetailsAsTableNotNullResults() throws Exception {
-
-		final int studyDbId = current().nextInt();
-
-		final List<Integer> observationVariablesId = ImmutableList.<Integer>builder().add(current().nextInt()).build();
-
-		final List<String> observationVariableName = ImmutableList.<String>builder().add(randomAlphabetic(5)).build();
-
-		final List<List<String>> data = ImmutableList.<List<String>>builder()
-				.add(ImmutableList.<String>builder().add(randomAlphabetic(5)).add(randomAlphabetic(5))
-						.add(randomAlphabetic(5)).add(randomAlphabetic(5)).build())
-				.build();
-
-		StudyDetailDto mwStudyDetailDto = new StudyDetailDto().setStudyDbId(studyDbId)
-				.setObservationVariableDbIds(observationVariablesId).setObservationVariableNames(observationVariableName)
-				.setData(data);
-
-		Mockito.when(this.studyServiceMW.getStudyDetails(studyDbId)).thenReturn(mwStudyDetailDto);
-
-		UriComponents uriComponents = UriComponentsBuilder.newInstance()
-				.path("/maize/brapi/v1/studies/{studyDbId}/table")
-				.buildAndExpand(ImmutableMap.<String, Object>builder().put("studyDbId", studyDbId).build()).encode();
-
-		this.mockMvc.perform(MockMvcRequestBuilders.get(uriComponents.toUriString()).contentType(this.contentType)) //
-				.andExpect(MockMvcResultMatchers.status().isOk()) //
-				.andDo(MockMvcResultHandlers.print())
-				.andExpect(jsonPath("$.result.studyDbId", is(mwStudyDetailDto.getStudyDbId()))) //
-				.andExpect(jsonPath("$.result.observationVariableDbIds",
-						IsCollectionWithSize.hasSize(observationVariablesId.size()))) //
-				.andExpect(jsonPath("$.result.observationVariableDbIds[0]", is(observationVariablesId.get(0)))) //
-				.andExpect(jsonPath("$.result.observationVariableNames",
-						IsCollectionWithSize.hasSize(observationVariableName.size()))) //
-				.andExpect(jsonPath("$.result.observationVariableNames[0]", is(observationVariableName.get(0)))) //
-				.andExpect(jsonPath("$.result.data", IsCollectionWithSize.hasSize(data.size()))) //
-				.andExpect(jsonPath("$.result.data[0][0]", is(data.get(0).get(0)))) //
-				.andExpect(jsonPath("$.result.data[0][1]", is(data.get(0).get(1)))) //
-				.andExpect(jsonPath("$.result.data[0][2]", is(data.get(0).get(2)))) //
-				.andExpect(jsonPath("$.result.data[0][3]", is(data.get(0).get(3)))) //
-				.andExpect(jsonPath("$.metadata.pagination.pageNumber", is(1))) //
-				.andExpect(jsonPath("$.metadata.pagination.pageSize", is(1))) //
-				.andExpect(jsonPath("$.metadata.pagination.totalCount", is(1))) //
-				.andExpect(jsonPath("$.metadata.pagination.totalPages", is(1))) //
-				.andExpect(jsonPath("$.metadata.status", is(IsCollectionWithSize.hasSize(0))));
-	}
 
 	@Test
-	public void testListStudyDetailsAsTableNullResults() throws Exception {
+	public void testGetStudyObservationsAsTable() throws Exception {
 
-		final int studyDbId = current().nextInt();
-
-		StudyDetailDto mwStudyDetailDto = new StudyDetailDto().setStudyDbId(studyDbId)
-				.setObservationVariableDbIds(Lists.<Integer>newArrayList()).setObservationVariableNames(Lists.<String>newArrayList())
-				.setData(Lists.<List<String>>newArrayList());
-
-		Mockito.when(this.studyServiceMW.getStudyDetails(studyDbId)).thenReturn(mwStudyDetailDto);
-
-		UriComponents uriComponents = UriComponentsBuilder.newInstance()
-				.path("/maize/brapi/v1/studies/{studyDbId}/table")
-				.buildAndExpand(ImmutableMap.<String, Object>builder().put("studyDbId", studyDbId).build()).encode();
-
-		this.mockMvc.perform(MockMvcRequestBuilders.get(uriComponents.toUriString()).contentType(this.contentType)) //
-				.andExpect(MockMvcResultMatchers.status().isOk()) //
-				.andDo(MockMvcResultHandlers.print())
-				.andExpect(jsonPath("$.result.studyDbId", is(mwStudyDetailDto.getStudyDbId()))) //
-				.andExpect(jsonPath("$.result.observationVariableDbIds", IsCollectionWithSize.hasSize(0))) //
-				.andExpect(jsonPath("$.result.observationVariableNames", IsCollectionWithSize.hasSize(0))) //
-				.andExpect(jsonPath("$.result.data", IsCollectionWithSize.hasSize(0))) //
-				.andExpect(jsonPath("$.metadata.pagination.pageNumber", is(1))) //
-				.andExpect(jsonPath("$.metadata.pagination.pageSize", is(1))) //
-				.andExpect(jsonPath("$.metadata.pagination.totalCount", is(1))) //
-				.andExpect(jsonPath("$.metadata.pagination.totalPages", is(1))) //
-				.andExpect(jsonPath("$.metadata.status", is(IsCollectionWithSize.hasSize(0))));
+		// TODO with StudyResourceBrapi implementation
 	}
 
 }
