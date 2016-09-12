59b63a627281cca4c1367f7d3f00d7739a256d15
Vanina Maletta <vmaletta@droptek.com>
2016-09-12 17:05:55 -0300

    Move the API

--- src/main/java/org/ibp/api/java/user/UsersApi.java
+++ src/main/java/org/ibp/api/java/user/UsersApi.java
@@ -6,21 +6,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
-@RestController
-@RequestMapping("/users")
+@Api(value = "BrAPI User Services")
+@Controller
 public class UsersApi {
 
 	@Autowired
 	private UserService userService;
 
 	@ApiOperation(value = "List all users", notes = "List all users in this deployment instance of BMSAPI. ")
-	@RequestMapping(value = "/listUsers", method = RequestMethod.GET)
+	@RequestMapping(value = "/brapi/v1/users", method = RequestMethod.GET)
+	@ResponseBody
 	public ResponseEntity<List<UserData>> listUsers() {
 		return new ResponseEntity<List<UserData>>(this.userService.getAllUserDtosSorted(), HttpStatus.OK);
 	}
