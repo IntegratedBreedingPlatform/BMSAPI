0e98af6143f0c7f15c2a79638742f6fc613d39a7
Clarysabel Tovar <clarysabel@leafnode.io>
2016-10-18 10:40:25 -0300

    Adding Category Issue: BMS-3416 Reviewer: Nahuel Soldevilla

---
+++ src/main/java/org/ibp/api/domain/ontology/Category.java
@@ -0,0 +1,46 @@
+package org.ibp.api.domain.ontology;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Category extends TermSummary {
+
+	private boolean editable;
+
+	Category() {
+		super();
+		this.editable = Boolean.TRUE;
+	}
+
+	public boolean isEditable() {
+		return editable;
+	}
+
+	public void setEditable(final boolean editable) {
+		this.editable = editable;
+	}
+
+	@Override public String toString() {
+		return "TermSummary{" + "id='" + super.getId() + '\'' + ", name='" + super.getName() + '\'' + ", description='" + super
+				.getDescription() + '\'' + ", editable='" + this.isEditable() + '\'' + '}';
+	}
+
+	@Override public boolean equals(final Object other) {
+		if (!(other instanceof Category)) {
+			return false;
+		}
+		Category castOther = (Category) other;
+		return new EqualsBuilder().append(super.getId(), castOther.getId()).append(super.getName(), castOther.getName())
+				.append(super.getDescription(), castOther.getDescription()).append(this.isEditable(), castOther.isEditable()).isEquals();
+	}
+
+	@Override public int hashCode() {
+		return new HashCodeBuilder().append(super.getId()).append(super.getName()).append(super.getDescription()).append(this.isEditable())
+				.toHashCode();
+	}
+
+}
