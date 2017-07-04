22c39c2cb7aad3d919918ff44485646da3718613
cuenyad <cuenya.diego@gmail.com>
2017-07-04 13:44:40 -0300

    Test modified
    ISSUE:BMS-4261
    Reviewer:Nahuel

--- src/test/java/org/ibp/api/brapi/v1/location/LocationResourceBrapiTest.java
+++ src/test/java/org/ibp/api/brapi/v1/location/LocationResourceBrapiTest.java
@@ -15,6 +15,7 @@ import org.generationcp.middleware.service.api.location.LocationDetailsDto;
 import org.generationcp.middleware.service.api.location.LocationFilters;
 import org.hamcrest.Matchers;
 import org.ibp.ApiUnitTestBase;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -100,32 +101,37 @@ public class LocationResourceBrapiTest extends ApiUnitTestBase {
 	@Test
 	public void testListLocationsWithAdditionalInfo() throws Exception {
 
-		String locType = "Breeding Location";
-		LocationDetailsDto location1 = new LocationDetailsDto(156,locType,"New Zealand","NZL","NZL","NZ",156.2,58.6,5.2);
-		LocationDetailsDto location2 = new LocationDetailsDto(100,locType,"Argentina","ARG"," "," ",-34.7108688,-58.280082,24.000);
+		final String locType = "Breeding Location";
+		final LocationDetailsDto location1 = new LocationDetailsDto(156,locType,"New Zealand","NZL","NZL","NZ",156.2,58.6,5.2);
+		final LocationDetailsDto location2 = new LocationDetailsDto(100,locType,"Argentina","ARG","ARG","ARG",-34.7108688,-58.280082,24.000);
 
 		AdditionalInfoDto additionalInfoDto = new AdditionalInfoDto(156);
 		additionalInfoDto.addInfo("province","Auckland");
 		location1.setMapAdditionalInfo(additionalInfoDto);
 
 		additionalInfoDto = new AdditionalInfoDto(100);
-		additionalInfoDto.addInfo("province","Bernal");
-		location1.setMapAdditionalInfo(additionalInfoDto);
+		additionalInfoDto.addInfo("province","Buenos Aires");
+		location2.setMapAdditionalInfo(additionalInfoDto);
+
+		final Integer locationTypeId = 410;
+		final UserDefinedField locTypeUDFLD = new UserDefinedField(locationTypeId);
+		locTypeUDFLD.setFname(locType);
 		
 		final HashMap<Integer, AdditionalInfoDto> mapAdditionalInfo = new HashMap<>();
 		mapAdditionalInfo.put(100, additionalInfoDto);
 		List<LocationDetailsDto> mwLocations = Lists.newArrayList(location1, location2);
-		Mockito.when(this.locationDataManager.getUserDefinedFieldIdOfName(org.generationcp.middleware.pojos.UDTableType.LOCATION_LTYPE,Mockito.anyString())).thenReturn(Integer.valueOf(410));
+		Mockito.when(this.locationDataManager.getUserDefinedFieldByID(locationTypeId)).thenReturn(locTypeUDFLD);
+
 		Mockito.when(this.locationDataManager.getLocationsByFilter(Mockito.anyInt(), Mockito.anyInt(),
 				Mockito.anyMapOf(LocationFilters.class, Object.class))).thenReturn(mwLocations);
 		Mockito.when(this.locationDataManager.countLocationsByFilter(Mockito.anyMapOf(LocationFilters.class, Object.class))).thenReturn(1L);
 	
-		this.mockMvc.perform(MockMvcRequestBuilders.get("/maize/brapi/v1/locations?pageNumber=1&pageSize=10&locationType=Country").contentType(this.contentType)) //
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/maize/brapi/v1/locations?pageNumber=1&pageSize=10&locationType="+locType).contentType(this.contentType)) //
 				.andExpect(MockMvcResultMatchers.status().isOk()) //
 				.andDo(MockMvcResultHandlers.print()) //
 				.andExpect(jsonPath("$.result.data", IsCollectionWithSize.hasSize(mwLocations.size()))) //
 				.andExpect(jsonPath("$.result.data[0].locationDbId", Matchers.is(location1.getLocationDbId()))) //
-				.andExpect(jsonPath("$.result.data[0].locationType", Matchers.is(locType))) //
+				.andExpect(jsonPath("$.result.data[0].locationType", Matchers.is(location1.getLocationType()))) //
 				.andExpect(jsonPath("$.result.data[0].name", Matchers.is(location1.getName()))) //
 				.andExpect(jsonPath("$.result.data[0].abbreviation", Matchers.is(location1.getAbbreviation()))) //
 				.andExpect(jsonPath("$.result.data[0].countryCode", Matchers.is(location1.getCountryCode()))) //
@@ -134,21 +140,16 @@ public class LocationResourceBrapiTest extends ApiUnitTestBase {
 				.andExpect(jsonPath("$.result.data[0].longitude", Matchers.is(location1.getLongitude()))) //
 				.andExpect(jsonPath("$.result.data[0].altitude", Matchers.is(location1.getAltitude()))) //
 				.andExpect(jsonPath("$.result.data[1].locationDbId", Matchers.is(location2.getLocationDbId()))) //
-				.andExpect(jsonPath("$.result.data[1].locationType", Matchers.is(""))) //
+				.andExpect(jsonPath("$.result.data[1].locationType", Matchers.is(location2.getLocationType()))) //
 				.andExpect(jsonPath("$.result.data[1].name", Matchers.is(location2.getName()))) //
 				.andExpect(jsonPath("$.result.data[1].abbreviation", Matchers.is(location2.getAbbreviation()))) //
-				.andExpect(jsonPath("$.result.data[1].countryCode", Matchers.is(""))) //
-				.andExpect(jsonPath("$.result.data[1].countryName", Matchers.is("Unknown"))) //
+				.andExpect(jsonPath("$.result.data[1].countryCode", Matchers.is(location2.getCountryCode()))) //
+				.andExpect(jsonPath("$.result.data[1].countryName", Matchers.is(location2.getCountryName()))) //
 				.andExpect(jsonPath("$.result.data[1].latitude", Matchers.is(location2.getLatitude()))) //
 				.andExpect(jsonPath("$.result.data[1].longitude", Matchers.is(location2.getLongitude()))) //
 				.andExpect(jsonPath("$.result.data[1].altitude", Matchers.is(location2.getAltitude()))) //
-				
-				/*.andExpect(jsonPath("$.result.data[1].additionalInfo.FIELD_PARENT", Matchers.is("FiledParent"))) //
-				.andExpect(jsonPath("$.result.data[1].additionalInfo.BLOCK_PARENT", Matchers.is("BlockParent"))) //
-				.andExpect(jsonPath("$.result.data[1].additionalInfo.COL_IN_BLK", Matchers.is("1"))) //
-				.andExpect(jsonPath("$.result.data[1].additionalInfo.RANGE_IN_BLK", Matchers.is("2"))) //
-				.andExpect(jsonPath("$.result.data[1].additionalInfo.PLOT_LYOUT", Matchers.is("1"))) //
-				*/
+				.andExpect(jsonPath("$.result.data[0].additionalInfo.province", Matchers.is(location1.getAdditionalInfo().getInfoValue("province")))) //
+				.andExpect(jsonPath("$.result.data[1].additionalInfo.province", Matchers.is(location2.getAdditionalInfo().getInfoValue("province")))) //
 				.andExpect(jsonPath("$.metadata.pagination.pageNumber", Matchers.is(1))) //
 				.andExpect(jsonPath("$.metadata.pagination.pageSize", Matchers.is(10))) //
 				.andExpect(jsonPath("$.metadata.pagination.totalCount", Matchers.is(1))) //
