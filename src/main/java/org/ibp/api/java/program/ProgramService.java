e90d23b87e68e0f1ae8116e7c6d8d56e463f0496
cuenyad <cuenya.diego@gmail.com>
2017-07-06 15:55:53 -0300

    I Created queries for the new Api Programs
    ISSUE:BMS-4258
    Reviewer:Nahuel

--- src/main/java/org/ibp/api/java/program/ProgramService.java
+++ src/main/java/org/ibp/api/java/program/ProgramService.java
@@ -2,10 +2,17 @@
 package org.ibp.api.java.program;
 
 import java.util.List;
+import java.util.Map;
 
+import org.generationcp.middleware.service.api.program.ProgramDetailsDto;
+import org.generationcp.middleware.service.api.program.ProgramFilters;
 import org.ibp.api.domain.program.ProgramSummary;
 
 public interface ProgramService {
 
 	List<ProgramSummary> listAllPrograms();
+
+	List<ProgramDetailsDto> getProgramsByFilter(final int pageNumber, final int pageSize, final Map<ProgramFilters, Object> filters);
+
+	long countProgramsByFilter(final Map<ProgramFilters, Object> filter);
 }
