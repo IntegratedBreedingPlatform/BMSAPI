6371e47721a1b84726ab33c17488c3e5be5adf72
cuenyad <cuenya.diego@gmail.com>
2017-07-11 16:05:43 -0300

    I did the modification about the ticket and refactored the code too.
    ISSUE:BMS-4267
    Reviewer:Nahuel

--- src/main/java/org/ibp/api/brapi/v1/trial/TrialResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v1/trial/TrialResourceBrapi.java
@@ -2,11 +2,16 @@
 package org.ibp.api.brapi.v1.trial;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.google.common.collect.ImmutableList;
+import org.apache.commons.lang3.StringUtils;
 import org.generationcp.middleware.dao.dms.InstanceMetadata;
 import org.generationcp.middleware.domain.dms.StudySummary;
-import org.generationcp.middleware.manager.api.StudyDataManager;
+import org.generationcp.middleware.service.api.study.StudyFilters;
 import org.generationcp.middleware.service.api.study.TrialObservationTable;
 import org.ibp.api.brapi.v1.common.Metadata;
 import org.ibp.api.brapi.v1.common.Pagination;
@@ -40,9 +45,6 @@ import com.wordnik.swagger.annotations.ApiParam;
 public class TrialResourceBrapi {
 
 	@Autowired
-	private StudyDataManager studyDataManager;
-
-	@Autowired
 	private StudyService studyService;
 
 	@ApiOperation(value = "List of trial summaries", notes = "Get a list of trial summaries.")
@@ -53,28 +55,48 @@ public class TrialResourceBrapi {
 			@ApiParam(value = "Location filter to only return studies associated with given location id.", required = false) @RequestParam(value = "locationDbId", required = false) final String locationDbId,
 			@ApiParam(value = "Page number to retrieve in case of multi paged results. Defaults to 1 (first page) if not supplied.", required = false) @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
 			@ApiParam(value = "Number of results to retrieve per page. Defaults to 100 if not supplied. Max page size allowed is 200.", required = false) @RequestParam(value = "pageSize", required = false) Integer pageSize,
-			@ApiParam(value = "Filter active status true/false", required = false) @RequestParam(value = "active", required = false) Boolean active,
-			@ApiParam(value = "Sort order. Name of the field to sorty by.", required = false) @RequestParam(value = "sortBy", required = false) Boolean sortBy,
-			@ApiParam(value = "Sort order direction. Ascending/Descending.", required = false) @RequestParam(value = "sortOrder", required = false) Boolean sortOrder) {
+			@ApiParam(value = "Filter active status true/false", required = false) @RequestParam(value = "active", required = false) final Boolean active,
+			@ApiParam(value = "Sort order. Name of the field to sorty by.", required = false) @RequestParam(value = "sortBy", required = false) final String sortBy,
+			@ApiParam(value = "Sort order direction. Ascending/Descending.", required = false) @RequestParam(value = "sortOrder", required = false) final String sortOrder) {
 
-		final PagedResult<StudySummary> resultPage = new PaginatedSearch().execute(pageNumber, pageSize,
-				new SearchSpec<StudySummary>() {
+		final String validationError = parameterValidation(active, sortBy, sortOrder);
+		if (!StringUtils.isBlank(validationError)) {
+			Map<String, String> status = new HashMap<>();
+			status.put("message", validationError);
+			Metadata metadata = new Metadata(null, status);
+			TrialSummaries trialSummaries = new TrialSummaries().withMetadata(metadata).withResult(new Result<TrialSummary>());
+			return new ResponseEntity<>(trialSummaries, HttpStatus.NOT_FOUND);
+		}
 
-					@Override
-					public long getCount() {
-						return TrialResourceBrapi.this.studyDataManager.countAllStudies(programDbId, locationDbId,
-								null);
-					}
+		final EnumMap<StudyFilters, String> parameters = setParameter(programDbId, locationDbId, sortBy, sortOrder);
+		final PagedResult<StudySummary> resultPage = new PaginatedSearch().execute(pageNumber, pageSize, new SearchSpec<StudySummary>() {
 
-					@Override
-					public List<StudySummary> getResults(PagedResult<StudySummary> pagedResult) {
-						return TrialResourceBrapi.this.studyDataManager.findPagedProjects(programDbId, locationDbId,
-								null, pagedResult.getPageSize(), pagedResult.getPageNumber());
-					}
-				});
+			@Override
+			public long getCount() {
+				return TrialResourceBrapi.this.studyService.countStudies(parameters);
+			}
 
-		final List<TrialSummary> trialSummaryList = new ArrayList<>();
+			@Override
+			public List<StudySummary> getResults(PagedResult<StudySummary> pagedResult) {
+				return TrialResourceBrapi.this.studyService.getStudies(parameters, pagedResult.getPageSize(), pagedResult.getPageNumber());
+			}
+		});
+
+		final List<TrialSummary> trialSummaryList = translatedResults(resultPage);
+		final Result<TrialSummary> results = new Result<TrialSummary>().withData(trialSummaryList);
+		final Pagination pagination = new Pagination().withPageNumber(resultPage.getPageNumber()).withPageSize(resultPage.getPageSize())
+			.withTotalCount(resultPage.getTotalResults()).withTotalPages(resultPage.getTotalPages());
+
+		final Metadata metadata = new Metadata().withPagination(pagination);
+		final TrialSummaries trialSummaries = new TrialSummaries().withMetadata(metadata).withResult(results);
+
+		return new ResponseEntity<>(trialSummaries, HttpStatus.OK);
+
+	}
+
+	private List<TrialSummary> translatedResults(PagedResult<StudySummary> resultPage) {
 		final ModelMapper modelMapper = TrialMapper.getInstance();
+		final List<TrialSummary> trialSummaryList = new ArrayList<>();
 
 		for (final StudySummary mwStudy : resultPage.getPageResults()) {
 			final TrialSummary trialSummaryDto = modelMapper.map(mwStudy, TrialSummary.class);
@@ -88,16 +110,88 @@ public class TrialResourceBrapi {
 			}
 			trialSummaryList.add(trialSummaryDto);
 		}
+		return trialSummaryList;
+	}
 
-		final Result<TrialSummary> results = new Result<TrialSummary>().withData(trialSummaryList);
-		final Pagination pagination = new Pagination().withPageNumber(resultPage.getPageNumber()).withPageSize(resultPage.getPageSize())
-				.withTotalCount(resultPage.getTotalResults()).withTotalPages(resultPage.getTotalPages());
+	private EnumMap<StudyFilters, String> setParameter(final String programDbId, final String locationDbId, final String sortByField,
+		final String sortOrder) {
 
-		final Metadata metadata = new Metadata().withPagination(pagination);
-		final TrialSummaries trialSummaries = new TrialSummaries().withMetadata(metadata).withResult(results);
+		final EnumMap<StudyFilters, String> parametersMap = new EnumMap<>(StudyFilters.class);
+		if (!StringUtils.isBlank(programDbId)) {
+			parametersMap.put(StudyFilters.PROGRAM_ID, programDbId);
+		}
+		if (!StringUtils.isBlank(locationDbId)) {
+			parametersMap.put(StudyFilters.LOCATION_ID, locationDbId);
+		}
 
-		return new ResponseEntity<>(trialSummaries, HttpStatus.OK);
+		if (!StringUtils.isBlank(sortByField)) {
+			parametersMap.put(StudyFilters.SORT_BY_FIELD, translateNameField(sortByField));
+		} else {
+			parametersMap.put(StudyFilters.SORT_BY_FIELD, "projectId");
 
+		}
+		if (StringUtils.isBlank(sortOrder) || "Ascending".equalsIgnoreCase(sortOrder)) {
+			parametersMap.put(StudyFilters.ORDER, "asc");
+		} else if (!StringUtils.isBlank(sortOrder) && "Descending".equalsIgnoreCase(sortOrder)) {
+			parametersMap.put(StudyFilters.ORDER, "desc");
+		}
+		return parametersMap;
+	}
+
+	private String translateNameField(final String sortByField) {
+		final String nameField;
+		switch (sortByField) {
+			case "programDbId":
+				nameField = "programUUID";
+				break;
+			case "trialName":
+				nameField = "name";
+				break;
+			case "trialDbId":
+				nameField = "projectId";
+				break;
+			default:
+				nameField = "programName";
+				break;
+		}
+		return nameField;
+	}
+
+	private String parameterValidation(final Boolean active, final String sortBy, final String sortOrder) {
+		final List<String> sortbyFields =
+			ImmutableList.<String>builder().add("trialDbId").add("trialName").add("programDbId").add("programName").build();
+		final List<String> sortOrders = ImmutableList.<String>builder().add("Ascending").add("Descending").build();
+
+		if (active != null && !active) {
+			return gerMessageError(1);
+		}
+		if (!StringUtils.isBlank(sortBy) && !sortbyFields.contains(sortBy)) {
+			return gerMessageError(2);
+
+		}
+		if (!StringUtils.isBlank(sortOrder) && !sortOrders.contains(sortOrder)) {
+			return gerMessageError(3);
+		}
+		return "";
+	}
+
+	private String gerMessageError(final Integer messageValue) {
+		final String messageError;
+		switch (messageValue) {
+			case 1:
+				messageError = "not found inactive studies";
+				break;
+			case 2:
+				messageError = "sortBy bad filter, expect trialDbId/trialName/programDbId/programName";
+				break;
+			case 3:
+				messageError = "sortOrder bad filter, expect Ascending/Descending";
+				break;
+			default:
+				messageError = "";
+				break;
+		}
+		return messageError;
 	}
 
 	@ApiOperation(value = "Get trial observation details as table", notes = "Get trial observation details as table")
