27d3a911d12fbc0464f0233b5c594f16ace668ce
Vanina Maletta <vmaletta@droptek.com>
2017-07-13 11:16:09 -0300

    File download research Issue: BMS-4260 Reviewer: Nahuel

--- src/main/java/org/ibp/api/brapi/v1/study/StudyResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v1/study/StudyResourceBrapi.java
@@ -1,5 +1,15 @@
 package org.ibp.api.brapi.v1.study;
 
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.common.net.MediaType;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import org.generationcp.commons.util.FileUtils;
 import org.generationcp.middleware.manager.api.LocationDataManager;
 import org.generationcp.middleware.manager.api.StudyDataManager;
 import org.generationcp.middleware.service.api.location.LocationDetailsDto;
@@ -14,8 +24,12 @@ import org.ibp.api.brapi.v1.location.LocationMapper;
 import org.ibp.api.java.study.StudyService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +37,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
-
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,6 +55,12 @@ import java.util.Map;
 @Api(value = "BrAPI Study Services")
 @Controller
 public class StudyResourceBrapi {
+
+	public static final String CSV = "csv";
+	public static final String TSV = "tsv";
+
+	public static final String CONTENT_TYPE = "Content-Type";
+	public static final String CONTENT_DISPOSITION = "Content-Disposition";
 
 	@SuppressWarnings("unused") // temporary
 	@Autowired
@@ -86,11 +107,26 @@ public class StudyResourceBrapi {
 	}
 
 	@ApiOperation(value = "Get study observation details as table", notes = "Get study observation details as table")
-	@RequestMapping(value = "/{crop}/brapi/v1/studies/{studyDbId}/table", method = RequestMethod.GET)
-	@ResponseBody
-	public ResponseEntity<StudyObservations> getStudyObservationsAsTable(@PathVariable final String crop,
-			@PathVariable final int studyDbId) throws Exception {
+	@RequestMapping(value = "/{crop}/brapi/v1/studies/{studyDbId}/table", method = RequestMethod.GET) @ResponseBody
+	public ResponseEntity<StudyObservations> getStudyObservationsAsTable(HttpServletResponse response, @PathVariable final String crop,
+		@PathVariable final int studyDbId,
+		@ApiParam(value = "The format parameter will cause the data to be dumped to a file in the specified format", required = false)
+		@RequestParam(value = "format", required = false) final String format) throws Exception {
 
+		if (CSV.equals(format)) {
+
+			response.sendRedirect("/bmsapi/" + crop + "/brapi/v1/" + studyDbId + "/table/csv");
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else if (TSV.equals(format)) {
+
+			response.sendRedirect("/bmsapi/" + crop + "/brapi/v1/" + studyDbId + "/table/tsv");
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+
+		return new ResponseEntity<>(this.getStudyObservations(studyDbId), HttpStatus.OK);
+	}
+
+	private StudyObservations getStudyObservations(int studyDbId) throws Exception {
 		StudyObservationTable studyObservationsTable = new StudyObservationTable();
 
 		Integer trialDbId = this.studyDataManager.getProjectIdByStudyDbId(studyDbId);
@@ -109,11 +145,12 @@ public class StudyResourceBrapi {
 		}
 
 		Pagination pagination =
-				new Pagination().withPageNumber(1).withPageSize(resultNumber).withTotalCount((long) resultNumber).withTotalPages(1);
+			new Pagination().withPageNumber(1).withPageSize(resultNumber).withTotalCount((long) resultNumber).withTotalPages(1);
 
 		Metadata metadata = new Metadata().withPagination(pagination);
 		StudyObservations studyObservations = new StudyObservations().setMetadata(metadata).setResult(studyObservationsTable);
-		return new ResponseEntity<>(studyObservations, HttpStatus.OK);
+
+		return studyObservations;
 	}
 
 	@ApiOperation(value = "Get study details", notes = "Get study details")
@@ -151,5 +188,75 @@ public class StudyResourceBrapi {
 
 	}
 
+	@RequestMapping(value = "/{crop}/brapi/v1/{studyDbId}/table/csv", method = RequestMethod.GET)
+	public ResponseEntity<FileSystemResource> streamCSV(@PathVariable final String crop, @PathVariable final Integer studyDbId) throws Exception {
 
+		File file = createDownloadFile(this.getStudyObservations(studyDbId).getResult(), ',', "studyObservations.csv");
+		final String filename = file.getName();
+		final String absoluteLocation = file.getAbsolutePath();
+
+		return this.createResponseEntityForFileDownload(absoluteLocation, filename);
+	}
+
+
+	/**
+	 * Creates ResponseEntity to download a file from a controller.
+	 *
+	 * @param fileWithFullPath  - path of the file to be downloaded
+	 * @param filename  - the filename that will be set in the http response header
+	 * @return
+	 */
+	public static ResponseEntity<FileSystemResource> createResponseEntityForFileDownload(String fileWithFullPath, String filename) throws
+		UnsupportedEncodingException {
+		final HttpHeaders respHeaders = new HttpHeaders();
+
+		final File resource = new File(fileWithFullPath);
+		final FileSystemResource fileSystemResource = new FileSystemResource(resource);
+
+		final String mimeType = FileUtils.detectMimeType(filename);
+		final String sanitizedFilename = FileUtils.sanitizeFileName(filename);
+
+		respHeaders.set(CONTENT_TYPE,String.format("%s;charset=utf-8",mimeType));
+		respHeaders.set(CONTENT_DISPOSITION, String.format("attachment; filename=\"%s\"; filename*=utf-8\'\'%s", sanitizedFilename, FileUtils.encodeFilenameForDownload(sanitizedFilename)));
+
+		return new ResponseEntity<>(fileSystemResource, respHeaders, HttpStatus.OK);
+
+	}
+
+	private File createDownloadFile(StudyObservationTable table, char sep, String pathname) throws IOException {
+		// create mapper and schema
+		CsvMapper mapper = new CsvMapper();
+		CsvSchema schema = mapper.schemaFor(List.class);
+		schema = schema.withColumnSeparator(sep);
+
+		// output writer
+		ObjectWriter myObjectWriter = mapper.writer(schema);
+		File resultFile = new File(pathname);
+		String header = "";
+
+		for (String headerName : table.getHeaderRow()) {
+			header = header + headerName + sep;
+		}
+
+		Object[] variableIds = table.getObservationVariableDbIds().toArray();
+		Object[] variableNames = table.getObservationVariableNames().toArray();
+		for (int i = 0; i < variableIds.length; i++) {
+			header = header + variableNames[i] + '|' + variableIds[i] + sep;
+		}
+
+		header = header.substring(0, header.length() - 1);
+		mapper.writeValue(resultFile, header);
+		mapper.writeValue(resultFile, table.getData());
+
+		FileOutputStream tempFileOutputStream = new FileOutputStream(resultFile);
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(tempFileOutputStream, 1024);
+		OutputStreamWriter writerOutputStream = new OutputStreamWriter(bufferedOutputStream, "UTF-8");
+		myObjectWriter.writeValue(writerOutputStream, table.getData());
+		return resultFile;
+	}
+
+	@RequestMapping(value = "/{crop}/brapi/v1/{studyDbId}/table/tsv", method = RequestMethod.GET)
+	public void streamTSV(@PathVariable final String crop, @PathVariable final Integer studyDbId) throws Exception {
+		createDownloadFile(this.getStudyObservations(studyDbId).getResult(), '\t', "studyObservations.tsv");
+	}
 }
