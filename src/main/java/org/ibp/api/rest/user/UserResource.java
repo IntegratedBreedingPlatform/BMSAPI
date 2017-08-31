ad6668ec9675878713fb091a0563c112257c4fed
cuenyad <cuenya.diego@gmail.com>
2017-08-31 09:41:35 -0300

    I did the modification to return messages error.
    Issue: BMS-4341
    Reviewer: Nahuel

--- src/main/java/org/ibp/api/rest/user/UserResource.java
+++ src/main/java/org/ibp/api/rest/user/UserResource.java
@@ -1,7 +1,8 @@
 package org.ibp.api.rest.user;
 
 import com.wordnik.swagger.annotations.ApiOperation;
-import org.ibp.api.brapi.v1.user.UserService;
+import org.ibp.api.java.impl.middleware.user.UserDetailDto;
+import org.ibp.api.java.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @Controller
@@ -20,14 +21,9 @@ public class UserResource {
 	@Autowired
 	private UserService userService;
 
-	@ApiOperation(value = "List all users of one project", notes = "List all users")
+	@ApiOperation(value = "List all users of one projectUUID", notes = "List all users")
 	@RequestMapping(value = "/user/list", method = RequestMethod.GET)
-	public ResponseEntity<Map<String, Object>> list(@RequestParam(value = "projectUUID") final String projectUUID) {
-
-		final Map<String, Object> mapResults = this.userService.getUsersByProjectUUID(projectUUID);
-		if (mapResults.get("ERROR") != null) {
-			return new ResponseEntity<>(mapResults, HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity<>(mapResults, HttpStatus.OK);
+	public ResponseEntity<List<UserDetailDto>> list(@RequestParam(value = "projectUUID") final String projectUUID) {
+		return new ResponseEntity<>(this.userService.getUsersByProjectUUID(projectUUID), HttpStatus.OK);
 	}
 }
