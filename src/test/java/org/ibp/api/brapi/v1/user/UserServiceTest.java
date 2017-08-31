19ca593b0e885354e97b044f321bcef13cc89e94
cuenyad <cuenya.diego@gmail.com>
2017-08-31 09:45:47 -0300

    implemented UserTestDataGenerator to refactor test.
    Issue: BMS-4341
    Reviewer: Nahuel

--- src/test/java/org/ibp/api/brapi/v1/user/UserServiceTest.java
+++ src/test/java/org/ibp/api/brapi/v1/user/UserServiceTest.java
@@ -1,15 +1,16 @@
 
 package org.ibp.api.brapi.v1.user;
 
-import com.google.common.collect.Lists;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.manager.api.WorkbenchDataManager;
-import org.generationcp.middleware.pojos.Person;
 import org.generationcp.middleware.pojos.User;
 import org.generationcp.middleware.service.api.user.UserDto;
 import org.ibp.api.domain.common.ErrorResponse;
+import org.ibp.api.exception.ApiRuntimeException;
+import org.ibp.api.java.impl.middleware.UserTestDataGenerator;
 import org.ibp.api.java.impl.middleware.manager.UserValidator;
+import org.ibp.api.java.impl.middleware.user.UserDetailDto;
+import org.ibp.api.java.impl.middleware.user.UserServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -54,7 +55,7 @@ public class UserServiceTest {
 	 */
 	@Test
 	public void testGetAllUsers() throws Exception {
-		final List<UserDto> usersDto = getAllListUser();
+		final List<UserDto> usersDto = UserTestDataGenerator.getAllListUser();
 
 		Mockito.when(this.workbenchDataManager.getAllUsersSortedByLastName()).thenReturn(usersDto);
 		final List<UserDetailDto> usersDtlsDto = this.userServiceImpl.getAllUsersSortedByLastName();
@@ -75,8 +76,8 @@ public class UserServiceTest {
 	 */
 	@Test
 	public void testCreateUser() throws Exception {
-		final UserDetailDto usrDtlDto = initializeUserDetailDto(0);
-		final UserDto userDto = initializeUserDto(0);
+		final UserDetailDto usrDtlDto = UserTestDataGenerator.initializeUserDetailDto(0);
+		final UserDto userDto = UserTestDataGenerator.initializeUserDto(0);
 		
 		Mockito.when(this.workbenchDataManager.createUser(userDto)).thenReturn(new Integer(7));
 		
@@ -91,8 +92,8 @@ public class UserServiceTest {
 	 */
 	@Test
 	public void testCreateUserError() throws Exception {
-		final UserDetailDto usrDtlDto = initializeUserDetailDto(0);
-		final UserDto userDto = initializeUserDto(0);
+		final UserDetailDto usrDtlDto = UserTestDataGenerator.initializeUserDetailDto(0);
+		final UserDto userDto = UserTestDataGenerator.initializeUserDto(0);
 
 		Mockito.when(this.workbenchDataManager.isUsernameExists(usrDtlDto.getUsername())).thenReturn(false);
 		Mockito.when(this.workbenchDataManager.isPersonWithEmailExists(usrDtlDto.getEmail())).thenReturn(false);
@@ -114,7 +115,7 @@ public class UserServiceTest {
 	 */
 	@Test
 	public void testCreateUserValidateError() throws Exception {
-		final UserDetailDto usrDtlDto = initializeUserDetailDto(0);
+		final UserDetailDto usrDtlDto = UserTestDataGenerator.initializeUserDetailDto(0);
 
 		Mockito.when(this.workbenchDataManager.isUsernameExists(usrDtlDto.getUsername())).thenReturn(true);
 		Mockito.when(this.workbenchDataManager.isPersonWithEmailExists(usrDtlDto.getEmail())).thenReturn(true);
@@ -133,9 +134,9 @@ public class UserServiceTest {
 	 */
 	@Test
 	public void testUpdateUser() throws Exception {
-		final UserDetailDto usrDtlDto = initializeUserDetailDto(8);
-		final UserDto userDto = initializeUserDto(8);
-		final User user = initializeUser(8);
+		final UserDetailDto usrDtlDto = UserTestDataGenerator.initializeUserDetailDto(8);
+		final UserDto userDto = UserTestDataGenerator.initializeUserDto(8);
+		final User user = UserTestDataGenerator.initializeUser(8);
 		
 		Mockito.when(this.workbenchDataManager.updateUser(userDto)).thenReturn(new Integer(8));
 		Mockito.when(this.workbenchDataManager.getUserById(8)).thenReturn(user);
@@ -151,8 +152,8 @@ public class UserServiceTest {
 	 */
 	@Test
 	public void testUpdateUserValidateError() throws Exception {
-		final UserDetailDto usrDtlDto = initializeUserDetailDto(10);
-		final User usr = initializeUser(10);
+		final UserDetailDto usrDtlDto = UserTestDataGenerator.initializeUserDetailDto(10);
+		final User usr = UserTestDataGenerator.initializeUser(10);
 		usr.getPerson().setEmail("diego.nicolas.cuenya@leafnode.io");
 		Mockito.when(this.workbenchDataManager.getUserById(usrDtlDto.getId())).thenReturn(usr);
 		Mockito.when(this.workbenchDataManager.isUsernameExists(usrDtlDto.getUsername())).thenReturn(false);
@@ -172,9 +173,9 @@ public class UserServiceTest {
 	 */
 	@Test
 	public void testUpdateUserError() throws Exception {
-		final UserDetailDto usrDtlDto = initializeUserDetailDto(10);
-		final User usr = initializeUser(10);
-		final UserDto userDto = initializeUserDto(10);
+		final UserDetailDto usrDtlDto = UserTestDataGenerator.initializeUserDetailDto(10);
+		final User usr = UserTestDataGenerator.initializeUser(10);
+		final UserDto userDto = UserTestDataGenerator.initializeUserDto(10);
 		usr.getPerson().setEmail("diego.nicolas.cuenya@leafnode.io");
 		Mockito.when(this.workbenchDataManager.getUserById(usrDtlDto.getId())).thenReturn(usr);
 		Mockito.when(this.workbenchDataManager.isUsernameExists(usrDtlDto.getUsername())).thenReturn(false);
@@ -197,11 +198,10 @@ public class UserServiceTest {
 	 */
 	@Test
 	public void getUsersByProjectUUID() throws Exception {
-		List<UserDto> usersDto = this.getAllListUser();
+		List<UserDto> usersDto = UserTestDataGenerator.getAllListUser();
 
 		Mockito.when(this.workbenchDataManager.getUsersByProjectUuid(projectUUID)).thenReturn(usersDto);
-		final Map<String, Object> mapResults = this.userServiceImpl.getUsersByProjectUUID(projectUUID);
-		final List<UserDetailDto> userDetailDtoList = (List<UserDetailDto>) mapResults.get("USERS");
+		final List<UserDetailDto> userDetailDtoList = this.userServiceImpl.getUsersByProjectUUID(projectUUID);
 		assertThat(usersDto.get(0).getFirstName(), equalTo(userDetailDtoList.get(0).getFirstName()));
 		assertThat(usersDto.get(0).getLastName(), equalTo(userDetailDtoList.get(0).getLastName()));
 		assertThat(usersDto.get(0).getUsername(), equalTo(userDetailDtoList.get(0).getUsername()));
@@ -216,11 +216,9 @@ public class UserServiceTest {
 	 *
 	 * @throws Exception
 	 */
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void getUsersByProjectUUIDWithOutProjectUUID() throws Exception {
-		final Map<String, Object> mapResults = this.userServiceImpl.getUsersByProjectUUID("");
-		assertThat("The projectUUID must not be empty", equalTo(mapResults.get("ERROR")));
-
+		this.userServiceImpl.getUsersByProjectUUID("");
 	}
 
 	/**
@@ -228,11 +226,9 @@ public class UserServiceTest {
 	 *
 	 * @throws Exception
 	 */
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void getUsersByProjectUUIDWithOutUsers() throws Exception {
-		final Map<String, Object> mapResults = this.userServiceImpl.getUsersByProjectUUID(projectUUID);
-		assertThat("don't exists users for this projectUUID", equalTo(mapResults.get("ERROR")));
-
+		this.userServiceImpl.getUsersByProjectUUID(projectUUID);
 	}
 
 	/**
@@ -240,139 +236,13 @@ public class UserServiceTest {
 	 *
 	 * @throws Exception
 	 */
-	@Test
+	@Test(expected = ApiRuntimeException.class)
 	public void getUsersByProjectUUIDErrorQuery() throws Exception {
 		Mockito.when(this.workbenchDataManager.getUsersByProjectUuid(projectUUID))
 			.thenThrow(new MiddlewareQueryException("Error in getUsersByProjectUUId()"));
-
-		final Map<String, Object> mapResults = this.userServiceImpl.getUsersByProjectUUID(projectUUID);
-		assertThat("An internal error occurred while trying to get the users", equalTo(mapResults.get("ERROR")));
-
-	}
-
-	/**
-	 * initialize List all user
-	 *
-	 * @return List<UserDetailDto>
-	 */
-	public List<UserDetailDto> createUserDetailDto() {
-		final UserDetailDto user = initializeUserDetailDto(10);
-		final List<UserDetailDto> users = Lists.newArrayList(user);
-
-		return users;
+		this.userServiceImpl.getUsersByProjectUUID(projectUUID);
 	}
 
 
-	/**
-	 * initialize List all user
-	 * 
-	 * @return List<UserDto>
-	 */
-	public List<UserDto> getAllListUser() {
-		final UserDto user = initializeUserDto(10);
-		final List<UserDto> users = Lists.newArrayList(user);
-		
-		return users;
-	}
-
-	/**
-	 * initialize UserDto
-	 * 
-	 * @return user UserDto
-	 */
-	public UserDto initializeUserDto(final Integer userId) {
-		final UserDto user = new UserDto();
-		
-		final String firstName = RandomStringUtils.randomAlphanumeric(20);
-		user.setFirstName(firstName);
-		
-		final String lastName = RandomStringUtils.randomAlphanumeric(50);
-		user.setLastName(lastName);
-		
-		user.setStatus(0);
-		user.setRole("Breeder");
-		user.setUserId(userId);
-		
-		final String username = RandomStringUtils.randomAlphanumeric(30);
-		user.setUsername(username);
-		
-		final String email = RandomStringUtils.randomAlphanumeric(24);
-		user.setEmail("test" + email + "@leafnode.io");
-
-		return user;
-	}
-
-	/**
-	 * Initialize UserDetailDto
-	 * 
-	 * @param userId Integer
-	 * @return UserDetailDto
-	 */
-	public UserDetailDto initializeUserDetailDto(final Integer userId) {
-		final UserDetailDto user = new UserDetailDto();
-		
-		final String firstName = RandomStringUtils.randomAlphanumeric(20);
-		user.setFirstName(firstName);
-		
-		final String lastName = RandomStringUtils.randomAlphanumeric(50);
-		user.setLastName(lastName);
-		
-		user.setStatus("true");
-		user.setRole("Breeder");
-		user.setId(userId);
-		
-		final String username = RandomStringUtils.randomAlphanumeric(30);
-		user.setUsername(username);
-		
-		final String email = RandomStringUtils.randomAlphanumeric(24);
-		user.setEmail("test" + email + "@leafnode.io");
-		
-		return user;
-	}
-	
-	/**
-	 * initialize User
-	 * 
-	 * @param userId Integer
-	 * @return user User
-	 */
-	public User initializeUser(final Integer userId) {
-		final User user = new User();
-		final Person person = new Person();
-		
-		person.setId(2);
-		
-		final String firstName = RandomStringUtils.randomAlphanumeric(20);
-		person.setFirstName(firstName);
-		
-		person.setMiddleName("");
-		final String lastName = RandomStringUtils.randomAlphanumeric(50);
-		person.setLastName(lastName);
-		
-		final String email = RandomStringUtils.randomAlphanumeric(24);
-		person.setEmail("test" + email + "@leafnode.io");
-		
-		person.setTitle("-");
-		person.setContact("-");
-		person.setExtension("-");
-		person.setFax("-");
-		person.setInstituteId(0);
-		person.setLanguage(0);
-		person.setNotes("-");
-		person.setPositionName("-");
-		person.setPhone("-");
-		user.setPerson(person);
-
-		user.setPersonid(person.getId());
-		user.setPerson(person);
-		
-		final String username = RandomStringUtils.randomAlphanumeric(30);
-		user.setName(username);
-		user.setAccess(0);
-		user.setInstalid(0);
-		user.setType(0);
-		
-		return user;
-	}
 
 }
