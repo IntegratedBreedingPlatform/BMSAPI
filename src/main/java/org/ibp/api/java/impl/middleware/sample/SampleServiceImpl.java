e330bbfc54af0ff2f7ccae1eb8d4f6e05b7b5917
cuenyad <cuenya.diego@gmail.com>
2017-09-15 11:21:57 -0300

    added try/catch
    ISSUE BMS-4387
    Reviewer Nahuel

--- src/main/java/org/ibp/api/java/impl/middleware/sample/SampleServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/sample/SampleServiceImpl.java
@@ -2,6 +2,8 @@ package org.ibp.api.java.impl.middleware.sample;
 
 import org.generationcp.middleware.domain.sample.SampleDTO;
 import org.generationcp.middleware.domain.sample.SampleDetailsDTO;
+import org.generationcp.middleware.exceptions.MiddlewareException;
+import org.ibp.api.exception.ApiRuntimeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,8 +23,13 @@ public class SampleServiceImpl implements SampleService {
 	}
 
 	@Override
-	public SampleDetailsDTO getSampleObservation(final String sampleId) {
-		return this.sampleService.getSampleObservation(sampleId);
-
+	public SampleDetailsDTO getSampleObservation(final String sampleId){
+		final SampleDetailsDTO sampleDetailsDTO;
+		try {
+			sampleDetailsDTO = this.sampleService.getSampleObservation(sampleId);
+		} catch (MiddlewareException e) {
+			throw new ApiRuntimeException("an error happened when try to get the sample", e);
+		}
+		return sampleDetailsDTO;
 	}
 }
