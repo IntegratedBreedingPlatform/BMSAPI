40376c9d717e6cb48bdb72abf52026411c2fe251
cuenyad <cuenya.diego@gmail.com>
2017-12-05 14:11:09 -0300

    catching AssertionFailure and MiddlewareException exceptions.
    Issue: BMS-4586
    Reviewer: Nahuel

--- src/main/java/org/ibp/api/rest/sample/SampleListResource.java
+++ src/main/java/org/ibp/api/rest/sample/SampleListResource.java
@@ -2,12 +2,20 @@ package org.ibp.api.rest.sample;
 
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
+import org.generationcp.middleware.exceptions.MiddlewareException;
+import org.hibernate.AssertionFailure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Api(value = "Sample Services")
@@ -26,8 +34,13 @@ public class SampleListResource {
 	@ResponseBody
 	public ResponseEntity<Map<String, Object>> createSampleList(@PathVariable final String crop, @RequestBody final SampleListDto dto) {
 		dto.setCropName(crop);
-		final Map<String, Object> map = this.sampleListService.createSampleList(dto);
-
+		Map<String, Object> map;
+		try {
+			map = this.sampleListService.createSampleList(dto);
+		} catch (final AssertionFailure | MiddlewareException e) {
+			map = new HashMap<>();
+			map.put("ERROR", "Something went wrong, please try again");
+		}
 		if (map.get(SampleListResource.ERROR) != null || SampleListResource.NULL.equals(map.get("id"))) {
 			return new ResponseEntity<>(map, HttpStatus.CONFLICT);
 		}
