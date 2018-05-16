efea292484508c4d85a2ee6fe17810396e74c070
Nahuel Soldevilla <nahuel@leafnode.io>
2018-05-16 18:44:28 -0300

    Some progress on Manage sample paginated list
    - overload getSampleDTOS with Pageable (Middleware)
    - return X-Total-Count header (BMSAPI)
    - Uncomment JHipster pagination component (Middleware)
    - Don't navigate on transition to encapsulate page state inside the
    sample component
    
    TODO
    - PaginatedSearch getCount
    - Datasets with pagination
    
    Issue: BMS-4882

--- src/main/java/org/ibp/api/rest/sample/SampleResource.java
+++ src/main/java/org/ibp/api/rest/sample/SampleResource.java
@@ -4,20 +4,21 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
 import org.generationcp.middleware.domain.sample.SampleDTO;
-import org.ibp.api.brapi.v1.common.Metadata;
-import org.ibp.api.brapi.v1.common.Pagination;
-import org.ibp.api.brapi.v1.common.Result;
-import org.ibp.api.brapi.v1.phenotype.PhenotypeSearchSummariesDto;
 import org.ibp.api.domain.common.PagedResult;
 import org.ibp.api.java.impl.middleware.sample.SampleService;
 import org.ibp.api.rest.common.PaginatedSearch;
 import org.ibp.api.rest.common.SearchSpec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -38,8 +39,26 @@ public class SampleResource {
 		// TODO use in swagger
 		final Pageable pageable) {
 
-		List<SampleDTO> samples = this.sampleService.filter(plotId, listId, pageable);
+		final PagedResult<SampleDTO> resultPage =
+			new PaginatedSearch().executeBrapiSearch(pageable.getPageNumber(), pageable.getPageSize(), new SearchSpec<SampleDTO>() {
 
-		return new ResponseEntity<>(samples, HttpStatus.OK);
+				@Override
+				public long getCount() {
+					// TODO
+					return 21;
+				}
+
+				@Override
+				public List<SampleDTO> getResults(final PagedResult<SampleDTO> pagedResult) {
+					return sampleService.filter(plotId, listId, pageable);
+				}
+			});
+
+		final List<SampleDTO> samples = resultPage.getPageResults();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("X-Total-Count", Long.toString(resultPage.getTotalResults()));
+
+		return new ResponseEntity<>(samples, headers, HttpStatus.OK);
 	}
 }
