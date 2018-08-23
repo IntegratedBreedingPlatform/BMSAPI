cda6102b980c7b29fab7cd8ee66e7888f1947aff
Nahuel Soldevilla <nahuel@leafnode.io>
2018-08-23 16:07:51 -0300

    Use errors.reject when there is no field
    Issue: BMS-5088
    Reviewer: Diego

--- src/main/java/org/ibp/api/java/impl/middleware/ontology/validator/FormulaValidator.java
+++ src/main/java/org/ibp/api/java/impl/middleware/ontology/validator/FormulaValidator.java
@@ -57,7 +57,7 @@ public class FormulaValidator implements Validator {
 		final FormulaDto formulaDto = (FormulaDto) target;
 
 		if (formulaDto == null) {
-			errors.rejectValue("variable.formula.required", "");
+			errors.reject("variable.formula.required", "");
 			return;
 		}
 
@@ -130,7 +130,8 @@ public class FormulaValidator implements Validator {
 		} catch (final Exception e) {
 			// Inform the ontology manager admin about the exception
 			// Mapping engine errors to UI errors would be impractical
-			errors.reject("variable.formula.invalid", new Object[] {e.getMessage(), e.getCause()}, "");
+			throw new IllegalArgumentException(
+				getMessage("variable.formula.invalid") + e.getMessage() + " - " + e.getCause());
 		}
 	}
 
