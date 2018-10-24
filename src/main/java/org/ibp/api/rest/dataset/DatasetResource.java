2cbc43878a2525ec60ef000224025f2f61cc5579
Darla Hermo <darla@leafnode.io>
2018-10-24 17:06:55 +0800

    Implement API service to count phenotypes for given crop, study, dataset and list of traitIds
    IBP-2002

---
+++ src/main/java/org/ibp/api/rest/dataset/DatasetResource.java
@@ -0,0 +1,43 @@
+
+package org.ibp.api.rest.dataset;
+
+import java.util.Arrays;
+
+import org.generationcp.middleware.service.api.study.StudyDatasetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
+@Api(value = "Dataset Services")
+@Controller
+public class DatasetResource {
+
+	@Autowired
+	private StudyDatasetService datasetService;
+
+	@ApiOperation(value = "Count Phenotypes", notes = "Returns count of phenotypes for variables")
+	@RequestMapping(value = "studies/{crop}/{studyId}/datasets/{datasetId}/variables/observations", method = RequestMethod.HEAD)
+	@Transactional
+	public ResponseEntity<String> countPhenotypes(@PathVariable final String crop, @PathVariable final Integer studyId,
+			@PathVariable final Integer datasetId,  @RequestParam(value = "variableIds", required = true) final Integer[] variableIds) {
+		if (!this.datasetService.datasetExists(studyId, datasetId)) {
+			return new ResponseEntity<>("", HttpStatus.NOT_FOUND);
+		}
+		final long count = this.datasetService.countPhenotypesForDataset(datasetId, Arrays.asList(variableIds));
+		final HttpHeaders respHeaders = new HttpHeaders();
+		respHeaders.add("X-Total-Count", String.valueOf(count));
+
+		return new ResponseEntity<>("", respHeaders, HttpStatus.OK);
+	}
+
+}
