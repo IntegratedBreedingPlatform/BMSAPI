f342a9a40f0453a18392eddcba407b8ba8af37ad
cuenyad <cuenya.diego@gmail.com>
2018-10-25 17:55:52 -0300

    Adding new service to retrieve a list of dataset by study and datasetTypeId list.
    Issue: IBP-2015
    Reviewer: None

--- src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetService.java
+++ src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetService.java
@@ -1,9 +1,16 @@
 package org.ibp.api.java.impl.middleware.dataset;
 
+import org.ibp.api.rest.dataset.DatasetDTO;
+
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by clarysabel on 10/24/18.
  */
 public interface DatasetService {
 
+
+	List<DatasetDTO> getDatasetByStudyId(final Integer studyId, final Set<Integer> filterByTypeIds);
 
 }
--- src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
@@ -1,8 +1,13 @@
 package org.ibp.api.java.impl.middleware.dataset;
 
+import org.ibp.api.rest.dataset.DatasetDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -11,4 +16,12 @@ public class DatasetServiceImpl implements DatasetService {
 	@Autowired
 	private org.generationcp.middleware.service.api.dataset.DatasetService datasetService;
 
+	@Override
+	public List<DatasetDTO> getDatasetByStudyId(final Integer studyId, final Set<Integer> filterByTypeIds) {
+		List<org.generationcp.middleware.domain.dms.DatasetDTO> datasetDTOS = this.datasetService.getDatasetByStudyId(studyId, filterByTypeIds);
+
+		final ModelMapper mapper = new ModelMapper();
+		final List<DatasetDTO> DatasetDTOs = mapper.map(datasetDTOS, List.class);
+		return DatasetDTOs;
+	}
 }
