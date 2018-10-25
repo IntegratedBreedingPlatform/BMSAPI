a922ec78a73c1f58f4b79d2840fd47de12159622
cuenyad <cuenya.diego@gmail.com>
2018-10-25 07:41:02 -0300

    creating the DatasetDTO for DatasetResource
    Issue: IBP-1935
    Reviewer: None

---
+++ src/main/java/org/ibp/api/rest/study/dataset/DatasetDTO.java
@@ -0,0 +1,74 @@
+package org.ibp.api.rest.study.dataset;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DatasetDTO {
+
+	private Integer projectId;
+
+	private Integer parent;
+
+	private Integer datasetTypeId;
+
+	private String name;
+
+	public Integer getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(Integer projectId) {
+		this.projectId = projectId;
+	}
+
+	public Integer getParent() {
+		return parent;
+	}
+
+	public void setParent(Integer parent) {
+		this.parent = parent;
+	}
+
+	public Integer getDatasetTypeId() {
+		return datasetTypeId;
+	}
+
+	public void setDatasetTypeId(Integer datasetTypeId) {
+		this.datasetTypeId = datasetTypeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return new ReflectionToStringBuilder(this).toString();
+	}
+
+	@Override
+	public boolean equals(final Object other) {
+		if (!(other instanceof DatasetDTO)) {
+			return false;
+		}
+		final DatasetDTO castOther = (DatasetDTO) other;
+		return new EqualsBuilder().append(this.projectId, castOther.projectId)
+			.append(this.parent,castOther.parent)
+			.append(this.datasetTypeId,castOther.datasetTypeId)
+			.append(this.name,castOther.name)
+			.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(this.projectId).append(this.parent).append(this.datasetTypeId).append(this.name).toHashCode();
+	}
+}
