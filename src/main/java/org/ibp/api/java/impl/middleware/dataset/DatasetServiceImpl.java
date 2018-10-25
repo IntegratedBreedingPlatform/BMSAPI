a1a1983bfe2834aba0ec32f9383c7da7be908a73
cuenyad <cuenya.diego@gmail.com>
2018-10-25 14:49:41 -0300

    leave only the body of the classes. for the branch master (IBP-1935).
    Issue: IBP-1935
    Reviewer: None

--- src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
@@ -1,6 +1,5 @@
 package org.ibp.api.java.impl.middleware.dataset;
 
-import org.ibp.api.rest.study.dataset.DatasetGeneratorInput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +10,5 @@ public class DatasetServiceImpl implements DatasetService {
 
 	@Autowired
 	private org.generationcp.middleware.service.api.dataset.DatasetService datasetService;
-
-	@Override
-	public Integer generateSubObservationDataset(final DatasetGeneratorInput datasetGeneratorInput) {
-		return datasetService.generateSubObservationDataset(null, null, null, null, null, null);
-	}
 
 }
--- src/main/java/org/ibp/api/rest/dataset/DatasetResource.java
+++ src/main/java/org/ibp/api/rest/dataset/DatasetResource.java
@@ -1,24 +1,11 @@
 package org.ibp.api.rest.dataset;
 
 import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
+import org.ibp.api.java.impl.middleware.dataset.DatasetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.ibp.api.domain.study.Observation;
-import org.ibp.api.java.impl.middleware.dataset.DatasetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-import java.util.Set;
 
 @Api(value = "Dataset Services")
 @Controller
@@ -29,23 +16,4 @@ public class DatasetResource {
 	@Autowired
 	private DatasetService datasetService;
 
-	@ApiOperation(value = "Generate and save a dataset", notes = "Returns the basic information for the generated dataset")
-	@RequestMapping(value = "/crops/{cropname}/studies/{studyId}/datasets/generation", method = RequestMethod.POST)
-	@ResponseBody
-	public ResponseEntity<Observation> generateDataset(@PathVariable
-	final String cropname, @PathVariable final Integer studyId,
-			@PathVariable final Integer observationId, @RequestBody final Object o) {
-		datasetService.generateSubObservationDataset(null);
-		return null;
-	}
-
-	@ApiOperation(value = "It will retrieve a list of datasets", notes = "Retrieves the list of datasets for the specified study.")
-	@RequestMapping(value = "/{cropname}/studies/{studyId}/datasets/list", method = RequestMethod.GET)
-	@ResponseBody
-	public ResponseEntity<List<DatasetDTO>> getDatasets(
-		@PathVariable final String cropname,
-		@PathVariable final Integer studyId,
-		@RequestParam(value = "filterByTypeIds", required = false) final Set<Integer> filterByTypeIds) {
-		return new ResponseEntity<>(this.datasetService.getDatasetByStudyId(studyId, filterByTypeIds), HttpStatus.OK);
-	}
 }
