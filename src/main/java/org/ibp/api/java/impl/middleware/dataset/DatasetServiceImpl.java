b8fef25e0aa2c7ebf71c19917b25e49030380f9f
Clarysabel Tovar <clarysabel@leafnode.io>
2018-10-26 10:27:44 -0300

    Logic to save a dataset Issue: IBP-2014 Reviewer: none

--- src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
@@ -1,9 +1,12 @@
 package org.ibp.api.java.impl.middleware.dataset;
 
+import org.ibp.api.java.impl.middleware.dataset.validator.StudyValidator;
 import org.ibp.api.rest.dataset.DatasetGeneratorInput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
 
 @Service
 @Transactional
@@ -12,11 +15,17 @@ public class DatasetServiceImpl implements DatasetService {
 	@Autowired
 	private org.generationcp.middleware.service.api.dataset.DatasetService datasetService;
 
+	@Autowired
+	private StudyValidator studyValidator;
+
 	@Override
 	public Integer generateSubObservationDataset(final Integer studyId, final DatasetGeneratorInput datasetGeneratorInput) {
+
+		this.studyValidator.validate(studyId, true);
+
 		return datasetService
 				.generateSubObservationDataset(studyId, datasetGeneratorInput.getDatasetName(), datasetGeneratorInput.getDatasetTypeId(),
-						datasetGeneratorInput.getInstanceIds(), datasetGeneratorInput.getSequenceVariableId(),
+						Arrays.asList(datasetGeneratorInput.getInstanceIds()), datasetGeneratorInput.getSequenceVariableId(),
 						datasetGeneratorInput.getNumberOfSubObservationUnits());
 	}
 
