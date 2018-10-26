b9c7efe4a495820c42eda891437315c3bfd49ace
cuenyad <cuenya.diego@gmail.com>
2018-10-26 13:28:11 -0300

    Refactoring code.
    Issue: IBP-2015
    Reviewer: None

--- src/main/java/org/ibp/api/rest/dataset/DatasetResource.java
+++ src/main/java/org/ibp/api/rest/dataset/DatasetResource.java
@@ -2,9 +2,9 @@ package org.ibp.api.rest.dataset;
 
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
+import org.ibp.api.java.impl.middleware.dataset.DatasetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.ibp.api.java.impl.middleware.dataset.DatasetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Set;
@@ -29,7 +28,6 @@ public class DatasetResource {
 
 	@ApiOperation(value = "It will retrieve a list of datasets", notes = "Retrieves the list of datasets for the specified study.")
 	@RequestMapping(value = "/crops/{cropname}/studies/{studyId}/datasets/list", method = RequestMethod.GET)
-	@ResponseBody
 	public ResponseEntity<List<DatasetDTO>> getDatasets(
 		@PathVariable final String cropname,
 		@PathVariable final Integer studyId,
