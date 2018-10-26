1bb979fdb1e265020a16d4b5bd4113c8d588524f
cuenyad <cuenya.diego@gmail.com>
2018-10-26 17:41:55 -0300

    New service to return the a list of dataset by StudyId and DatasetTypeId.
    Issue: IBP-2015
    Reviewer: None

--- src/main/java/org/ibp/api/java/dataset/DatasetService.java
+++ src/main/java/org/ibp/api/java/dataset/DatasetService.java
@@ -1,9 +1,14 @@
 package org.ibp.api.java.dataset;
 
+import org.ibp.api.rest.dataset.DatasetDTO;
+
 import java.util.List;
+import java.util.Set;
 
 public interface DatasetService {
-	
-	long countPhenotypes(Integer studyId, Integer datasetId, List<Integer> traitIds);
-	
+
+	long countPhenotypes(final Integer studyId, final Integer datasetId, final List<Integer> traitIds);
+
+	List<DatasetDTO> getDatasetByStudyId(final Integer studyId, final Set<Integer> filterByTypeIds);
+
 }
--- src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
@@ -1,15 +1,20 @@
 package org.ibp.api.java.impl.middleware.dataset;
 
-import org.ibp.api.rest.dataset.DatasetDTO;
-import org.modelmapper.ModelMapper;
-import java.util.List;
-
+import org.generationcp.middleware.domain.dms.DataSetType;
+import org.generationcp.middleware.domain.dms.Study;
+import org.generationcp.middleware.manager.api.StudyDataManager;
+import org.ibp.api.exception.ResourceNotFoundException;
 import org.ibp.api.java.dataset.DatasetService;
 import org.ibp.api.java.impl.middleware.dataset.validator.StudyValidator;
+import org.ibp.api.rest.dataset.DatasetDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.MapBindingResult;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
@@ -23,6 +28,9 @@ public class DatasetServiceImpl implements DatasetService {
 	@Autowired
 	private StudyValidator studyValidator;
 
+	@Autowired
+	private StudyDataManager studyDataManager;
+
 	@Override
 	public long countPhenotypes(final Integer studyId, final Integer datasetId, final List<Integer> traitIds) {
 		this.studyValidator.validate(studyId, false);
@@ -32,7 +40,27 @@ public class DatasetServiceImpl implements DatasetService {
 
 	@Override
 	public List<DatasetDTO> getDatasetByStudyId(final Integer studyId, final Set<Integer> filterByTypeIds) {
-		List<org.generationcp.middleware.domain.dms.DatasetDTO> datasetDTOS = this.datasetService.getDatasetByStudyId(studyId, filterByTypeIds);
+		final BindingResult errors = new MapBindingResult(new HashMap<String, String>(), Integer.class.getName());
+
+		final Study study = studyDataManager.getStudy(studyId);
+
+		if (study == null) {
+			errors.reject("study.not.exist", "");
+			throw new ResourceNotFoundException(errors.getAllErrors().get(0));
+		}
+
+		if (filterByTypeIds != null) {
+			for (Integer dataSetTypeId : filterByTypeIds) {
+				final DataSetType dataSetType = DataSetType.findById(dataSetTypeId);
+				if (dataSetType == null) {
+					errors.reject("dataset.type.id.not.exist", new Object[]{dataSetTypeId},"");
+					throw new ResourceNotFoundException(errors.getAllErrors().get(0));
+				}
+			}
+		}
+
+		final List<org.generationcp.middleware.domain.dms.DatasetDTO> datasetDTOS =
+			this.middlewareDatasetService.getDatasetByStudyId(studyId, filterByTypeIds);
 
 		final ModelMapper mapper = new ModelMapper();
 		final List<DatasetDTO> DatasetDTOs = mapper.map(datasetDTOS, List.class);
