4e380a35dd908be9053b38071c6ca6b592393dff
Clarysabel Tovar <clarysabel@leafnode.io>
2018-10-26 13:39:38 -0300

    Fixing merge error Issue: IBP-2014 Reviewer: none

--- src/main/java/org/ibp/api/rest/dataset/DatasetResource.java
+++ src/main/java/org/ibp/api/rest/dataset/DatasetResource.java
@@ -5,27 +5,46 @@ import com.wordnik.swagger.annotations.ApiOperation;
 import org.ibp.api.domain.study.Observation;
 import org.ibp.api.java.dataset.DatasetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
 
 @Api(value = "Dataset Services")
 @Controller
 public class DatasetResource {
 
 	@Autowired
-	private DatasetService datasetService;
+	private DatasetService studyDatasetService;
+
+	@ApiOperation(value = "Count Phenotypes", notes = "Returns count of phenotypes for variables")
+	@RequestMapping(value = "/{crop}/studies/{studyId}/datasets/{datasetId}/variables/observations", method = RequestMethod.HEAD)
+	@Transactional
+	public ResponseEntity<String> countPhenotypes(@PathVariable final String crop, @PathVariable final Integer studyId,
+			@PathVariable final Integer datasetId,  @RequestParam(value = "variableIds", required = true) final Integer[] variableIds) {
+
+		final long count = this.studyDatasetService.countPhenotypes(studyId, datasetId, Arrays.asList(variableIds));
+		final HttpHeaders respHeaders = new HttpHeaders();
+		respHeaders.add("X-Total-Count", String.valueOf(count));
+
+		return new ResponseEntity<>("", respHeaders, HttpStatus.OK);
+	}
 
 	@ApiOperation(value = "Generate and save a dataset", notes = "Returns the basic information for the generated dataset")
 	@RequestMapping(value = "/crops/{cropName}/studies/{studyId}/datasets/generation", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<Observation> generateDataset(@PathVariable
 	final String cropName, @PathVariable final Integer studyId, @RequestBody final DatasetGeneratorInput datasetGeneratorInput) {
-		datasetService.generateSubObservationDataset(studyId, datasetGeneratorInput);
+		studyDatasetService.generateSubObservationDataset(studyId, datasetGeneratorInput);
 		return null;
 	}
 }
