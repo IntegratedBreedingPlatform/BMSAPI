0f2e6336c9a6b976725a36460f62b4ab44424ff4
Darla Hermo <darla@leafnode.io>
2018-10-26 16:34:28 +0800

    Fix DatasetResource test for countPhenotypes
    IBP-2002

--- src/test/java/org/ibp/api/rest/dataset/DatasetResourceTest.java
+++ src/test/java/org/ibp/api/rest/dataset/DatasetResourceTest.java
@@ -8,7 +8,6 @@ import org.ibp.ApiUnitTestBase;
 import org.ibp.api.java.dataset.DatasetService;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -40,21 +39,12 @@ public class DatasetResourceTest extends ApiUnitTestBase {
 	}
 	
 	@Test
-	public void testCountPhenotypesForInvalidDataset() throws Exception {
-		this.mockMvc
-			.perform(MockMvcRequestBuilders.head("/crops/{crop}/{studyId}/datasets/{datasetId}/variables/observations", this.cropName, 100, 102)
-					.param("variableIds", "1,2,3").contentType(this.contentType))
-			.andDo(MockMvcResultHandlers.print())
-			.andExpect(MockMvcResultMatchers.status().isNotFound());
-	}
-	
-	@Test
 	public void testCountPhenotypesForValidDataset() throws Exception {
 		final long count = 10;
-		doReturn(count).when(this.studyDatasetService).countPhenotypesForDataset(102, Arrays.asList(1, 2, 3));
+		doReturn(count).when(this.studyDatasetService).countPhenotypes(100, 102, Arrays.asList(1, 2, 3));
 		
 		this.mockMvc
-			.perform(MockMvcRequestBuilders.head("/crops/{crop}/{studyId}/datasets/{datasetId}/variables/observations", this.cropName, 100, 102)
+			.perform(MockMvcRequestBuilders.head("/crops/{crop}/studies/{studyId}/datasets/{datasetId}/variables/observations", this.cropName, 100, 102)
 					.param("variableIds", "1,2,3").contentType(this.contentType))
 			.andDo(MockMvcResultHandlers.print())
 			.andExpect(MockMvcResultMatchers.status().isOk())
