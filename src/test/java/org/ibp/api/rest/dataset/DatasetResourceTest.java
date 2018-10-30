8742ba501248d8ae971094269eed0b1f5c4b88ce
Darla Hermo <darla@leafnode.io>
2018-10-30 15:30:09 +0800

    Unit test for adding dataset trait
    IBP-2001

--- src/test/java/org/ibp/api/rest/dataset/DatasetResourceTest.java
+++ src/test/java/org/ibp/api/rest/dataset/DatasetResourceTest.java
@@ -3,8 +3,12 @@ package org.ibp.api.rest.dataset;
 import static org.mockito.Mockito.doReturn;
 
 import java.util.Arrays;
+import java.util.Random;
 
+import org.apache.commons.lang.RandomStringUtils;
+import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.ibp.ApiUnitTestBase;
+import org.ibp.api.domain.dataset.DatasetTrait;
 import org.ibp.api.java.dataset.DatasetService;
 import org.junit.Before;
 import org.junit.Test;
@@ -39,7 +43,7 @@ public class DatasetResourceTest extends ApiUnitTestBase {
 	}
 	
 	@Test
-	public void testCountPhenotypesForValidDataset() throws Exception {
+	public void testCountPhenotypes() throws Exception {
 		final long count = 10;
 		doReturn(count).when(this.studyDatasetService).countPhenotypes(100, 102, Arrays.asList(1, 2, 3));
 		
@@ -49,5 +53,24 @@ public class DatasetResourceTest extends ApiUnitTestBase {
 			.andDo(MockMvcResultHandlers.print())
 			.andExpect(MockMvcResultMatchers.status().isOk())
 			.andExpect(MockMvcResultMatchers.header().string("X-Total-Count", String.valueOf(count)));
+	}
+	
+	@Test
+	public void testAddDatasetTrait() throws Exception {
+		final Random random = new Random();
+		final int studyId = random.nextInt(10000);
+		final int datasetId = random.nextInt(10000);
+		final int traitId = random.nextInt(10000);
+		final String alias = RandomStringUtils.randomAlphabetic(20);
+		final MeasurementVariable measurementVariable = new MeasurementVariable();
+		final DatasetTrait datasetTrait = new DatasetTrait(traitId, alias);
+		doReturn(measurementVariable).when(this.studyDatasetService).addDatasetTrait(studyId, datasetId,  datasetTrait);
+		
+		this.mockMvc
+			.perform(MockMvcRequestBuilders.put("/crops/{crop}/studies/{studyId}/datasets/{datasetId}/variables", this.cropName, studyId, datasetId)
+					.contentType(this.contentType)
+					.content(this.convertObjectToByte(datasetTrait)))
+			.andDo(MockMvcResultHandlers.print())
+			.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 }
