3650230d3103b5a76e48d0911c2939861d5ca466
cuenyad <cuenya.diego@gmail.com>
2018-10-31 17:10:13 -0300

    Adding new properties return on getDatasetId servive.
    Issue: IBP-2016
    Reviewer: None

--- src/main/java/org/ibp/api/rest/dataset/DatasetDTO.java
+++ src/main/java/org/ibp/api/rest/dataset/DatasetDTO.java
@@ -1,10 +1,13 @@
 package org.ibp.api.rest.dataset;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import org.ibp.api.domain.study.StudyInstance;
 import org.pojomatic.Pojomatic;
 import org.pojomatic.annotations.AutoProperty;
 
 import java.io.Serializable;
+import java.util.List;
+
 @AutoProperty
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class DatasetDTO implements Serializable {
@@ -16,6 +19,14 @@ public class DatasetDTO implements Serializable {
 	private String name;
 
 	private Integer parentDatasetId;
+
+	private String description;
+
+	private Integer studyId;
+
+	private String cropName;
+
+	private List<StudyInstance> instances;
 
 
 	public Integer getDatasetId() {
@@ -48,6 +59,38 @@ public class DatasetDTO implements Serializable {
 
 	public void setParentDatasetId(final Integer parentDatasetId) {
 		this.parentDatasetId = parentDatasetId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	public Integer getStudyId() {
+		return studyId;
+	}
+
+	public void setStudyId(final Integer studyId) {
+		this.studyId = studyId;
+	}
+
+	public String getCropName() {
+		return cropName;
+	}
+
+	public void setCropName(final String cropName) {
+		this.cropName = cropName;
+	}
+
+	public List<StudyInstance> getInstances() {
+		return instances;
+	}
+
+	public void setInstances(List<StudyInstance> instances) {
+		this.instances = instances;
 	}
 
 	@Override
