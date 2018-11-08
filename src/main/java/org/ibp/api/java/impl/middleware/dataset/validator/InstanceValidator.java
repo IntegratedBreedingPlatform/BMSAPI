32e462635148148c506d418e84182d529274eece
Aldrin Batac <aldrin.batac@leafnode.io>
2018-11-08 13:44:02 +0800

    Use studyDataManager.isInstanceExistsInDataset to validate instance for a specific dataset.
    IBP-2006

--- src/main/java/org/ibp/api/java/impl/middleware/dataset/validator/InstanceValidator.java
+++ src/main/java/org/ibp/api/java/impl/middleware/dataset/validator/InstanceValidator.java
@@ -1,7 +1,5 @@
 package org.ibp.api.java.impl.middleware.dataset.validator;
 
-import com.google.common.base.Optional;
-import org.generationcp.middleware.dao.dms.InstanceMetadata;
 import org.generationcp.middleware.manager.api.StudyDataManager;
 import org.ibp.api.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +17,11 @@ public class InstanceValidator {
 
 	private BindingResult errors;
 
-	public void validate(final Integer studyId, final Integer instanceId) {
+	public void validate(final Integer datasetId, final Integer instanceId) {
 
 		errors = new MapBindingResult(new HashMap<String, String>(), Integer.class.getName());
 
-		final Optional<InstanceMetadata> instanceMetadataOptional = studyDataManager.getInstanceMetadataByInstanceId(studyId, instanceId);
-
-		if (!instanceMetadataOptional.isPresent()) {
+		if (!studyDataManager.isInstanceExistsInDataset(datasetId, instanceId)) {
 			errors.reject("instance.does.not.exist", "");
 			throw new ResourceNotFoundException(this.errors.getAllErrors().get(0));
 		}
--- src/test/java/org/ibp/api/java/impl/middleware/dataset/validator/InstanceValidatorTest.java
+++ src/test/java/org/ibp/api/java/impl/middleware/dataset/validator/InstanceValidatorTest.java
@@ -1,7 +1,5 @@
 package org.ibp.api.java.impl.middleware.dataset.validator;
 
-import com.google.common.base.Optional;
-import org.generationcp.middleware.dao.dms.InstanceMetadata;
 import org.generationcp.middleware.manager.api.StudyDataManager;
 import org.ibp.ApiUnitTestBase;
 import org.ibp.api.exception.ResourceNotFoundException;
@@ -22,23 +20,22 @@ public class InstanceValidatorTest extends ApiUnitTestBase {
 	@Test
 	public void testValidateSuccess() {
 		final Random ran = new Random();
-		final int studyId = ran.nextInt();
+		final int datasetId = ran.nextInt();
 		final int instanceId = ran.nextInt();
 
-		final Optional<InstanceMetadata> instanceMetadataOptional = Optional.of(new InstanceMetadata());
-		Mockito.when(studyDataManager.getInstanceMetadataByInstanceId(studyId, instanceId)).thenReturn(instanceMetadataOptional);
-		instanceValidator.validate(studyId, instanceId);
+		Mockito.when(studyDataManager.isInstanceExistsInDataset(datasetId, instanceId)).thenReturn(true);
+		instanceValidator.validate(datasetId, instanceId);
 
 	}
 
 	@Test(expected = ResourceNotFoundException.class)
 	public void testValidateRejected() {
 		final Random ran = new Random();
-		final int studyId = ran.nextInt();
+		final int datasetId = ran.nextInt();
 		final int instanceId = ran.nextInt();
 
-		Mockito.when(studyDataManager.getInstanceMetadataByInstanceId(studyId, instanceId)).thenReturn(Optional.<InstanceMetadata>absent());
-		instanceValidator.validate(studyId, instanceId);
+		Mockito.when(studyDataManager.isInstanceExistsInDataset(datasetId, instanceId)).thenReturn(false);
+		instanceValidator.validate(datasetId, instanceId);
 
 	}
 
