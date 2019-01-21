463401346dfb3729ef9d0fc6b9513599d763e8bf
clarysabel <clarysabel@gmail.com>
2019-01-21 18:15:14 -0300

    Export summary of labels needed for subObservations dataset label printing IBP-2078

--- src/main/java/org/ibp/api/rest/labelprinting/LabelPrintingResource.java
+++ src/main/java/org/ibp/api/rest/labelprinting/LabelPrintingResource.java
@@ -3,55 +3,49 @@ package org.ibp.api.rest.labelprinting;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import org.ibp.api.exception.NotSupportedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Api(value = "Label Printing Services")
 @RestController
 public class LabelPrintingResource {
 
-	@RequestMapping(value = "/crops/{cropname}/labelPrinting/{printingLabelType}/labels/summary", method = RequestMethod.GET)
+	@Autowired
+	private SubObservationDatasetLabelPrinting subObservationDatasetLabelPrinting;
+
+	@RequestMapping(value = "/crops/{cropname}/labelPrinting/{printingLabelType}/labels/summary", method = RequestMethod.POST)
 	@ApiOperation(value = "Get Summary of Labels Needed according to the specified printing label type",
 		notes = "Returns summary of labels needed according to the printing label type.")
-	public ResponseEntity<LabelsNeededSummary> getLabelsNeededSummary (@PathVariable String cropname, @PathVariable Integer printingLabelType) {
+	@ResponseBody
+	public ResponseEntity<LabelsNeededSummaryResponse> getLabelsNeededSummary(
+		@PathVariable String cropname, @PathVariable Integer printingLabelType,
+		@RequestBody LabelsNeededSummaryInput labelsNeededSummaryInput) {
 		final LabelPrintingType labelPrintingType = LabelPrintingType.getEnumByCode(printingLabelType);
 		if (labelPrintingType == null) {
 			throw new NotSupportedException(null);
 		}
-		final LabelsNeededSummary labelsNeededSummary = new LabelsNeededSummary();
-		final List<String> headers = new ArrayList<>();
-		headers.add("Environment");
-		headers.add("# of sub-obs units");
-		headers.add("Labels needed");
-		labelsNeededSummary.setHeaders(headers);
+		final LabelPrintingStrategy labelPrintingStrategy = this.locateLabelPrintingImpl(labelPrintingType);
+		labelPrintingStrategy.validateInputData(labelsNeededSummaryInput);
+		final LabelsNeededSummary labelsNeededSummary = labelPrintingStrategy.getSummaryOfLabelsNeeded(labelsNeededSummaryInput);
+		final LabelsNeededSummaryResponse labelsNeededSummaryResponse = labelPrintingStrategy.transformLabelsNeededSummary(labelsNeededSummary);
 
-		final List<Map<String, String>> values = new ArrayList<>();
-		Map<String, String> row1 = new HashMap<>();
-		row1.put("Environment", "1");
-		row1.put("# of sub-obs units", "20");
-		row1.put("Labels needed", "20");
-		values.add(row1);
-
-		Map<String, String> row2 = new HashMap<>();
-		row2.put("Environment", "2");
-		row2.put("# of sub-obs units", "20");
-		row2.put("Labels needed", "20");
-		values.add(row2);
-
-		labelsNeededSummary.setValues(values);
-
-		return new ResponseEntity<>(labelsNeededSummary, HttpStatus.OK);
+		return new ResponseEntity<>(labelsNeededSummaryResponse, HttpStatus.OK);
 	}
 
-
+	private LabelPrintingStrategy locateLabelPrintingImpl(final LabelPrintingType labelPrintingType) {
+		switch (labelPrintingType) {
+			case DATASET:
+				return subObservationDatasetLabelPrinting;
+			default:
+				return null;
+		}
+	}
 
 }
