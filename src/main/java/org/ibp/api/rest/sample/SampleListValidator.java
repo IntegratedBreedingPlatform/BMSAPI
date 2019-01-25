19dfe4aba1b27734580c4a4582971a6d4c262d79
Aldrin Batac <aldrin.batac@leafnode.io>
2019-01-25 13:16:53 +0800

    Create message property entries for sample list validation messages.
    IBP-1927

--- src/main/java/org/ibp/api/rest/sample/SampleListValidator.java
+++ src/main/java/org/ibp/api/rest/sample/SampleListValidator.java
@@ -18,31 +18,28 @@ public class SampleListValidator {
 		this.errors = new MapBindingResult(new HashMap<String, String>(), Integer.class.getName());
 
 		if (sampleListDto.getInstanceIds() == null) {
-			this.errors.reject("The Instance List must not be null");
+			this.errors.reject("sample.list.instance.list.must.not.be.null","The Instance List must not be null");
 		}
 		if (sampleListDto.getInstanceIds().isEmpty()) {
-			this.errors.reject("The Instance List must not be empty");
+			this.errors.reject("sample.list.instance.list.must.not.empty","The Instance List must not be empty");
 		}
 		if (sampleListDto.getSelectionVariableId() == null) {
-			this.errors.reject("The Selection Variable Id must not be empty");
-		}
-		if (StringUtils.isEmpty(sampleListDto.getListName())) {
-			this.errors.reject("The List Name must not be empty");
+			this.errors.reject("sample.list.selection.variable.id.must.not.empty", "The Selection Variable Id must not be empty");
 		}
 		if (StringUtils.isBlank(sampleListDto.getListName())) {
-			this.errors.reject("The List Name must not be empty");
+			this.errors.reject("sample.list.listname.must.not.empty","The List Name must not be empty");
 		}
 		if (sampleListDto.getListName().length() > 100) {
-			this.errors.reject("List Name must not exceed 100 characters");
+			this.errors.reject("sample.list.listname.exceed.length","List Name must not exceed 100 characters");
 		}
 		if (StringUtils.isEmpty(sampleListDto.getCreatedDate())) {
-			this.errors.reject("The Created Date must not be empty");
+			this.errors.reject("sample.list.created.date.empty","The Created Date must not be empty");
 		}
 		if (StringUtils.isNotBlank(sampleListDto.getDescription()) && sampleListDto.getDescription().length() > 255) {
-			this.errors.reject("List Description must not exceed 255 characters");
+			this.errors.reject("sample.list.description.exceed.length","List Description must not exceed 255 characters");
 		}
 		if (StringUtils.isNotBlank(sampleListDto.getNotes()) && sampleListDto.getNotes().length() > 65535) {
-			this.errors.reject("Notes must not exceed 65535 characters");
+			this.errors.reject("sample.list.notes.exceed.length","Notes must not exceed 65535 characters");
 		}
 
 		if (this.errors.hasErrors()) {
@@ -54,21 +51,21 @@ public class SampleListValidator {
 	public void validateFolderName(final String folderName) {
 
 		if (folderName == null) {
-			this.errors.reject("The folder name must not be null");
+			this.errors.reject("sample.list.folder.is.null","The folder name must not be null");
 			throw new ApiRequestValidationException(this.errors.getAllErrors());
 		}
 	}
 
 	public void validateFolderId(final Integer parentId) {
 		if (parentId == null) {
-			this.errors.reject("The parent Id must not be null");
+			this.errors.reject("sample.list.parent.id.is.null","The parent Id must not be null");
 			throw new ApiRequestValidationException(this.errors.getAllErrors());
 		}
 	}
 
 	public void validateProgramUUID(final String programUUID) {
 		if (programUUID == null) {
-			this.errors.reject("The parent Id must not be null");
+			this.errors.reject("sample.list.program.uuid.is.null","The program UUID must not be null");
 			throw new ApiRequestValidationException(this.errors.getAllErrors());
 		}
 	}
