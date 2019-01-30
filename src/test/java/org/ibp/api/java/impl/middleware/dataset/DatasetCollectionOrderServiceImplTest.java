0d7f38d6985aea0b16145c1128db3ee15ea1fc53
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2019-01-30 19:00:08 +0800

    Added unit test for DatasetCollectionOrderServiceImpl class
    IBP-1078

---
+++ src/test/java/org/ibp/api/java/impl/middleware/dataset/DatasetCollectionOrderServiceImplTest.java
@@ -0,0 +1,119 @@
+package org.ibp.api.java.impl.middleware.dataset;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.generationcp.middleware.domain.fieldbook.FieldmapBlockInfo;
+import org.generationcp.middleware.service.impl.study.StudyInstance;
+import org.ibp.api.java.impl.middleware.study.FieldMapService;
+import org.ibp.api.rest.dataset.ObservationUnitRow;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DatasetCollectionOrderServiceImplTest {
+
+	@Mock
+	private FieldMapService fieldMapService;
+
+	@Mock
+	private DataCollectionSorter dataCollectionSorter;
+
+	@InjectMocks
+	private DatasetCollectionOrderServiceImpl datasetCollectionOrderService;
+
+	@Test
+	public void testReorderForPLOT_ORDER() {
+		this.datasetCollectionOrderService.reorder(DatasetCollectionOrderServiceImpl.CollectionOrder.PLOT_ORDER, 1,
+		1, new ArrayList<ObservationUnitRow>());
+		Mockito.verify(this.fieldMapService).getBlockId(1, String.valueOf(1));
+		Mockito.verify(this.fieldMapService, Mockito.never()).getBlockInformation(Matchers.anyInt());
+		final FieldmapBlockInfo fieldmapBlockInfo = null;
+		Mockito.verify(this.dataCollectionSorter, Mockito.never()).orderByRange(eq(fieldmapBlockInfo), any(ArrayList.class));
+		Mockito.verify(this.dataCollectionSorter, Mockito.never()).orderByColumn(eq(fieldmapBlockInfo), any(ArrayList.class));
+	}
+
+	@Test
+	public void testReorderForSERPENTINE_ALONG_ROWS() {
+		Mockito.when(this.fieldMapService.getBlockId(1, "1")).thenReturn("1");
+		final FieldmapBlockInfo fieldmapBlockInfo = Mockito.mock(FieldmapBlockInfo.class);
+		Mockito.when(this.fieldMapService.getBlockInformation(1)).thenReturn(fieldmapBlockInfo);
+		this.datasetCollectionOrderService.reorder(DatasetCollectionOrderServiceImpl.CollectionOrder.SERPENTINE_ALONG_ROWS, 1,
+			1, new ArrayList<ObservationUnitRow>());
+
+		Mockito.verify(this.fieldMapService).getBlockId(1, String.valueOf(1));
+		Mockito.verify(this.fieldMapService).getBlockInformation(Matchers.anyInt());
+		Mockito.verify(this.dataCollectionSorter).orderByRange(eq(fieldmapBlockInfo), any(ArrayList.class));
+		Mockito.verify(this.dataCollectionSorter, Mockito.never()).orderByColumn(eq(fieldmapBlockInfo), any(ArrayList.class));
+	}
+
+	@Test
+	public void testReorderForSERPENTINE_ALONG_COLUMNS() {
+		Mockito.when(this.fieldMapService.getBlockId(1, "1")).thenReturn("1");
+		final FieldmapBlockInfo fieldmapBlockInfo = Mockito.mock(FieldmapBlockInfo.class);
+		Mockito.when(this.fieldMapService.getBlockInformation(1)).thenReturn(fieldmapBlockInfo);
+		this.datasetCollectionOrderService.reorder(DatasetCollectionOrderServiceImpl.CollectionOrder.SERPENTINE_ALONG_COLUMNS, 1,
+			1, new ArrayList<ObservationUnitRow>());
+
+		Mockito.verify(this.fieldMapService).getBlockId(1, String.valueOf(1));
+		Mockito.verify(this.fieldMapService).getBlockInformation(Matchers.anyInt());
+		Mockito.verify(this.dataCollectionSorter, Mockito.never()).orderByRange(eq(fieldmapBlockInfo), any(ArrayList.class));
+		Mockito.verify(this.dataCollectionSorter).orderByColumn(eq(fieldmapBlockInfo), any(ArrayList.class));
+	}
+
+	@Test
+	public void testReorder() {
+		Mockito.when(this.fieldMapService.getBlockId(anyInt(), anyString())).thenReturn("1");
+		final FieldmapBlockInfo fieldmapBlockInfo = Mockito.mock(FieldmapBlockInfo.class);
+		Mockito.when(this.fieldMapService.getBlockInformation(anyInt())).thenReturn(fieldmapBlockInfo);
+
+		final Map<Integer, StudyInstance> selectedDatasetInstancesMap = this.createStudyInstanceMap();
+		final Map<Integer, List<ObservationUnitRow>> observationUnitRowMap = this.createObservationUnitRowMap();
+
+		this.datasetCollectionOrderService.reorder(DatasetCollectionOrderServiceImpl.CollectionOrder.SERPENTINE_ALONG_COLUMNS,
+			1, selectedDatasetInstancesMap, observationUnitRowMap);
+
+		Mockito.verify(this.fieldMapService, Mockito.times(2)).getBlockId(anyInt(), anyString());
+		Mockito.verify(this.fieldMapService, Mockito.times(2)).getBlockInformation(anyInt());
+		Mockito.verify(this.dataCollectionSorter, Mockito.never()).orderByRange(eq(fieldmapBlockInfo), any(ArrayList.class));
+		Mockito.verify(this.dataCollectionSorter, Mockito.times(2)).orderByColumn(eq(fieldmapBlockInfo), any(ArrayList.class));
+	}
+
+	private Map<Integer, StudyInstance> createStudyInstanceMap() {
+		final StudyInstance studyInstance1 = this.createStudyInstance(1);
+		final StudyInstance studyInstance2 = this.createStudyInstance(2);
+		final Map<Integer, StudyInstance> studyInstanceMap = new HashMap<>();
+		studyInstanceMap.put(1, studyInstance1);
+		studyInstanceMap.put(2, studyInstance2);
+		return studyInstanceMap;
+	}
+
+	private StudyInstance createStudyInstance(final Integer instanceId) {
+		final StudyInstance studyInstance = new StudyInstance();
+		studyInstance.setInstanceDbId(instanceId);
+		studyInstance.setInstanceNumber(instanceId);
+		studyInstance.setLocationName("LOC - " + instanceId);
+		return studyInstance;
+	}
+
+	private Map<Integer, List<ObservationUnitRow>> createObservationUnitRowMap() {
+		final Map<Integer, List<ObservationUnitRow>> observationUnitRowMap = new HashMap<>();
+		observationUnitRowMap.put(1, new ArrayList<ObservationUnitRow>());
+		observationUnitRowMap.put(2, new ArrayList<ObservationUnitRow>());
+		return  observationUnitRowMap;
+	}
+}
