ee68a92561a185347bc986c736feeeb456a76a6f
clarysabel <clarysabel@leafnode.io>
2019-02-01 11:29:44 -0300

    Renamed classes, including service to export labels file signature IBP-2078

---
+++ src/main/java/org/ibp/api/rest/labelprinting/domain/LabelsGeneratorInput.java
@@ -0,0 +1,24 @@
+package org.ibp.api.rest.labelprinting.domain;
+
+import org.pojomatic.Pojomatic;
+import org.pojomatic.annotations.AutoProperty;
+
+@AutoProperty
+public class LabelsGeneratorInput extends LabelsInfoInput {
+
+
+	@Override
+	public int hashCode() {
+		return Pojomatic.hashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		return Pojomatic.toString(this);
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		return Pojomatic.equals(this, o);
+	}
+}
