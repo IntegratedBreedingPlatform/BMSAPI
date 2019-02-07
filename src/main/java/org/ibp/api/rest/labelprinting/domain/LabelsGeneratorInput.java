2673a1d076155ec7f77a04407b77205e60e382d5
Clarysabel Tovar <clarysabel@leafnode.io>
2019-02-07 11:09:14 -0300

    Adding body params Issue: IBP-2078 IBP-2375 Reviewer: none

--- src/main/java/org/ibp/api/rest/labelprinting/domain/LabelsGeneratorInput.java
+++ src/main/java/org/ibp/api/rest/labelprinting/domain/LabelsGeneratorInput.java
@@ -1,11 +1,61 @@
 package org.ibp.api.rest.labelprinting.domain;
 
+import com.sun.tools.javac.util.List;
 import org.pojomatic.Pojomatic;
 import org.pojomatic.annotations.AutoProperty;
 
 @AutoProperty
 public class LabelsGeneratorInput extends LabelsInfoInput {
 
+	private List<List<String>> fields;
+
+	private boolean barcodeRequired;
+
+	private boolean automaticBarcode;
+
+	private List<String> barcodeFields;
+
+	private String fileName;
+
+	public List<List<String>> getFields() {
+		return fields;
+	}
+
+	public void setFields(final List<List<String>> fields) {
+		this.fields = fields;
+	}
+
+	public boolean isBarcodeRequired() {
+		return barcodeRequired;
+	}
+
+	public void setBarcodeRequired(final boolean barcodeRequired) {
+		this.barcodeRequired = barcodeRequired;
+	}
+
+	public boolean isAutomaticBarcode() {
+		return automaticBarcode;
+	}
+
+	public void setAutomaticBarcode(final boolean automaticBarcode) {
+		this.automaticBarcode = automaticBarcode;
+	}
+
+	public List<String> getBarcodeFields() {
+		return barcodeFields;
+	}
+
+	public void setBarcodeFields(final List<String> barcodeFields) {
+		this.barcodeFields = barcodeFields;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(final String fileName) {
+		this.fileName = fileName;
+	}
 
 	@Override
 	public int hashCode() {
