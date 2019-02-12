8a331b560d3e2fc08510de38e5cd8706f92c6dc8
cuenyad <cuenya.diego@gmail.com>
2019-02-11 23:46:16 -0300

    Adding throws IOException
    IBP-2078

--- src/main/java/org/ibp/api/rest/labelprinting/LabelsFileGenerator.java
+++ src/main/java/org/ibp/api/rest/labelprinting/LabelsFileGenerator.java
@@ -4,12 +4,13 @@ import org.ibp.api.rest.labelprinting.domain.LabelsData;
 import org.ibp.api.rest.labelprinting.domain.LabelsGeneratorInput;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by clarysabel on 2/7/19.
  */
 public interface LabelsFileGenerator {
 
-	File generate (final LabelsGeneratorInput labelsGeneratorInput, final LabelsData labelsData);
+	File generate(final LabelsGeneratorInput labelsGeneratorInput, final LabelsData labelsData) throws IOException;
 
 }
