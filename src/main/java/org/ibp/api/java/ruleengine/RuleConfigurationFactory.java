fbab4e2bf8017261c88c2920f5ac84c9421ba8a5
Nahuel Soldevilla <nahuel@leafnode.io>
2019-03-29 15:59:40 -0300

    Group generators and resolvers in the rule engine
    SeedSourceGenerator, BreedersCrossIDGenerator. It'll simplify
    refactoring the classes to remove workbook dependency.
    
    Issue: IBP-2437 / IBP-2600
    Reviewer: None

--- src/main/java/org/ibp/api/java/ruleengine/RuleConfigurationFactory.java
+++ src/main/java/org/ibp/api/java/ruleengine/RuleConfigurationFactory.java
@@ -1,18 +1,13 @@
 package org.ibp.api.java.ruleengine;
 
 import org.generationcp.commons.ruleengine.ProcessCodeRuleFactory;
-import org.generationcp.commons.ruleengine.RuleFactory;
-import org.generationcp.commons.ruleengine.naming.expression.ComponentPostProcessor;
 import org.generationcp.commons.ruleengine.naming.impl.ProcessCodeFactory;
 import org.generationcp.commons.ruleengine.provider.PropertyFileRuleConfigurationProvider;
 import org.generationcp.commons.service.GermplasmNamingProperties;
-import org.generationcp.commons.service.impl.BreedersCrossIDGenerator;
-import org.generationcp.middleware.pojos.KeySequenceRegister;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.generationcp.commons.ruleengine.generator.BreedersCrossIDGenerator;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
