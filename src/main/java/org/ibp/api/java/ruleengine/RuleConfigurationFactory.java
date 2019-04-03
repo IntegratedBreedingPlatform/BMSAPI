4a83aa9d826d830360f51bd319659fa799332155
Nahuel Soldevilla <nahuel@leafnode.io>
2019-04-03 19:03:26 -0300

    Consolidate crossing.properties in Commons
    Configure PropertySource in BMSAPI RuleConfigurationFactory to inject
    properties using Value annotation or inside spring Environment
    
    Issue: IBP-2437 / IBP-2600
    Reviewer: None

--- src/main/java/org/ibp/api/java/ruleengine/RuleConfigurationFactory.java
+++ src/main/java/org/ibp/api/java/ruleengine/RuleConfigurationFactory.java
@@ -9,9 +9,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @EnableConfigurationProperties
+@PropertySources({
+	@PropertySource("classpath:crossing.properties")
+})
 public class RuleConfigurationFactory {
 
 	@Bean
