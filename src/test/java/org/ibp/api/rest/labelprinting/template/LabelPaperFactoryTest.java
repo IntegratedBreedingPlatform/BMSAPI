732c760ccdc7459c0288642b5b7a82629c284729
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2019-04-11 14:17:20 +0800

    Fixed assertions
    IBP-2079

--- src/test/java/org/ibp/api/rest/labelprinting/template/LabelPaperFactoryTest.java
+++ src/test/java/org/ibp/api/rest/labelprinting/template/LabelPaperFactoryTest.java
@@ -13,22 +13,22 @@ public class LabelPaperFactoryTest {
 	@Test
 	public void testGenerateLabelPaper() {
 		LabelPaper labelPaper = this.labelPaperFactory.generateLabelPaper(3, 7, LabelPaperFactory.SIZE_OF_PAPER_LETTER);
-		Assert.assertEquals(LabelPaper.PAPER_3_BY_7_LETTER, labelPaper.getClass());
+		Assert.assertEquals(LabelPaper.PAPER_3_BY_7_LETTER, labelPaper);
 
 		labelPaper = this.labelPaperFactory.generateLabelPaper(3, 8, LabelPaperFactory.SIZE_OF_PAPER_LETTER);
-		Assert.assertEquals(LabelPaper.PAPER_3_BY_8_LETTER, labelPaper.getClass());
+		Assert.assertEquals(LabelPaper.PAPER_3_BY_8_LETTER, labelPaper);
 
 		labelPaper = this.labelPaperFactory.generateLabelPaper(3, 10, LabelPaperFactory.SIZE_OF_PAPER_LETTER);
-		Assert.assertEquals(LabelPaper.PAPER_3_BY_10_LETTER, labelPaper.getClass());
+		Assert.assertEquals(LabelPaper.PAPER_3_BY_10_LETTER, labelPaper);
 
 		labelPaper = this.labelPaperFactory.generateLabelPaper(3, 7, LabelPaperFactory.SIZE_OF_PAPER_A4);
-		Assert.assertEquals(LabelPaper.PAPER_3_BY_7_A4, labelPaper.getClass());
+		Assert.assertEquals(LabelPaper.PAPER_3_BY_7_A4, labelPaper);
 
 		labelPaper = this.labelPaperFactory.generateLabelPaper(3, 8, LabelPaperFactory.SIZE_OF_PAPER_A4);
-		Assert.assertEquals(LabelPaper.PAPER_3_BY_8_A4, labelPaper.getClass());
+		Assert.assertEquals(LabelPaper.PAPER_3_BY_8_A4, labelPaper);
 
 		labelPaper = this.labelPaperFactory.generateLabelPaper(3, 10, LabelPaperFactory.SIZE_OF_PAPER_A4);
-		Assert.assertEquals(LabelPaper.PAPER_3_BY_10_A4, labelPaper.getClass());
+		Assert.assertEquals(LabelPaper.PAPER_3_BY_10_A4, labelPaper);
 	}
 
 }
