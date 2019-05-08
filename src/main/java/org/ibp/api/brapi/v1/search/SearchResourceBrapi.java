0344b8a0214a249681eb6ea63ab7877d0b844563
Vanina Maletta <vmaletta@droptek.com>
2019-05-08 15:11:51 -0300

    Adding service to MiddlewareFactory
    Issue: IBP-2693
    Reviewer: Clary

--- src/main/java/org/ibp/api/brapi/v1/search/SearchResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v1/search/SearchResourceBrapi.java
@@ -1,16 +1,11 @@
 package org.ibp.api.brapi.v1.search;
 
 import com.wordnik.swagger.annotations.Api;
-import org.ibp.api.java.search.SearchRequestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Api(value = "BrAPI Search Services")
 @Controller
 public class SearchResourceBrapi {
-
-	@Autowired
-	private SearchRequestService brapiSearchRequestService;
 
 
 }
