bd9ae07e55ba8b9688732d4d23f11370abdf4f70
Vanina Maletta <vmaletta@droptek.com>
2019-05-10 16:39:58 -0300

    Remove request_type from search_request table
    Issue: IBP-2693
    Reviewer: Clary

--- src/main/java/org/ibp/api/brapi/v1/search/SearchResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v1/search/SearchResourceBrapi.java
@@ -3,10 +3,8 @@ package org.ibp.api.brapi.v1.search;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import org.generationcp.middleware.domain.search_request.GermplasmSearchRequestDto;
-import org.generationcp.middleware.domain.search_request.SearchRequestType;
 import org.generationcp.middleware.manager.api.SearchRequestService;
 import org.generationcp.middleware.pojos.search.SearchRequest;
-import org.ibp.api.brapi.v1.common.SingleEntityResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,19 +28,6 @@ public class SearchResourceBrapi {
 	public ResponseEntity<Integer> postSearchGermplasm(@PathVariable final String crop, @RequestBody final GermplasmSearchRequestDto germplasmSearchRequestDto) {
 		final SearchRequest searchRequest = this.searchRequestService.saveSearchRequest(germplasmSearchRequestDto, GermplasmSearchRequestDto.class);
 		return new ResponseEntity<>(searchRequest.getRequestId(), HttpStatus.OK);
-
-	}
-
-	@ApiOperation(value = "Get germplasm search", notes = "Get germplasm search")
-	@RequestMapping(value = "/{crop}/brapi/v1/search/germplasm/{requestId}", method = RequestMethod.GET)
-	@ResponseBody
-	public ResponseEntity<SingleEntityResponse<GermplasmSearchRequestDto>> getSearchGermplasm(@PathVariable final String crop, @PathVariable final Integer requestId) {
-		final GermplasmSearchRequestDto searchRequest =
-			(GermplasmSearchRequestDto) this.searchRequestService.getSearchRequest(requestId, GermplasmSearchRequestDto.class);
-
-		final SingleEntityResponse<GermplasmSearchRequestDto> searchResourceResponse = new SingleEntityResponse<>(searchRequest);
-
-		return new ResponseEntity<>(searchResourceResponse, HttpStatus.OK);
 
 	}
 }
