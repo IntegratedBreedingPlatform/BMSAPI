7aeebffd369b8463bb04b56fa9e2a37ffadcca73
cuenyad <cuenya.diego@gmail.com>
2019-06-28 16:42:41 -0300

    Defining the service to get the role types.
    issue: IBP-2691
    review: none

---
+++ src/main/java/org/ibp/api/java/impl/middleware/role/type/RoleTypeServiceImpl.java
@@ -0,0 +1,29 @@
+package org.ibp.api.java.impl.middleware.role.type;
+
+import org.apache.commons.lang3.text.WordUtils;
+import org.generationcp.middleware.manager.api.WorkbenchDataManager;
+import org.generationcp.middleware.pojos.workbench.RoleType;
+import org.ibp.api.domain.role.RoleTypeDto;
+import org.ibp.api.java.role.type.RoleTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RoleTypeServiceImpl implements RoleTypeService {
+
+	@Autowired
+	private WorkbenchDataManager workbenchDataManager;
+
+	@Override
+	public List<RoleTypeDto> getRoleTypes() {
+
+		final List<RoleType> roleTypes = this.workbenchDataManager.getRoleTypes();
+		final List<RoleTypeDto> roleTypeDtos = roleTypes.stream()
+			.map(roleType -> new RoleTypeDto(roleType.getId(), WordUtils.capitalizeFully(roleType.getName())))
+			.collect(Collectors.toList());
+		return roleTypeDtos;
+	}
+}
