d1aef27cf6a075efeaeb723e80ea0b9d64988403
Clarysabel Tovar <clarysabel@leafnode.io>
2019-09-20 16:57:26 -0300

    Skeleton for lot services IBP-2975

--- src/main/java/org/ibp/api/java/impl/middleware/inventory_new/LotServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/inventory_new/LotServiceImpl.java
@@ -1,9 +1,14 @@
 package org.ibp.api.java.impl.middleware.inventory_new;
 
+import org.generationcp.middleware.domain.inventory_new.LotDto;
+import org.generationcp.middleware.domain.inventory_new.LotsSearchDto;
 import org.ibp.api.java.inventory_new.LotService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -12,4 +17,13 @@ public class LotServiceImpl implements LotService {
 	@Autowired
 	private org.generationcp.middleware.service.api.inventory.LotService lotService;
 
+	@Override
+	public List<LotDto> searchLots(final LotsSearchDto lotsSearchDto, final Pageable pageable) {
+		return lotService.searchLots(lotsSearchDto, pageable);
+	}
+
+	@Override
+	public long countSearchLots(final LotsSearchDto lotsSearchDto) {
+		return lotService.countSearchLots(lotsSearchDto);
+	}
 }
--- src/main/java/org/ibp/api/rest/inventory_new/LotResource.java
+++ src/main/java/org/ibp/api/rest/inventory_new/LotResource.java
@@ -4,24 +4,83 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import org.generationcp.middleware.domain.inventory_new.LotDto;
 import org.generationcp.middleware.domain.inventory_new.LotsSearchDto;
+import org.generationcp.middleware.manager.api.SearchRequestService;
+import org.ibp.api.brapi.v1.common.SingleEntityResponse;
+import org.ibp.api.domain.common.PagedResult;
+import org.ibp.api.domain.search.SearchDto;
+import org.ibp.api.java.inventory_new.LotService;
+import org.ibp.api.rest.common.PaginatedSearch;
+import org.ibp.api.rest.common.SearchSpec;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Api(value = "Lot Resource")
 @RestController
 public class LotResource {
 
-	@ApiOperation(value = "It will retrieve all the observation units", notes = "It will retrieve all the observation units including observations and props values in a format that will be used by the Observations table.")
-	@RequestMapping(value = "/crops/{cropname}/lots/search", method = RequestMethod.POST)
+	@Autowired
+	private LotService lotService;
+
+	@Autowired
+	private SearchRequestService searchRequestService;
+
+	@ApiOperation(value = "Post lot search", notes = "Post lot search")
+	@RequestMapping(value = "/{cropName}/lots/search", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<LotDto> getLots(@PathVariable final String cropname, //
-		@RequestBody final LotsSearchDto searchDTO) {
-		return null;
+	public ResponseEntity<SingleEntityResponse<SearchDto>> postSearchLots(
+		@PathVariable final String cropName, @RequestBody final LotsSearchDto lotsSearchDto) {
+		final String searchRequestId =
+			this.searchRequestService.saveSearchRequest(lotsSearchDto, LotsSearchDto.class).toString();
+
+		final SearchDto searchDto = new SearchDto(searchRequestId);
+		final SingleEntityResponse<SearchDto> singleGermplasmResponse = new SingleEntityResponse<SearchDto>(searchDto);
+
+		return new ResponseEntity<>(singleGermplasmResponse, HttpStatus.OK);
+
+	}
+
+	@ApiOperation(value = "It will retrieve lots that matches search conditions", notes = "It will retrieve lots that matches search conditions")
+	@RequestMapping(value = "/{cropName}/lots/search/{searchRequestId}", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<List<LotDto>> getLots(@PathVariable final String cropName, //
+		@RequestParam final Integer searchRequestId, final Pageable pageable) {
+
+		final LotsSearchDto searchDTO = (LotsSearchDto) this.searchRequestService
+			.getSearchRequest(searchRequestId, LotsSearchDto.class);
+
+		final PagedResult<LotDto> resultPage =
+			new PaginatedSearch().executeBrapiSearch(pageable.getPageNumber(), pageable.getPageSize(), new SearchSpec<LotDto>() {
+
+				@Override
+				public long getCount() {
+					return LotResource.this.lotService.countSearchLots(searchDTO);
+				}
+
+				@Override
+				public List<LotDto> getResults(final PagedResult<LotDto> pagedResult) {
+					return LotResource.this.lotService.searchLots(searchDTO, pageable);
+				}
+			});
+
+		final List<LotDto> lotDtos = resultPage.getPageResults();
+
+		final HttpHeaders headers = new HttpHeaders();
+		headers.add("X-Total-Count", Long.toString(resultPage.getTotalResults()));
+
+		return new ResponseEntity<>(lotDtos, headers, HttpStatus.OK);
+
 	}
 
 }
