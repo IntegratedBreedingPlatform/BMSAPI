fb1acba8fe2a67310f3581b1bd1016572318b404
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2019-10-08 08:43:55 +0800

    Moved error messages and exception to BMSAPI
    IBP-3107 / IBP-3137

---
+++ src/main/java/org/ibp/api/exception/BVDesignException.java
@@ -0,0 +1,25 @@
+
+package org.ibp.api.exception;
+
+public class BVDesignException extends Exception {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 24993039887181720L;
+	private String bvErrorCode;
+
+	public BVDesignException(String bvErrorCode) {
+		super();
+		this.bvErrorCode = bvErrorCode;
+	}
+
+	public String getBvErrorCode() {
+		return this.bvErrorCode;
+	}
+
+	public void setBvErrorCode(String bvErrorCode) {
+		this.bvErrorCode = bvErrorCode;
+	}
+
+}
