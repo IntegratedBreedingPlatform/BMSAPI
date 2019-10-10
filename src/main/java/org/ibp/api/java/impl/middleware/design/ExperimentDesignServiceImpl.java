c2a8532445baba20de87f23ccbfbe4f454d9e0d7
Aldrin Batac <aldrin.batac@leafnode.io>
2019-10-10 08:35:58 +0800

    Map ObservationUnitRow to Middleware's ObservationUnitRow.
    IBP-3107

--- src/main/java/org/ibp/api/java/impl/middleware/design/ExperimentDesignServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/design/ExperimentDesignServiceImpl.java
@@ -12,13 +12,17 @@ import org.ibp.api.java.impl.middleware.design.type.ExperimentDesignTypeServiceF
 import org.ibp.api.java.study.StudyService;
 import org.ibp.api.rest.dataset.ObservationUnitRow;
 import org.ibp.api.rest.design.ExperimentDesignInput;
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -64,11 +68,32 @@ public class ExperimentDesignServiceImpl implements ExperimentDesignService {
 
 		this.experimentDesignMiddlewareService.deleteExperimentDesign(studyId);
 		this.experimentDesignMiddlewareService
-			.saveExperimentDesign(cropType, studyId, measurementVariables, this.convert(observationUnitRows));
+			.saveExperimentDesign(cropType, studyId, measurementVariables, this.mapObservationUnitRow(observationUnitRows));
 	}
 
-	List<org.generationcp.middleware.service.api.dataset.ObservationUnitRow> convert(final List<ObservationUnitRow> observationUnitRows) {
-		// TODO: Convert dto to middleware pojo version ?
-		return new ArrayList<>();
+	List<org.generationcp.middleware.service.api.dataset.ObservationUnitRow> mapObservationUnitRow(
+		final List<ObservationUnitRow> observationUnitRows) {
+		final ModelMapper observationUnitRowMapper = new ModelMapper();
+		observationUnitRowMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+
+		final List<org.generationcp.middleware.service.api.dataset.ObservationUnitRow> convertedRows = new ArrayList<>();
+		for (final ObservationUnitRow row : observationUnitRows) {
+			final Map<String, org.generationcp.middleware.service.api.dataset.ObservationUnitData> variables = new HashMap<>();
+			final Map<String, org.generationcp.middleware.service.api.dataset.ObservationUnitData> environmentVariables = new HashMap<>();
+			for (final String data : row.getVariables().keySet()) {
+				variables.put(data, observationUnitRowMapper
+					.map(row.getVariables().get(data), org.generationcp.middleware.service.api.dataset.ObservationUnitData.class));
+			}
+			for (final String data : row.getEnvironmentVariables().keySet()) {
+				environmentVariables.put(data, observationUnitRowMapper.map(row.getEnvironmentVariables().get(data),
+					org.generationcp.middleware.service.api.dataset.ObservationUnitData.class));
+			}
+			final org.generationcp.middleware.service.api.dataset.ObservationUnitRow convertedRow =
+				observationUnitRowMapper.map(row, org.generationcp.middleware.service.api.dataset.ObservationUnitRow.class);
+			convertedRow.setVariables(variables);
+			convertedRow.setEnvironmentVariables(environmentVariables);
+			convertedRows.add(convertedRow);
+		}
+		return convertedRows;
 	}
 }
