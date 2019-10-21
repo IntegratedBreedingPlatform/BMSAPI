1d94899d5af7820d5d426b8454cfe12a2dcec0f7
Darla Hermo <darla@leafnode.io>
2019-10-21 17:05:59 +0800

    Moved service for checking BV license expiry days
    IBP-3107

--- src/main/java/org/ibp/api/ibpworkbench/rest/BreedingView.java
+++ src/main/java/org/ibp/api/ibpworkbench/rest/BreedingView.java
@@ -17,10 +17,14 @@ import com.wordnik.swagger.annotations.ApiParam;
 import org.ibp.api.ibpworkbench.constants.WebAPIConstants;
 import org.ibp.api.ibpworkbench.model.DataResponse;
 import org.ibp.api.ibpworkbench.service.BreedingViewService;
+import org.ibp.api.java.design.DesignLicenseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,12 +32,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Api(value = "Breeding View", description = "Web Services to process the Breeding View output")
+@Api(value = "Breeding View Resource")
 @RestController
 @RequestMapping("/breeding_view")
 public class BreedingView {
@@ -43,31 +48,36 @@ public class BreedingView {
 	@Autowired
 	private BreedingViewService breedingViewService;
 
+	@Resource
+	private DesignLicenseService designLicenseService;
+
 	@ApiOperation(value = "Save the Single-Site Analysis CSV output file with heritability", notes = "", response = DataResponse.class)
 	@RequestMapping(value = "{cropName}/ssa/save_result_summary", method = RequestMethod.GET, produces = {MediaType.APPLICATION_XML_VALUE})
 	@ResponseBody
 	public DataResponse saveSsaResultSummary(
-			@ApiParam(value = "Path and filename of the SSA output file", required = true) @RequestParam("mainOutputFilePath") String mainOutputFilePath,
+			@ApiParam(value = "Path and filename of the SSA output file", required = true) @RequestParam("mainOutputFilePath") final String mainOutputFilePath,
 
-			@ApiParam(value = "Path and filename of the Summary output file", required = true) @RequestParam("SummaryOutputFilePath") String summaryOutputFilePath,
+			@ApiParam(value = "Path and filename of the Summary output file", required = true) @RequestParam("SummaryOutputFilePath")
+			final String summaryOutputFilePath,
 
-			@ApiParam(value = "Path and filename of the Outlier output file", required = false) @RequestParam(value = "OutlierFilePath", required = false) String outlierOutputFilePath,
+			@ApiParam(value = "Path and filename of the Outlier output file", required = false) @RequestParam(value = "OutlierFilePath", required = false)
+			final String outlierOutputFilePath,
 
-			@ApiParam(value = "Current Project ID", required = true) @RequestParam("WorkbenchProjectId") String workbenchProjectId,
+			@ApiParam(value = "Current Project ID", required = true) @RequestParam("WorkbenchProjectId") final String workbenchProjectId,
 
-			@ApiParam(value = "Study ID", required = true) @RequestParam("StudyId") String studyId,
+			@ApiParam(value = "Study ID", required = true) @RequestParam("StudyId") final String studyId,
 
-			@ApiParam(value = "Input Dataset ID", required = true) @RequestParam("InputDataSetId") String inputDataSetId,
+			@ApiParam(value = "Input Dataset ID", required = true) @RequestParam("InputDataSetId") final String inputDataSetId,
 
-			@ApiParam(value = "Output Dataset ID", required = true) @RequestParam("OutputDataSetId") String outputDataSetId,
+			@ApiParam(value = "Output Dataset ID", required = true) @RequestParam("OutputDataSetId") final String outputDataSetId,
 
-			@PathVariable(value = "cropName") String cropName
+			@PathVariable(value = "cropName") final String cropName
 	) {
 		DataResponse response;
 
 		try {
-			Map<String, String> params = new HashMap<String, String>();
-			List<String> errors = new ArrayList<String>();
+			final Map<String, String> params = new HashMap<>();
+			final List<String> errors = new ArrayList<>();
 			if (mainOutputFilePath == null || mainOutputFilePath.isEmpty()) {
 				errors.add("mainOutputFilePath is a required field!");
 			}
@@ -102,7 +112,7 @@ public class BreedingView {
 			} else {
 				response = new DataResponse(false, "Errors invoking web service: " + errors);
 			}
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			BreedingView.LOG.debug(e.getMessage(), e);
 			response = new DataResponse(false, "Failed to invoke service: " + e.toString());
 		}
@@ -110,8 +120,19 @@ public class BreedingView {
 		return response;
 	}
 
-	public void setBreedingViewService(BreedingViewService breedingViewService) {
+	@ApiOperation(value = "Count number of days before Breeding View license expires")
+	@RequestMapping(value= "/license/expiryDays", method = RequestMethod.HEAD)
+	@ResponseBody
+	public ResponseEntity<String> getLicenseExpiryDays() {
+		final Integer expiryDays = this.designLicenseService.getExpiryDays();
+		final HttpHeaders respHeaders = new HttpHeaders();
+		respHeaders.add("X-Total-Count", String.valueOf(expiryDays));
+		return new ResponseEntity<>("", respHeaders, HttpStatus.OK);
+	}
+
+	public void setBreedingViewService(final BreedingViewService breedingViewService) {
 		this.breedingViewService = breedingViewService;
 	}
+
 
 }
