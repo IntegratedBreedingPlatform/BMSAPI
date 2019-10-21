7c02d3ae8f6dacf4b76e3a730255e94b29484d6e
Darla Hermo <darla@leafnode.io>
2019-10-21 10:28:49 +0800

    Create ExperimentDesignValidator class with validator if experiment design exists for study
    IBP-3107

---
+++ src/main/java/org/ibp/api/java/impl/middleware/design/validator/ExperimentDesignValidator.java
@@ -0,0 +1,38 @@
+package org.ibp.api.java.impl.middleware.design.validator;
+
+import com.google.common.base.Optional;
+import org.ibp.api.exception.ApiRequestValidationException;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.MapBindingResult;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+
+@Component
+public class ExperimentDesignValidator {
+
+	@Resource
+	private org.generationcp.middleware.service.api.study.generation.ExperimentDesignService experimentDesignMiddlewareService;
+
+	private BindingResult errors;
+
+	public void validateExperimentDesignExistence(final Integer studyId, final Boolean experimentDesignShouldExist) {
+		this.errors = new MapBindingResult(new HashMap<String, String>(), Integer.class.getName());
+
+		final Optional<Integer> experimentDesignTypeTermId = this.experimentDesignMiddlewareService.getExperimentDesignTypeTermId(studyId);
+
+		if (experimentDesignShouldExist && !experimentDesignTypeTermId.isPresent()) {
+			this.errors.reject("study.has.no.experiment.design");
+			throw new ApiRequestValidationException(this.errors.getAllErrors());
+		}
+
+		if (!experimentDesignShouldExist && experimentDesignTypeTermId.isPresent()) {
+			this.errors.reject("study.already.has.experiment.design");
+			throw new ApiRequestValidationException(this.errors.getAllErrors());
+		}
+	}
+
+
+
+}
