b31760394fcebfc127c3d5006cdbaccfd9db2245
Vanina Maletta <vmaletta@droptek.com>
2019-12-13 10:27:45 -0300

    Add missing tests
    Issue: IBP-3279
    Reviewer: Clary

--- src/test/java/org/ibp/api/java/impl/middleware/inventory/TransactionInputValidatorTest.java
+++ src/test/java/org/ibp/api/java/impl/middleware/inventory/TransactionInputValidatorTest.java
@@ -1,6 +1,10 @@
 package org.ibp.api.java.impl.middleware.inventory;
 
+import com.google.common.collect.Lists;
+import org.generationcp.middleware.domain.inventory.manager.ExtendedLotDto;
+import org.generationcp.middleware.domain.inventory.manager.LotsSearchDto;
 import org.generationcp.middleware.domain.inventory.manager.TransactionDto;
+import org.generationcp.middleware.pojos.ims.LotStatus;
 import org.generationcp.middleware.service.api.inventory.LotService;
 import org.ibp.api.exception.ApiRequestValidationException;
 import org.ibp.api.exception.NotSupportedException;
@@ -11,7 +15,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TransactionInputValidatorTest {
@@ -56,6 +63,31 @@ public class TransactionInputValidatorTest {
 		this.transactionDto.setNotes(
 			"notesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotesnotes");
 
+		this.transactionInputValidator.validate(this.transactionDto);
+	}
+
+	@Test(expected = ApiRequestValidationException.class)
+	public void validateCloseLot() {
+		this.transactionDto.setAmount(10.0);
+		this.transactionDto.setTransactionType("Deposit");
+		final LotsSearchDto lotsSearchDto = new LotsSearchDto();
+		lotsSearchDto.setLotIds(Lists.newArrayList(this.transactionDto.getLot().getLotId()));
+		final ExtendedLotDto lot = new ExtendedLotDto();
+		lot.setScaleId(1);
+		lot.setStatus(LotStatus.CLOSED.name());
+		final List<ExtendedLotDto> result = Lists.newArrayList(lot);
+		Mockito.when(this.lotService.searchLots(lotsSearchDto, null)).thenReturn(result);
+		this.transactionInputValidator.validate(this.transactionDto);
+	}
+
+	@Test(expected = ApiRequestValidationException.class)
+	public void validateEmptyLot() {
+		this.transactionDto.setAmount(10.0);
+		this.transactionDto.setTransactionType("Deposit");
+		final LotsSearchDto lotsSearchDto = new LotsSearchDto();
+		lotsSearchDto.setLotIds(Lists.newArrayList(this.transactionDto.getLot().getLotId()));
+
+		Mockito.when(this.lotService.searchLots(lotsSearchDto, null)).thenReturn(Lists.newArrayList());
 		this.transactionInputValidator.validate(this.transactionDto);
 	}
 }
