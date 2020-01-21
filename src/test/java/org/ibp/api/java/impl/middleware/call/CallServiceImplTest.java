cdf502500956f889da16967cf6f021bec1f2ff3c
Nahuel Soldevilla <nahuel@leafnode.io>
2020-01-21 18:00:11 -0300

    Mock call.json in CallServiceImplTest.java
    No need to keep updating the test after adding new calls
    
    Issue: IBP-3213
    Reviewer: None

--- src/test/java/org/ibp/api/java/impl/middleware/call/CallServiceImplTest.java
+++ src/test/java/org/ibp/api/java/impl/middleware/call/CallServiceImplTest.java
@@ -1,37 +1,66 @@
 package org.ibp.api.java.impl.middleware.call;
 
-import org.ibp.api.java.calls.CallService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.io.Resource;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
+import static org.mockito.Mockito.doReturn;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CallServiceImplTest {
 
-	private final CallService callService = new CallServiceImpl();
+	@Mock
+	private Resource calls;
 
-	// TODO mock calls.json or rewrite so that test won't break after adding/removing calls
+	@InjectMocks
+	private CallServiceImpl callService;
+
+	@Before
+	public void setup() throws IOException {
+		this.resetInputStream();
+	}
+
 	@Test
-	public void testGetAllCalls() {
+	public void testGetAllCalls() throws IOException {
 
 		final List<Map<String, Object>> result = this.callService.getAllCalls(null, 10, 0);
 		Assert.assertEquals("First page should contain 10 records", 10, result.size());
+		this.resetInputStream();
 
 		final List<Map<String, Object>> result2 = this.callService.getAllCalls(null, 10, 1);
 		Assert.assertEquals("Second page should contain 10 records", 10, result2.size());
+		resetInputStream();
 
 		final List<Map<String, Object>> result3 = this.callService.getAllCalls(null, null, null);
 		Assert.assertEquals("Should return all records if pageSize and pageNumber are not specified", 21, result3.size());
+		resetInputStream();
 
 		// Search by BrAPI v1.2 where CSV data type = csv
 		final List<Map<String, Object>> result4 = this.callService.getAllCalls("csv", 10, 0);
 		Assert.assertEquals("There is only one call service with csv datatype", 1, result4.size());
+		resetInputStream();
 
 		// Search by BrAPI v1.2 where CSV data type = text/csv
 		final List<Map<String, Object>> result5 = this.callService.getAllCalls("text/csv", 10, 0);
 		Assert.assertEquals("There is only one call service with text/csv datatype", 1, result5.size());
 
+	}
+
+	private void resetInputStream() throws IOException {
+		final InputStream is = new FileInputStream(new File("src/test/resources/brapi/calls.json"));
+		doReturn(is).when(this.calls).getInputStream();
 	}
 
 }
