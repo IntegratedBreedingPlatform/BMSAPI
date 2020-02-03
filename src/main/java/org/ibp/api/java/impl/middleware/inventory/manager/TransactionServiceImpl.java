44751c4863ebe6dfc55350673391eb88ae9f4bf1
Vanina Maletta <vmaletta@droptek.com>
2020-02-03 16:15:15 -0300

    Add getAll TransactionStatus and TransactionType
    Issue: IBP-3309
    Reviewer: Clary

--- src/main/java/org/ibp/api/java/impl/middleware/inventory/manager/TransactionServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/inventory/manager/TransactionServiceImpl.java
@@ -2,6 +2,8 @@ package org.ibp.api.java.impl.middleware.inventory.manager;
 
 import org.generationcp.middleware.domain.inventory.manager.TransactionDto;
 import org.generationcp.middleware.domain.inventory.manager.TransactionsSearchDto;
+import org.generationcp.middleware.pojos.ims.TransactionStatus;
+import org.generationcp.middleware.pojos.ims.TransactionType;
 import org.ibp.api.java.impl.middleware.inventory.manager.validator.TransactionInputValidator;
 import org.ibp.api.java.inventory.manager.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,5 +38,15 @@ public class TransactionServiceImpl implements TransactionService {
 	public Integer saveTransaction(final TransactionDto transactionDto) {
 		this.transactionInputValidator.validate(transactionDto);
 		return this.transactionService.saveTransaction(transactionDto);
+	}
+
+	@Override
+	public List<TransactionStatus>  getAllTransactionStatuses() {
+		return TransactionStatus.getAll();
+	}
+
+	@Override
+	public List<TransactionType>  getAllTransactionTypes() {
+		return TransactionType.getAll();
 	}
 }
