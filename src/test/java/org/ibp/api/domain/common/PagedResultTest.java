1fb6158d5023313dc43de00c492c65559606cb51
Gelli Samson <gelli@leafnode.io>
2020-04-14 10:53:04 +0800

    Add Spring annotation to initialize objects IBP-3248-BrApiMaxPageSize

--- src/test/java/org/ibp/api/domain/common/PagedResultTest.java
+++ src/test/java/org/ibp/api/domain/common/PagedResultTest.java
@@ -1,11 +1,19 @@
 
 package org.ibp.api.domain.common;
 
+import org.ibp.Main;
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = Main.class)
 public class PagedResultTest {
 
 	@Test(expected = IllegalArgumentException.class)
