3496f8071bd6ff57813fb72a281ad369c96cfb21
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2020-04-23 12:39:09 +0800

    Added preauthorize annotation
    IBP-3459

--- src/main/java/org/ibp/api/rest/role/type/RoleTypeResource.java
+++ src/main/java/org/ibp/api/rest/role/type/RoleTypeResource.java
@@ -6,6 +6,7 @@ import org.ibp.api.java.role.type.RoleTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@PreAuthorize("hasAnyAuthority('ADMIN','ADMINISTRATION','SITE_ADMIN')")
 @Controller
 public class RoleTypeResource {
 
