256aea6532d3d86bd7fd2a6eb7ddfa196c560215
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2020-04-29 17:11:48 +0800

    Added programatic checking for roles in listing the programs per crop
    IBP-3459

--- src/main/java/org/ibp/api/rest/program/ProgramResource.java
+++ src/main/java/org/ibp/api/rest/program/ProgramResource.java
@@ -1,29 +1,42 @@
 
 package org.ibp.api.rest.program;
 
+import org.generationcp.middleware.pojos.workbench.PermissionsEnum;
 import org.ibp.api.domain.program.ProgramSummary;
+import org.ibp.api.java.impl.middleware.security.SecurityService;
 import org.ibp.api.java.program.ProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
-@PreAuthorize("hasAnyAuthority('ADMIN','ADMINISTRATION','CROP_MANAGEMENT','SITE_ADMIN')")
 public class ProgramResource {
 
     @Autowired
     private ProgramService programService;
 
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private SecurityService securityService;
+
     @RequestMapping(value = "/crops/{cropName}/programs", method = RequestMethod.GET)
     public ResponseEntity<List<ProgramSummary>> listPrograms(@PathVariable final String cropName) {
-        return new ResponseEntity<>(this.programService.listProgramsByCropName(cropName), HttpStatus.OK);
+        if (request.isUserInRole(PermissionsEnum.ADMIN.name())
+            || request.isUserInRole(PermissionsEnum.CROP_MANAGEMENT.name())
+            || request.isUserInRole(PermissionsEnum.ADMINISTRATION.name())
+            || request.isUserInRole(PermissionsEnum.SITE_ADMIN.name())) {
+            return new ResponseEntity<>(this.programService.listProgramsByCropName(cropName), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(this.programService.listProgramsByCropNameAndUser(this.securityService.getCurrentlyLoggedInUser(), cropName), HttpStatus.OK);
+        }
     }
 }
