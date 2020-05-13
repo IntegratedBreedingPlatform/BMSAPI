89ffc289ade78dbdc4008637ac19d29cc0666f6f
Clarysabel Tovar <clarysabel@leafnode.io>
2020-05-13 10:15:11 -0300

    Adding Planting Resource IBP-3669

---
+++ src/main/java/org/ibp/api/rest/inventory/planting/PlantingResource.java
@@ -0,0 +1,10 @@
+package org.ibp.api.rest.inventory.planting;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "Planting Services")
+@RestController
+public class PlantingResource {
+
+}
