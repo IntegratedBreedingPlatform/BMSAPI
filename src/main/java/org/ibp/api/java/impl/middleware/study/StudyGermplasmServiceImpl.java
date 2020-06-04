b626ba9e043efecdeb8a1eaa87691797a0087736
darla-leafnode <darla@leafnode.io>
2020-06-04 19:36:00 +0800

    Initial commit for StudyGermplasmResource
    IBP-3773

--- src/main/java/org/ibp/api/java/impl/middleware/study/StudyGermplasmServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/study/StudyGermplasmServiceImpl.java
@@ -1,9 +1,12 @@
 package org.ibp.api.java.impl.middleware.study;
 
+import org.generationcp.middleware.service.api.PedigreeService;
 import org.generationcp.middleware.service.api.study.StudyGermplasmDto;
+import org.generationcp.middleware.util.CrossExpansionProperties;
 import org.ibp.api.java.impl.middleware.study.validator.StudyGermplasmValidator;
 import org.ibp.api.java.impl.middleware.study.validator.StudyValidator;
 import org.ibp.api.java.study.StudyGermplasmService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +18,12 @@ public class StudyGermplasmServiceImpl implements StudyGermplasmService {
 
     @Resource
     private StudyValidator studyValidator;
+
+    @Autowired
+    private PedigreeService pedigreeService;
+
+    @Autowired
+    private CrossExpansionProperties crossExpansionProperties;
 
 
     @Resource
@@ -28,7 +37,7 @@ public class StudyGermplasmServiceImpl implements StudyGermplasmService {
         this.studyValidator.validate(studyId, true);
         this.studyGermplasmValidator.validate(studyId, entryId, gid);
 
-        return this.middlewareStudyGermplasmService.replaceStudyGermplasm(studyId, entryId, gid);
+        return this.middlewareStudyGermplasmService.replaceStudyGermplasm(studyId, entryId, gid, this.pedigreeService.getCrossExpansion(gid, this.crossExpansionProperties));
     }
 
 
