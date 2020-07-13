a082ad511f60ab42ddc05210fee5c60fd743af70
Clarysabel Tovar <clarysabel@leafnode.io>
2020-07-13 15:49:36 -0300

    Progress on hiding menu item when feature is disabled

--- src/main/java/org/ibp/api/rest/samplesubmission/GOBiiResource.java
+++ src/main/java/org/ibp/api/rest/samplesubmission/GOBiiResource.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import org.generationcp.middleware.service.api.gobii.GOBiiContactDTO;
 import org.generationcp.middleware.service.api.gobii.GOBiiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,14 +13,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
-@Api(value = "GOBii Contact Services")
+@Api(value = "GOBii Services")
 @Controller
-public class GOBiiContactResource {
+public class GOBiiResource {
 
 	@Autowired
 	private GOBiiService goBiiService;
+
+	@Autowired
+	private Environment environment;
+
+	private Boolean gobiiStatus;
+
+	@PostConstruct
+	public void resolveGobiiURL() {
+		gobiiStatus = Boolean.valueOf(environment.getProperty("gobii.submission.enabled"));
+	}
+
+	@ApiOperation(value = "Get GOBii Submission status", notes = "Get all GOBii contacts")
+	@RequestMapping(value = "/gobii-submission-status", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<Boolean> getGobiiStubmissionStatus() {
+		return new ResponseEntity<>(gobiiStatus, HttpStatus.OK);
+	}
 
 	@ApiOperation(value = "Get all GOBii contacts", notes = "Get all GOBii contacts")
 	@RequestMapping(value = "/gobii-contacts", method = RequestMethod.GET)
