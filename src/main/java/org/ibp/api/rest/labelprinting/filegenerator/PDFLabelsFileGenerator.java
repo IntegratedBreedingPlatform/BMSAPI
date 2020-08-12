e17baabe478d7f8126298561605b015c3f4add69
cuenyad <cuenya.diego@gmail.com>
2020-08-12 15:20:02 -0300

    Replace null by a double quote.
    IBP-3777
    Review none

--- src/main/java/org/ibp/api/rest/labelprinting/filegenerator/PDFLabelsFileGenerator.java
+++ src/main/java/org/ibp/api/rest/labelprinting/filegenerator/PDFLabelsFileGenerator.java
@@ -250,7 +250,8 @@ public class PDFLabelsFileGenerator implements LabelsFileGenerator {
 
 	protected String generateLabelText(final Map<Integer, String> labels, final List<Integer> selectedFields, final Map<Integer, Field> keyFieldMap, final int row) {
 		if(row < selectedFields.size()) {
-			return keyFieldMap.get(selectedFields.get(row)).getName() + FIELDNAME_VALUE_SEPARATOR + labels.get(selectedFields.get(row));
+			final String value = labels.get(selectedFields.get(row)) != null ? labels.get(selectedFields.get(row)) : "";
+			return keyFieldMap.get(selectedFields.get(row)).getName() + FIELDNAME_VALUE_SEPARATOR + value;
 		}
 		return "";
 	}
