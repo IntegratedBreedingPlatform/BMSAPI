5e2f81853d3200729a23ac47146a0156cd56ffe1
Clarysabel Tovar <clarysabel@leafnode.io>
2020-09-14 14:04:39 -0300

    Fixed validations IBP-3951

--- src/main/java/org/ibp/api/java/impl/middleware/study/validator/StudyGermplasmValidator.java
+++ src/main/java/org/ibp/api/java/impl/middleware/study/validator/StudyGermplasmValidator.java
@@ -54,7 +54,7 @@ public class StudyGermplasmValidator {
         // Check if means has dataset or advance or cross list
         boolean hasMeansDataset = this.studyService.studyHasGivenDatasetType(studyId, DatasetTypeEnum.MEANS_DATA.getId());
         if (hasMeansDataset) {
-            errors.reject("study.has.means.dataset", new Object[] {String.valueOf(studyId)}, "");
+            errors.reject("study.has.means.dataset");
         }
         boolean hasCrossesOrSelections = this.studyService.hasCrossesOrSelections(studyId);
         if (hasCrossesOrSelections) {
