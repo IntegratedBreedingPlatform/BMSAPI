f490c11fe0370a81049061e9c120e6315daaa0dd
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2020-10-21 10:49:30 +0800

    Removed unnecessary code
    IBP-4029

--- src/main/java/org/ibp/api/java/entrytype/EntryTypeService.java
+++ src/main/java/org/ibp/api/java/entrytype/EntryTypeService.java
@@ -1,8 +1,6 @@
 package org.ibp.api.java.entrytype;
 
 import org.generationcp.middleware.domain.dms.Enumeration;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
