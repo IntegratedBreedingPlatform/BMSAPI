da6ae5e28a8af8993384ff14bad4666638be2ade
Leonel Kovacic <leonel@leafnode.io>
2020-11-03 12:36:09 -0300

    IBP-4147: make programUUID a requestParam in GermplasmListResourceGroup

--- src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
+++ src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
@@ -6,9 +6,7 @@ import io.swagger.annotations.ApiParam;
 import org.generationcp.commons.pojo.treeview.TreeNode;
 import org.generationcp.middleware.api.germplasmlist.GermplasmListGeneratorDTO;
 import org.generationcp.middleware.domain.germplasm.GermplasmListTypeDTO;
-import org.generationcp.middleware.exceptions.MiddlewareException;
 import org.generationcp.middleware.pojos.workbench.PermissionsEnum;
-import org.ibp.api.domain.common.ErrorResponse;
 import org.ibp.api.java.germplasm.GermplamListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -56,7 +54,6 @@ public class GermplasmListResourceGroup {
 		return new ResponseEntity<>(this.germplamListService.create(request), HttpStatus.CREATED);
 	}
 
-
 	@ApiOperation(value = "Get germplasm lists types", notes = "Get germplasm lists types")
 	@RequestMapping(value = "/crops/{crop}/germplasm-list-types", method = RequestMethod.GET)
 	@ResponseBody
@@ -70,11 +67,11 @@ public class GermplasmListResourceGroup {
 	@ApiOperation(value = "Create germplasm list folder", notes = "Create sample list folder.")
 	//TODO: we need to add GERMPLASM_LIST permission
 	@PreAuthorize("hasAnyAuthority('ADMIN','GERMPLASM','MANAGE_GERMPLASM','GERMPLASM_LIST')")
-	@RequestMapping(value = "/crops/{crop}/programs/{programUUID}/germplasm-list-folders", method = RequestMethod.POST)
+	@RequestMapping(value = "/crops/{crop}/germplasm-list-folders", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity createGermplasmListFolder(
 		@PathVariable final String crop,
-		@PathVariable final String programUUID,
+		@RequestParam(required = false) final String programUUID,
 		@RequestParam final String folderName,
 		@RequestParam final String parentId) {
 
@@ -82,15 +79,15 @@ public class GermplasmListResourceGroup {
 		return new ResponseEntity<>(folderId, HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "Update germplasm list folder", notes = "Update germplasm list folder. ")
+	@ApiOperation(value = "Update germplasm list folder", notes = "Update germplasm list folder.")
 	//TODO: we need to add GERMPLASM_LIST permission
 	@PreAuthorize("hasAnyAuthority('ADMIN','GERMPLASM','MANAGE_GERMPLASM','GERMPLASM_LIST')")
-	@RequestMapping(value = "/crops/{crop}/programs/{programUUID}/germplasm-list-folders/{folderId}", method = RequestMethod.PUT)
+	@RequestMapping(value = "/crops/{crop}/germplasm-list-folders/{folderId}", method = RequestMethod.PUT)
 	@ResponseBody
 	public ResponseEntity updateGermplasmListFolderName(
 		@PathVariable final String crop,
-		@PathVariable final String programUUID,
 		@PathVariable final String folderId,
+		@RequestParam(required = false) final String programUUID,
 		@RequestParam final String newFolderName) {
 
 		final Integer updatedFolderId = this.germplamListService.updateGermplasmListFolderName(crop, programUUID, newFolderName, folderId);
@@ -100,12 +97,12 @@ public class GermplasmListResourceGroup {
 	@ApiOperation(value = "Move germplasm list folder.", notes = "Move germplasm list folder.")
 	//TODO: we need to add GERMPLASM_LIST permission
 	@PreAuthorize("hasAnyAuthority('ADMIN','GERMPLASM','MANAGE_GERMPLASM','GERMPLASM_LIST')")
-	@RequestMapping(value = "/crops/{crop}/programs/{programUUID}/germplasm-list-folders/{folderId}/move", method = RequestMethod.PUT)
+	@RequestMapping(value = "/crops/{crop}/germplasm-list-folders/{folderId}/move", method = RequestMethod.PUT)
 	@ResponseBody
 	public ResponseEntity moveGermplasmList(
 		@PathVariable final String crop,
-		@PathVariable final String programUUID,
 		@PathVariable final String folderId,
+		@RequestParam(required = false) final String programUUID,
 		@RequestParam final String newParentId,
 		@RequestParam final boolean isCropList) {
 
@@ -116,12 +113,12 @@ public class GermplasmListResourceGroup {
 	@ApiOperation(value = "Delete germplasm list folder", notes = "Delete germplasm sample list folder.")
 	//TODO: we need to add GERMPLASM_LIST permission
 	@PreAuthorize("hasAnyAuthority('ADMIN','GERMPLASM','MANAGE_GERMPLASM','GERMPLASM_LIST')")
-	@RequestMapping(value = "/{crop}/programs/{programUUID}/germplasm-list-folders/{folderId}", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/{crop}/germplasm-list-folders/{folderId}", method = RequestMethod.DELETE)
 	@ResponseBody
 	public ResponseEntity deleteGermplasmListFolder(
 		@PathVariable final String crop,
-		@PathVariable final String programUUID,
-		@PathVariable final String folderId) {
+		@PathVariable final String folderId,
+		@RequestParam(required = false) final String programUUID) {
 		this.germplamListService.deleteGermplamsListFolder(crop, programUUID, folderId);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
