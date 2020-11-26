35321e06371d44ad25a40b93415f1b8047c55331
Aldrin Batac <aldrin.batac@leafnode.io>
2020-11-26 09:51:41 +0800

    Code cleanup. BMS-4084

--- src/main/java/org/ibp/api/java/germplasm/GermplasmTemplateExportService.java
+++ src/main/java/org/ibp/api/java/germplasm/GermplasmTemplateExportService.java
@@ -1,12 +1,6 @@
 package org.ibp.api.java.germplasm;
 
-import org.generationcp.middleware.pojos.Method;
-import org.ibp.api.domain.germplasm.GermplasmName;
-import org.ibp.api.domain.location.LocationDto;
-import org.ibp.api.domain.ontology.VariableDetails;
-
 import java.io.File;
-import java.util.List;
 
 public interface GermplasmTemplateExportService {
 
