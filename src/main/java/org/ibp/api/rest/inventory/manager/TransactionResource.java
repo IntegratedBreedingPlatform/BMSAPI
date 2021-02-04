74fc40b63f597deac452722ecbf5d1c61170e4a6
gellisamson <gelli@leafnode.io>
2021-02-04 10:06:50 +0800

    PR Code cleanup: Remove method ObservationLabelPrintingHelper#getDefaultFileName Extract FileNameGenerator.generateFileName(file.getName()) to local variable IBP-4198-ExportFileName

--- src/main/java/org/ibp/api/rest/inventory/manager/TransactionResource.java
+++ src/main/java/org/ibp/api/rest/inventory/manager/TransactionResource.java
@@ -255,11 +255,10 @@ public class TransactionResource {
 
 		final File file = this.transactionExportServiceImpl.export(transactionDtoList);
 		final HttpHeaders headers = new HttpHeaders();
+		final String fileName = FileNameGenerator.generateFileName(file.getName());
 		headers
-			.add(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=%s", FileUtils.sanitizeFileName(FileNameGenerator
-				.generateFileName(file.getName()))));
-		headers.add(HttpHeaders.CONTENT_TYPE, String.format("%s;charset=utf-8", FileUtils.detectMimeType(FileNameGenerator
-			.generateFileName(file.getName()))));
+			.add(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=%s", FileUtils.sanitizeFileName(fileName)));
+		headers.add(HttpHeaders.CONTENT_TYPE, String.format("%s;charset=utf-8", FileUtils.detectMimeType(fileName)));
 		final FileSystemResource fileSystemResource = new FileSystemResource(file);
 		return new ResponseEntity<>(fileSystemResource, headers, HttpStatus.OK);
 	}
