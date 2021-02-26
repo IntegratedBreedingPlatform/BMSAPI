476afaf1896be1f9cfce6f68664cf60d2e8b2b11
Aldrin Batac <aldrin.batac@leafnode.io>
2021-02-26 08:29:50 +0800

    Implement equals, hashcode and toString for completeness.
    IBP-4207

--- src/main/java/org/ibp/api/domain/keysequenceregister/KeySequenceRegisterDeleteResponse.java
+++ src/main/java/org/ibp/api/domain/keysequenceregister/KeySequenceRegisterDeleteResponse.java
@@ -1,7 +1,11 @@
 package org.ibp.api.domain.keysequenceregister;
 
+import org.pojomatic.Pojomatic;
+import org.pojomatic.annotations.AutoProperty;
+
 import java.util.List;
 
+@AutoProperty
 public class KeySequenceRegisterDeleteResponse {
 
 	private List<String> deletedPrefixes;
@@ -26,6 +30,21 @@ public class KeySequenceRegisterDeleteResponse {
 
 	public void setUndeletedPrefixes(final List<String> undeletedPrefixes) {
 		this.undeletedPrefixes = undeletedPrefixes;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		return Pojomatic.equals(this, o);
+	}
+
+	@Override
+	public int hashCode() {
+		return Pojomatic.hashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		return Pojomatic.toString(this);
 	}
 
 }
