3703ceeda372f769312024dc6e66fac7aa9895b6
Aldrin Batac <aldrin.batac@leafnode.io>
2021-03-03 15:22:41 +0800

    Add todo.
    IBP-4392

--- src/main/java/org/ibp/api/brapi/v2/validation/CropValidator.java
+++ src/main/java/org/ibp/api/brapi/v2/validation/CropValidator.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// TODO: Find a centralize way to validate crop specified in URI parameter OR Request parameter.
 @Component
 public class CropValidator {
 
