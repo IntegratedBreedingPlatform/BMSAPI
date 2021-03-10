95faa9e64b1a163515115cd0e7f3dff166c3ee78
gellisamson <gelli@leafnode.io>
2021-03-10 18:25:19 +0800

    Added property germplasmUUID with jsonignore When mapping gemrplasmDbId use field germplasmUUID in v2 TransactionDto TransactionDto and StudyTransactionsDto added germplasmUUID in constructor Select germplsm_guid as germplasmUUID IBP-4349-BrapiUseGermplsmGUID

--- src/test/java/org/ibp/api/java/impl/middleware/inventory/TransactionServiceImplTest.java
+++ src/test/java/org/ibp/api/java/impl/middleware/inventory/TransactionServiceImplTest.java
@@ -1,5 +1,6 @@
 package org.ibp.api.java.impl.middleware.inventory;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.generationcp.middleware.domain.inventory.common.SearchCompositeDto;
 import org.generationcp.middleware.domain.inventory.manager.ExtendedLotDto;
 import org.generationcp.middleware.domain.inventory.manager.LotDepositRequestDto;
@@ -66,7 +67,7 @@ public class TransactionServiceImplTest {
 	public void testGetTransactions() {
 		final TransactionDto transactionDto = new TransactionDto(1, "admin", TransactionType.DEPOSIT.name(), new Double(1),new Double(0),
 			"comments", new Date(), 1, "1de85e1c-b947-4ee2-9d19-31eee6da9ad5", 1, "desig", "STOCK-1", 8314, "SEED_AMOUNT_g",
-			LotStatus.ACTIVE.name(), TransactionStatus.CONFIRMED.getValue(), 0, "UNKNOWN", "UNKNOWN", "comments");
+			LotStatus.ACTIVE.name(), TransactionStatus.CONFIRMED.getValue(), 0, "UNKNOWN", "UNKNOWN", "comments", RandomStringUtils.randomAlphanumeric(10));
 		Mockito.when(this.transactionService.searchTransactions(ArgumentMatchers.any(TransactionsSearchDto.class), ArgumentMatchers.any(
 			Pageable.class))).thenReturn(Collections.singletonList(transactionDto));
 		final Pageable pageable = Mockito.mock(Pageable.class);
