9184e3dc3f68dc1e277b0ef014451804c93d4642
cuenyad <cuenya.diego@gmail.com>
2021-04-29 09:55:00 -0300

    Adding new Dto.
    Issue: IBP-4474
    Review: none

---
+++ src/main/java/org/ibp/api/domain/user/UserProfileDto.java
@@ -0,0 +1,61 @@
+package org.ibp.api.domain.user;
+
+import org.pojomatic.Pojomatic;
+import org.pojomatic.annotations.AutoProperty;
+
+@AutoProperty
+public class UserProfileDto {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+
+	public UserProfileDto() {
+	}
+
+	public UserProfileDto(final String firstName, final String lastName, final String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(final String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(final String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Pojomatic.hashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		return Pojomatic.toString(this);
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		return Pojomatic.equals(this, o);
+	}
+
+}
