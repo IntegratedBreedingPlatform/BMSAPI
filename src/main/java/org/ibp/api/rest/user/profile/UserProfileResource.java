7c2444824450c4ccfcffe470d3557e72d095ba13
cuenyad <cuenya.diego@gmail.com>
2021-04-29 14:16:06 -0300

    Apply format code.
    Issue: IBP-4474
    Review: none

--- src/main/java/org/ibp/api/rest/user/profile/UserProfileResource.java
+++ src/main/java/org/ibp/api/rest/user/profile/UserProfileResource.java
@@ -2,24 +2,20 @@ package org.ibp.api.rest.user.profile;
 
 import io.swagger.annotations.ApiOperation;
 import org.generationcp.middleware.pojos.workbench.WorkbenchUser;
-import org.ibp.api.domain.user.UserDetailDto;
 import org.ibp.api.domain.user.UserProfileDto;
 import org.ibp.api.exception.ApiRuntimeException;
 import org.ibp.api.java.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +32,7 @@ public class UserProfileResource {
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
-	@ApiOperation(value = "Update user profile", notes = "Update first name, laste Name and email from user profile in this deployment instance of BMSAPI. ")
+	@ApiOperation(value = "Update user profile", notes = "Update First name, Laste name and Email from user profile in this deployment instance of BMSAPI. ")
 	@RequestMapping(value = "/users/profile", method = RequestMethod.PATCH)
 	@ResponseBody
 	public ResponseEntity<Integer> updateProfile(
@@ -45,10 +41,10 @@ public class UserProfileResource {
 		try {
 			final UsernamePasswordAuthenticationToken credentials = new UsernamePasswordAuthenticationToken(userProfileDto.getUserName(), userProfileDto.getPassword());
 			final Authentication authentication = this.authenticationManager.authenticate(credentials);
-		} catch (AuthenticationException e) {
-			throw new ApiRuntimeException("The password is worng, please check");
+		} catch (final AuthenticationException e) {
+			throw new ApiRuntimeException("The password is wrong, please check");
 		}
-		WorkbenchUser workbenchUser = userServiceMiddleware.getUserByUsername(userProfileDto.getUserName());
+		final WorkbenchUser workbenchUser = this.userServiceMiddleware.getUserByUsername(userProfileDto.getUserName());
 		return new ResponseEntity<>(this.userService.updateUserProfile(userProfileDto, workbenchUser), HttpStatus.OK);
 	}
 }
