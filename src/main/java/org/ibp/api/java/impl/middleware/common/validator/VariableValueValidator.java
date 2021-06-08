e905a9194991630338e36d8cce84a1c1046f9a7e
Clarysabel Tovar <clarysabel@leafnode.io>
2021-06-08 13:00:16 -0300

    Categorical variables to not support out of bounds values IBP-4440

--- src/main/java/org/ibp/api/java/impl/middleware/common/validator/VariableValueValidator.java
+++ src/main/java/org/ibp/api/java/impl/middleware/common/validator/VariableValueValidator.java
@@ -8,18 +8,23 @@ import org.generationcp.middleware.domain.ontology.DataType;
 import org.generationcp.middleware.domain.ontology.Variable;
 import org.springframework.stereotype.Component;
 
+import java.math.BigInteger;
+
 @Component
 public class VariableValueValidator {
 
 	public boolean isValidObservationValue(final Variable var, final String value) {
-		return this.isValidValue(var, value, true);
+		return this.isValidValue(var, value, true, true);
 	}
 
 	public boolean isValidAttributeValue(final Variable var, final String value) {
-		return this.isValidValue(var, value, false);
+		return this.isValidValue(var, value, false, false);
 	}
 
-	private boolean isValidValue(final Variable var, final String value, final boolean isMissingAccepted) {
+	//FIXME According to Mariano, observations should not accept invalid categories for a categorical scale
+	//FIXME invalidCategoricalScale should be removed when observations are fixed
+	private boolean isValidValue(final Variable var, final String value, final boolean isMissingAccepted,
+		final boolean isOutOfBoundsCategicalAccepted) {
 		if (StringUtils.isBlank(value)) {
 			return true;
 		}
@@ -29,6 +34,11 @@ public class VariableValueValidator {
 			return validateIfValueIsMissingOrNumber(value.trim(), isMissingAccepted);
 		} else if (var.getScale().getDataType() == DataType.DATE_TIME_VARIABLE) {
 			return new DateValidator().isValid(value, "yyyyMMdd");
+		} else if (var.getScale().getDataType() == DataType.CATEGORICAL_VARIABLE) {
+			if (isOutOfBoundsCategicalAccepted) {
+				return true;
+			}
+			return this.validateCategoricalValue(var, value);
 		}
 		return true;
 	}
@@ -38,6 +48,13 @@ public class VariableValueValidator {
 			return true;
 		}
 		return NumberUtils.isNumber(value);
+	}
+
+	private boolean validateCategoricalValue(final Variable variable, final String value) {
+		final BigInteger categoricalValueId =
+			variable.getScale().getCategories().stream().filter(category -> value.equalsIgnoreCase(category.getName())).findFirst()
+				.map(category -> BigInteger.valueOf(category.getId())).orElse(null);
+		return categoricalValueId != null;
 	}
 
 }
