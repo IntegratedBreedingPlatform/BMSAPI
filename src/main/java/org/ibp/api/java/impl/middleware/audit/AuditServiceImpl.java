c2c1265fb142c807abb2f8a129111745f2ae568a
Leonel Kovacic <leonel@leafnode.io>
2021-06-14 19:10:42 -0300

    IBP-4658: added pagination to get germplasm names changes results

--- src/main/java/org/ibp/api/java/impl/middleware/audit/AuditServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/audit/AuditServiceImpl.java
@@ -3,6 +3,7 @@ package org.ibp.api.java.impl.middleware.audit;
 import org.generationcp.middleware.service.impl.audit.GermplasmNameChangeDTO;
 import org.ibp.api.java.audit.AuditService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,8 +17,13 @@ public class AuditServiceImpl implements AuditService {
 	private org.generationcp.middleware.service.api.audit.AuditService auditService;
 
 	@Override
-	public List<GermplasmNameChangeDTO> getNameChangesByGidAndNameId(final Integer gid, final Integer nameId) {
-		return this.auditService.getNameChangesByGidAndNameId(gid, nameId);
+	public List<GermplasmNameChangeDTO> getNameChangesByGidAndNameId(final Integer gid, final Integer nameId, final Pageable pageable) {
+		return this.auditService.getNameChangesByGidAndNameId(gid, nameId, pageable);
+	}
+
+	@Override
+	public long countNameChangesByGidAndNameId(final Integer gid, final Integer nameId) {
+		return this.auditService.countNameChangesByGidAndNameId(gid, nameId);
 	}
 
 }
--- src/main/java/org/ibp/api/rest/audit/AuditResource.java
+++ src/main/java/org/ibp/api/rest/audit/AuditResource.java
@@ -1,13 +1,20 @@
 package org.ibp.api.rest.audit;
 
 import org.generationcp.middleware.service.impl.audit.GermplasmNameChangeDTO;
+import org.ibp.api.domain.common.PagedResult;
 import org.ibp.api.java.audit.AuditService;
+import org.ibp.api.rest.common.PaginatedSearch;
+import org.ibp.api.rest.common.SearchSpec;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,18 +22,40 @@ import java.util.List;
 @RestController
 public class AuditResource {
 
+	private static final String HAS_VIEW_CHANGE_HISTORY = "hasAnyAuthority('ADMIN', 'GERMPLASM', 'MANAGE_GERMPLASM', 'VIEW_CHANGE_HISTORY')";
+
 	@Autowired
 	private AuditService auditService;
 
+	@ResponseBody
 	@RequestMapping(
 		value = "/crops/{cropName}/germplasm/{gid}/name/{nameId}/changes",
 		method = RequestMethod.GET)
+	@PreAuthorize(HAS_VIEW_CHANGE_HISTORY)
 	public ResponseEntity<List<GermplasmNameChangeDTO>> getNameChanges(
 		@PathVariable final String cropName,
 		@PathVariable final Integer gid,
-		@PathVariable final Integer nameId) {
-		final List<GermplasmNameChangeDTO> namesAuditByNameId = this.auditService.getNameChangesByGidAndNameId(gid, nameId);
-		return new ResponseEntity<>(namesAuditByNameId, HttpStatus.OK);
+		@PathVariable final Integer nameId,
+		final Pageable pageable) {
+
+		final PagedResult<GermplasmNameChangeDTO> resultPage =
+			new PaginatedSearch().executeBrapiSearch(pageable.getPageNumber(), pageable.getPageSize(), new SearchSpec<GermplasmNameChangeDTO>() {
+
+				@Override
+				public long getCount() {
+					return AuditResource.this.auditService.countNameChangesByGidAndNameId(gid, nameId);
+				}
+
+				@Override
+				public List<GermplasmNameChangeDTO> getResults(final PagedResult<GermplasmNameChangeDTO> pagedResult) {
+					return AuditResource.this.auditService.getNameChangesByGidAndNameId(gid, nameId, pageable);
+				}
+			});
+
+
+		final HttpHeaders headers = new HttpHeaders();
+		headers.add("X-Total-Count", Long.toString(resultPage.getTotalResults()));
+		return new ResponseEntity<>(resultPage.getPageResults(), headers, HttpStatus.OK);
 	}
 
 }
