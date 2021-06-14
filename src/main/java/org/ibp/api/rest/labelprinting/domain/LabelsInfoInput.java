4e8bc19405758e0a8b4df58d85d60768b6c72281
cuenyad <cuenya.diego@gmail.com>
2021-06-14 09:46:32 -0300

    Adding programUUID.
    Issue: IBP-4659
    Review: none

--- src/main/java/org/ibp/api/rest/labelprinting/domain/LabelsInfoInput.java
+++ src/main/java/org/ibp/api/rest/labelprinting/domain/LabelsInfoInput.java
@@ -10,6 +10,7 @@ public class LabelsInfoInput {
 	private Integer datasetId;
 	private Integer searchRequestId;
 	private String  sortBy;
+	private String programUUID;
 
 	public Integer getStudyId() {
 		return studyId;
@@ -41,6 +42,14 @@ public class LabelsInfoInput {
 
 	public void setSortBy(final String sortBy) {
 		this.sortBy = sortBy;
+	}
+
+	public String getProgramUUID() {
+		return this.programUUID;
+	}
+
+	public void setProgramUUID(final String programUUID) {
+		this.programUUID = programUUID;
 	}
 
 	@Override
