5348b8329c7ebbc25987c05a2f75e077d44cc6e2
Gelli Samson <gelli@leafnode.io>
2021-06-30 18:05:57 +0800

    Added get samples to solve issue missing required resources SampleBrapiValidators

--- src/main/java/org/ibp/api/brapi/v1/sample/SampleResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v1/sample/SampleResourceBrapi.java
@@ -3,24 +3,35 @@ package org.ibp.api.brapi.v1.sample;
 import com.google.common.collect.ImmutableMap;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
+import org.generationcp.middleware.domain.sample.SampleDTO;
 import org.generationcp.middleware.domain.sample.SampleDetailsDTO;
+import org.ibp.api.brapi.v1.common.BrapiPagedResult;
+import org.ibp.api.brapi.v1.common.EntityListResponse;
 import org.ibp.api.brapi.v1.common.Metadata;
 import org.ibp.api.brapi.v1.common.Pagination;
+import org.ibp.api.brapi.v1.common.Result;
+import org.ibp.api.domain.common.PagedResult;
 import org.ibp.api.domain.sample.SampleObservationDto;
 import org.ibp.api.domain.sample.SampleObservationMapper;
 import org.ibp.api.domain.sample.SampleSummaryDto;
 import org.ibp.api.java.impl.middleware.sample.SampleService;
+import org.ibp.api.rest.common.PaginatedSearch;
+import org.ibp.api.rest.common.SearchSpec;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +42,60 @@ public class SampleResourceBrapi {
 
 	@Autowired
 	private SampleService sampleService;
+
+
+	@ApiOperation(value = "Get a sample by sampleDbId", notes = "Get a sample by sampleDbId")
+	@RequestMapping(value = "/{crop}/brapi/v1/samples", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<EntityListResponse<SampleObservationDto>> getSamples(
+			@PathVariable final String crop,
+			@RequestParam(required = false) final String sampleDbId,
+			@RequestParam(required = false) final String observationUnitDbId,
+			@RequestParam(required = false) final String plateDbId,
+			@RequestParam(required = false) final String germplasmDbId,
+			@ApiParam(value = BrapiPagedResult.CURRENT_PAGE_DESCRIPTION)
+			@RequestParam(value = "page",
+					required = false) final Integer currentPage,
+			@ApiParam(value = BrapiPagedResult.PAGE_SIZE_DESCRIPTION)
+			@RequestParam(value = "pageSize",
+					required = false) final Integer pageSize
+	) {
+		final int finalPageNumber = currentPage == null ? BrapiPagedResult.DEFAULT_PAGE_NUMBER : currentPage;
+		final int finalPageSize = pageSize == null ? BrapiPagedResult.DEFAULT_PAGE_SIZE : pageSize;
+
+		final PagedResult<SampleDTO> resultPage = new PaginatedSearch().executeBrapiSearch(finalPageNumber, finalPageSize,
+				new SearchSpec<SampleDTO>() {
+
+					@Override
+					public long getCount() {
+						return SampleResourceBrapi.this.sampleService.countFilter(observationUnitDbId, null);
+					}
+
+					@Override
+					public List<SampleDTO> getResults(final PagedResult<SampleDTO> pagedResult) {
+						return SampleResourceBrapi.this.sampleService.filter(observationUnitDbId, null, new PageRequest(finalPageNumber, finalPageSize));
+					}
+				});
+		final ModelMapper modelMapper = SampleObservationMapper.getInstance();
+		final List<SampleObservationDto> sampleObservationDtos = new ArrayList<>();
+		for (final SampleDTO sampleDTO : resultPage.getPageResults()) {
+			final SampleObservationDto dto = modelMapper.map(sampleDTO, SampleObservationDto.class);
+			dto.setSampleDbId(sampleDTO.getSampleBusinessKey());
+			sampleObservationDtos.add(dto);
+		}
+
+		final Result<SampleObservationDto> result = new Result<SampleObservationDto>().withData(sampleObservationDtos);
+
+		Pagination pagination =
+				new Pagination().withPageNumber(1).withPageSize(resultPage.getPageSize())
+						.withTotalCount(resultPage.getTotalResults())
+						.withTotalPages(resultPage.getTotalPages());
+
+		Metadata metadata = new Metadata().withPagination(pagination);
+
+		final EntityListResponse<SampleObservationDto> entityListResponse = new EntityListResponse<>(metadata, result);
+		return new ResponseEntity<>(entityListResponse, HttpStatus.OK);
+	}
 
 	@ApiOperation(value = "Get a sample by sampleDbId", notes = "Get a sample by sampleDbId")
 	@RequestMapping(value = "/{crop}/brapi/v1/samples/{sampleDbId}", method = RequestMethod.GET)
