509df89bf9140685bef405ea5817ef1fa05a4f36
Nahuel Soldevilla <nahuel@leafnode.io>
2021-07-08 18:06:56 -0300

    IBP-4732: save filename to phenotype after upload

--- src/main/java/org/ibp/api/java/impl/middleware/file/FileMetadataServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/file/FileMetadataServiceImpl.java
@@ -35,7 +35,7 @@ public class FileMetadataServiceImpl implements FileMetadataService {
 	public Image updateImageContent(final String imageDbId, final byte[] imageContent) {
 		final FileMetadataDTO fileMetadataDTO = this.fileMetadataService.getFileMetadataByUUID(imageDbId);
 		this.fileStorageService.upload(FileUtils.wrapAsMultipart(imageContent), fileMetadataDTO.getPath());
-		this.fileMetadataService.linkFileToObservation(fileMetadataDTO);
+		this.fileMetadataService.saveFilenameToObservation(fileMetadataDTO);
 
 		final FileMetadataMapper fileMetadataMapper = new FileMetadataMapper();
 		return fileMetadataMapper.map(fileMetadataDTO);
