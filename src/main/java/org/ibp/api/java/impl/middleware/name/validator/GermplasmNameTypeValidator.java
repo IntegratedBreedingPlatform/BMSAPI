e8d38e252293ff260b35f02efece46d0d427a69d
cuenyad <cuenya.diego@gmail.com>
2021-07-22 15:06:11 -0300

    Adding validation for edit and delete name types.
    Issue: IBP-4794
    Review: none

--- src/main/java/org/ibp/api/java/impl/middleware/name/validator/GermplasmNameTypeValidator.java
+++ src/main/java/org/ibp/api/java/impl/middleware/name/validator/GermplasmNameTypeValidator.java
@@ -1,9 +1,11 @@
 package org.ibp.api.java.impl.middleware.name.validator;
 
 import org.apache.commons.lang3.StringUtils;
+import org.generationcp.middleware.api.germplasm.GermplasmNameService;
 import org.generationcp.middleware.api.nametype.GermplasmNameTypeDTO;
 import org.generationcp.middleware.api.nametype.GermplasmNameTypeRequestDTO;
 import org.generationcp.middleware.api.nametype.GermplasmNameTypeService;
+import org.generationcp.middleware.pojos.Name;
 import org.ibp.api.exception.ApiRequestValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.MapBindingResult;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Component
@@ -21,14 +25,68 @@ public class GermplasmNameTypeValidator {
 
 	public static final Integer CODE_OR_NAME_MAX_LENGTH = 50;
 	public static final Integer DESCRIPTION_MAX_LENGTH = 255;
+	public static final List<String> NAME_TYPES_USED_FOR_THE_SYSTEM = Collections.unmodifiableList(
+		Arrays.asList("LNAME", "CODE1", "CODE2", "CODE3", "PEDIGREE", "CROSS NAME", "CROSSING NAME", "DERIVATIVE NAME"));
+
 	private BindingResult errors;
 
 	@Autowired
 	private GermplasmNameTypeService germplasmNameTypeService;
 
-	public void validate(final GermplasmNameTypeRequestDTO germplasmNameTypeRequestDTO) {
+	@Autowired
+	private GermplasmNameService germplasmNameService;
+
+	public void validate(final GermplasmNameTypeRequestDTO germplasmNameTypeRequestDTO, final Integer nameTypeId) {
 		this.errors = new MapBindingResult(new HashMap<>(), GermplasmNameTypeRequestDTO.class.getName());
 
+		if (nameTypeId != null) {
+			this.validateExistingNameType(nameTypeId);
+
+			if (StringUtils.isNotBlank(germplasmNameTypeRequestDTO.getCode())) {
+				this.validateNameTypeCode(germplasmNameTypeRequestDTO);
+				this.validateIfCodeAlreadyExists(germplasmNameTypeRequestDTO.getCode(), nameTypeId);
+
+			}
+
+			if (StringUtils.isNotBlank(germplasmNameTypeRequestDTO.getName())) {
+				this.validateNameTypeName(germplasmNameTypeRequestDTO);
+				this.validateIfNameAlreadyExists(germplasmNameTypeRequestDTO.getName(), nameTypeId);
+			}
+
+		} else {
+
+			this.validateNameTypeCode(germplasmNameTypeRequestDTO);
+			this.validateIfCodeAlreadyExists(germplasmNameTypeRequestDTO.getCode(), nameTypeId);
+			this.validateNameTypeName(germplasmNameTypeRequestDTO);
+			this.validateIfNameAlreadyExists(germplasmNameTypeRequestDTO.getName(), nameTypeId);
+		}
+
+		this.validateNameTypeDescription(germplasmNameTypeRequestDTO);
+	}
+
+	private void validateNameTypeDescription(final GermplasmNameTypeRequestDTO germplasmNameTypeRequestDTO) {
+		if (StringUtils.isNotBlank(germplasmNameTypeRequestDTO.getDescription()) && //
+			germplasmNameTypeRequestDTO.getDescription().length() > GermplasmNameTypeValidator.DESCRIPTION_MAX_LENGTH) {
+			this.errors.reject("germplasm.name.type.description.length.invalid",
+				new String[] {GermplasmNameTypeValidator.DESCRIPTION_MAX_LENGTH.toString()}, "");
+			throw new ApiRequestValidationException(this.errors.getAllErrors());
+		}
+	}
+
+	private void validateNameTypeName(final GermplasmNameTypeRequestDTO germplasmNameTypeRequestDTO) {
+		if (StringUtils.isBlank(germplasmNameTypeRequestDTO.getName())) {
+			this.errors.reject("germplasm.name.type.name.empty", "");
+			throw new ApiRequestValidationException(this.errors.getAllErrors());
+		}
+
+		if (germplasmNameTypeRequestDTO.getName().length() > GermplasmNameTypeValidator.CODE_OR_NAME_MAX_LENGTH) {
+			this.errors.reject("germplasm.name.type.name.length.invalid",
+				new String[] {GermplasmNameTypeValidator.CODE_OR_NAME_MAX_LENGTH.toString()}, "");
+			throw new ApiRequestValidationException(this.errors.getAllErrors());
+		}
+	}
+
+	private void validateNameTypeCode(final GermplasmNameTypeRequestDTO germplasmNameTypeRequestDTO) {
 		if (StringUtils.isBlank(germplasmNameTypeRequestDTO.getCode())) {
 			this.errors.reject("germplasm.name.type.code.empty", "");
 			throw new ApiRequestValidationException(this.errors.getAllErrors());
@@ -40,44 +98,66 @@ public class GermplasmNameTypeValidator {
 				new String[] {GermplasmNameTypeValidator.CODE_OR_NAME_MAX_LENGTH.toString()}, "");
 			throw new ApiRequestValidationException(this.errors.getAllErrors());
 		}
-
-		this.validateIfCodeAlreadyExists(germplasmNameTypeRequestDTO.getCode());
-
-		if (StringUtils.isBlank(germplasmNameTypeRequestDTO.getName())) {
-			this.errors.reject("germplasm.name.type.name.empty", "");
-			throw new ApiRequestValidationException(this.errors.getAllErrors());
-		}
-
-		if (germplasmNameTypeRequestDTO.getName().length() > GermplasmNameTypeValidator.CODE_OR_NAME_MAX_LENGTH) {
-			this.errors.reject("germplasm.name.type.name.length.invalid",
-				new String[] {GermplasmNameTypeValidator.CODE_OR_NAME_MAX_LENGTH.toString()}, "");
-			throw new ApiRequestValidationException(this.errors.getAllErrors());
-		}
-
-		this.validateIfNameAlreadyExists(germplasmNameTypeRequestDTO.getName());
-
-		if (StringUtils.isNotBlank(germplasmNameTypeRequestDTO.getDescription()) && //
-			germplasmNameTypeRequestDTO.getDescription().length() > GermplasmNameTypeValidator.DESCRIPTION_MAX_LENGTH) {
-			this.errors.reject("germplasm.name.type.description.length.invalid",
-				new String[] {GermplasmNameTypeValidator.DESCRIPTION_MAX_LENGTH.toString()}, "");
-			throw new ApiRequestValidationException(this.errors.getAllErrors());
-		}
 	}
 
-	protected void validateIfCodeAlreadyExists(final String code) {
+	protected void validateIfCodeAlreadyExists(final String code, final Integer nameTypeId) {
 		final Set<String> codes = new HashSet<>(Arrays.asList(code));
 		final List<GermplasmNameTypeDTO> nameTypeDTOS = this.germplasmNameTypeService.filterGermplasmNameTypes(codes);
-		if (nameTypeDTOS != null && !nameTypeDTOS.isEmpty()) {
+
+		if (nameTypeDTOS.size() > 1 || nameTypeDTOS.size() == 1 && nameTypeId != null && !nameTypeDTOS.get(0).getId().equals(nameTypeId)) {
 			this.errors.reject("germplasm.name.type.code.invalid", new String[] {code}, "");
 			throw new ApiRequestValidationException(this.errors.getAllErrors());
 		}
 	}
 
-	protected void validateIfNameAlreadyExists(final String name) {
+	protected void validateIfNameAlreadyExists(final String name, final Integer nameTypeId) {
 		final List<GermplasmNameTypeDTO> nameTypeDTOS = this.germplasmNameTypeService.filterGermplasmNameTypesByName(name);
-		if (!nameTypeDTOS.isEmpty()) {
+		if (nameTypeDTOS.size() > 1 || nameTypeDTOS.size() == 1 && nameTypeId != null && !nameTypeDTOS.get(0).getId().equals(nameTypeId)) {
 			this.errors.reject("germplasm.name.type.name.invalid", new String[] {name}, "");
 			throw new ApiRequestValidationException(this.errors.getAllErrors());
 		}
+	}
+
+	private void validateExistingNameType(final Integer nameTypeId) {
+		final Optional<GermplasmNameTypeDTO> germplasmNameTypeDTO = this.germplasmNameTypeService.getNameTypeById(nameTypeId);
+
+		if (!germplasmNameTypeDTO.isPresent()) {
+			this.errors.reject("germplasm.name.type.invalid", "");
+			throw new ApiRequestValidationException(this.errors.getAllErrors());
+		}
+
+		this.validateNameTypeBelongsToSystem(germplasmNameTypeDTO.get().getCode());
+		this.validateNameTypeBelongsToGermplasm(nameTypeId);
+		this.validateNameTypeBelongsToGermplasmList(germplasmNameTypeDTO.get().getName());
+	}
+
+	private void validateNameTypeBelongsToSystem(final String code) {
+		if (GermplasmNameTypeValidator.NAME_TYPES_USED_FOR_THE_SYSTEM.contains(code)) {
+			this.errors.reject("germplasm.name.type.used.for.the.system", "");
+			throw new ApiRequestValidationException(this.errors.getAllErrors());
+		}
+	}
+
+	private void validateNameTypeBelongsToGermplasm(final Integer nameTypeId) {
+		final List<Name> names = this.germplasmNameService.getGermplasmNamesByNameTypeId(nameTypeId);
+		if (!names.isEmpty()) {
+			this.errors.reject("germplasm.name.type.is.in.used", "");
+			throw new ApiRequestValidationException(this.errors.getAllErrors());
+		}
+	}
+
+	private void validateNameTypeBelongsToGermplasmList(final String name) {
+		final boolean isNameTypeAssociatedToList =
+			this.germplasmNameTypeService.existNameTypeUsedInListDataProp(name);
+
+		if (isNameTypeAssociatedToList) {
+			this.errors.reject("germplasm.name.type.asociated.to.list", "");
+			throw new ApiRequestValidationException(this.errors.getAllErrors());
+		}
+	}
+
+	public void deletable(final Integer nameTypeId) {
+		this.errors = new MapBindingResult(new HashMap<>(), GermplasmNameTypeRequestDTO.class.getName());
+		this.validateExistingNameType(nameTypeId);
 	}
 }
