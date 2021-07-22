a1b7ae38ec3d59c6aca20bd0cb72d9874d317a4f
Nahuel Soldevilla <nahuel@leafnode.io>
2021-07-22 12:30:20 -0300

    IBP-4815: list filemetadata by observationid

---
+++ src/main/java/org/ibp/api/rest/file/FileMetadataResource.java
@@ -0,0 +1,35 @@
+package org.ibp.api.rest.file;
+
+import io.swagger.annotations.Api;
+import org.generationcp.middleware.api.file.FileMetadataDTO;
+import org.generationcp.middleware.api.file.FileMetadataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Separated from FileResource mostly because of wildcard get (files/**)
+ */
+@Api("FileMetadata services")
+@RequestMapping("/crops/{cropName}")
+@RestController
+public class FileMetadataResource {
+
+	@Autowired
+	private FileMetadataService fileMetadataService;
+
+	@RequestMapping(value = "/filemetadata", method = RequestMethod.GET)
+	public ResponseEntity<List<FileMetadataDTO>> list(
+		@PathVariable final String cropName,
+		@RequestParam final Integer observationId
+	) {
+		return new ResponseEntity<>(this.fileMetadataService.list(observationId), HttpStatus.OK);
+	}
+}
