1bd4286605a98b26259cf6ee8e76f062ffc39181
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2021-07-29 10:36:56 +0800

    updated unit test
    IBP-4791

--- src/test/java/org/ibp/api/brapi/v1/sample/SampleResourceBrapiTest.java
+++ src/test/java/org/ibp/api/brapi/v1/sample/SampleResourceBrapiTest.java
@@ -1,12 +1,12 @@
 
 package org.ibp.api.brapi.v1.sample;
 
+import com.beust.jcommander.internal.Lists;
 import org.generationcp.commons.util.DateUtil;
-import org.generationcp.middleware.domain.sample.SampleDetailsDTO;
 import org.generationcp.middleware.domain.search_request.brapi.v2.SampleSearchRequestDTO;
+import org.generationcp.middleware.service.api.sample.SampleObservationDto;
 import org.hamcrest.Matchers;
 import org.ibp.ApiUnitTestBase;
-import org.ibp.api.java.impl.middleware.SampleTestDataGenerator;
 import org.ibp.api.java.impl.middleware.sample.SampleService;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -22,6 +22,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 
 public class SampleResourceBrapiTest extends ApiUnitTestBase {
 
@@ -51,29 +55,27 @@ public class SampleResourceBrapiTest extends ApiUnitTestBase {
 		final String sampleId = "a1w2xed2f";
 		final UriComponents uriComponents = UriComponentsBuilder.newInstance().path("/maize/brapi/v1/samples/" + sampleId).build().encode();
 
+		final SampleObservationDto sampleObservationDto = this.createRandomSampleObservationDto();
 		final SampleSearchRequestDTO requestDTO = new SampleSearchRequestDTO();
 		requestDTO.setSampleDbId(sampleId);
-		Mockito.when(this.sampleService.getSampleObservations(requestDTO, null)).thenReturn(new ArrayList<>());
-/*
+		Mockito.when(this.sampleService.getSampleObservations(requestDTO, null)).thenReturn(Lists.newArrayList(sampleObservationDto));
+
 		this.mockMvc.perform(MockMvcRequestBuilders.get(uriComponents.toUriString()).contentType(this.contentType))
 			.andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.studyDbId", Matchers.is(sampleDetailsDTO.getStudyDbId().toString()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.observationUnitDbId", Matchers.is(sampleDetailsDTO.getObsUnitId()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.sampleDbId", Matchers.is(sampleDetailsDTO.getSampleBusinessKey()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.takenBy", Matchers.is(sampleDetailsDTO.getTakenBy()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.sampleType", Matchers.is(sampleDetailsDTO.getSampleType()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.tissueType", Matchers.is(sampleDetailsDTO.getTissueType()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.notes", Matchers.is(sampleDetailsDTO.getNotes()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.germplasmDbId", Matchers.is(sampleDetailsDTO.getGermplasmUUID()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.plateDbId", Matchers.is(sampleDetailsDTO.getPlateId()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.plateIndex", Matchers.is(sampleDetailsDTO.getSampleNumber()))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.result.plotDbId", Matchers.is(sampleDetailsDTO.getPlotNo().toString()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.studyDbId", Matchers.is(sampleObservationDto.getStudyDbId()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.observationUnitDbId", Matchers.is(sampleObservationDto.getObservationUnitDbId()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.sampleDbId", Matchers.is(sampleObservationDto.getSampleDbId()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.takenBy", Matchers.is(sampleObservationDto.getTakenBy()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.sampleType", Matchers.is(sampleObservationDto.getSampleType()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.tissueType", Matchers.is(sampleObservationDto.getTissueType()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.notes", Matchers.is(sampleObservationDto.getNotes()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.germplasmDbId", Matchers.is(sampleObservationDto.getGermplasmDbId()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.plateDbId", Matchers.is(sampleObservationDto.getPlateDbId()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.plateIndex", Matchers.is(sampleObservationDto.getPlateIndex()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.plotDbId", Matchers.is(sampleObservationDto.getPlotDbId()))) //
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result.plantDbId", Matchers.is(sampleObservationDto.getPlantDbId()))) //
 			.andExpect(MockMvcResultMatchers.jsonPath("$.result.sampleTimestamp",
-				Matchers.is(SampleResourceBrapiTest.DATE_FORMAT.format(sampleDetailsDTO.getSampleDate())))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.metadata.pagination.currentPage", Matchers.is(1))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.metadata.pagination.pageSize", Matchers.is(1))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.metadata.pagination.totalCount", Matchers.is(1))) //
-			.andExpect(MockMvcResultMatchers.jsonPath("$.metadata.pagination.totalPages", Matchers.is(1))) //*/
+				Matchers.is(SampleResourceBrapiTest.DATE_FORMAT.format(sampleObservationDto.getSampleTimestamp())))); //*/
 
 	}
 
@@ -93,8 +95,28 @@ public class SampleResourceBrapiTest extends ApiUnitTestBase {
 
 		this.mockMvc.perform(MockMvcRequestBuilders.get(uriComponents.toUriString()).contentType(this.contentType))
 			.andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isNotFound())
-			.andExpect(MockMvcResultMatchers.jsonPath("$.metadata.pagination", Matchers.isEmptyOrNullString())) //
 			.andExpect(MockMvcResultMatchers.jsonPath("$.metadata.status[0].message", Matchers.is("not found sample"))) //
 		;
+	}
+
+	private SampleObservationDto createRandomSampleObservationDto() {
+		final SampleObservationDto sampleObservationDto =
+				new SampleObservationDto();
+		sampleObservationDto.setGermplasmDbId(randomAlphanumeric(10));
+		sampleObservationDto.setNotes(randomAlphanumeric(6));
+		sampleObservationDto.setObservationUnitDbId(randomAlphanumeric(10));
+		sampleObservationDto.setPlantDbId(randomAlphanumeric(5));
+		sampleObservationDto.setPlateDbId(randomAlphanumeric(10));
+		sampleObservationDto.setPlateIndex(Integer.valueOf(randomNumeric(2)));
+		sampleObservationDto.setPlotDbId(randomAlphanumeric(2));
+		sampleObservationDto.setSampleDbId(randomAlphanumeric(10));
+		sampleObservationDto.setSampleTimestamp(new Date());
+		sampleObservationDto.setTakenBy(randomAlphanumeric(6));
+		sampleObservationDto.setStudyDbId(randomNumeric(2));
+		sampleObservationDto.setSampleType(randomAlphanumeric(6));
+		sampleObservationDto.setTissueType(randomAlphanumeric(6));
+
+
+		return sampleObservationDto;
 	}
 }
