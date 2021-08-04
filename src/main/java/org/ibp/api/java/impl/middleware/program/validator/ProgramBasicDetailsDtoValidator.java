d8cc9b5dcec17cc91c7f31294497960f2b46174e
Clarysabel Tovar <clarysabel@leafnode.io>
2021-08-04 10:37:45 -0300

    Fixed validations IBP-4839

--- src/main/java/org/ibp/api/java/impl/middleware/program/validator/ProgramBasicDetailsDtoValidator.java
+++ src/main/java/org/ibp/api/java/impl/middleware/program/validator/ProgramBasicDetailsDtoValidator.java
@@ -1,11 +1,10 @@
 package org.ibp.api.java.impl.middleware.program.validator;
 
-import org.generationcp.commons.util.DateUtil;
 import org.generationcp.middleware.api.program.ProgramBasicDetailsDto;
 import org.generationcp.middleware.api.program.ProgramDTO;
 import org.generationcp.middleware.api.program.ProgramService;
 import org.generationcp.middleware.domain.workbench.AddProgramMemberRequestDto;
-import org.generationcp.middleware.service.api.program.ProgramSearchRequest;
+import org.generationcp.middleware.util.Util;
 import org.ibp.api.exception.ApiRequestValidationException;
 import org.ibp.api.java.impl.middleware.common.validator.BaseValidator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.MapBindingResult;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ProgramBasicDetailsDtoValidator {
@@ -31,31 +30,24 @@ public class ProgramBasicDetailsDtoValidator {
 		BaseValidator.checkNotEmpty(programBasicDetailsDto.getName(), "param.null", new String[] {"name"});
 		BaseValidator.checkNotEmpty(programBasicDetailsDto.getStartDate(), "param.null", new String[] {"startDate"});
 
-		//check that is not empty and does not contain only blank spaces
 		if (programBasicDetailsDto.getName().trim().isEmpty()) {
 			errors.reject("program.name.empty", "");
 			throw new ApiRequestValidationException(errors.getAllErrors());
 		}
 
-		if (!DateUtil.isValidDate(programBasicDetailsDto.getStartDate())) {
+		if (Util.tryParseDate(programBasicDetailsDto.getStartDate(), Util.FRONTEND_DATE_FORMAT) == null) {
 			errors.reject("program.start.date.invalid", "");
 			throw new ApiRequestValidationException(errors.getAllErrors());
 		}
 
-		//check program name length
 		if (programBasicDetailsDto.getName().length() > PROGRAM_NAME_MAX_LENGTH) {
 			errors.reject("program.name.max.length.exceeded", "");
 			throw new ApiRequestValidationException(errors.getAllErrors());
 		}
 
-		//check that program name does not exist in that crop
-		final ProgramSearchRequest programSearchRequest = new ProgramSearchRequest();
-		programSearchRequest.setCommonCropName(cropName);
-		programSearchRequest.setProgramName(programBasicDetailsDto.getName());
-
-		final List<ProgramDTO> programDTOList = programService.filterPrograms(programSearchRequest, null);
-		if (!programDTOList.isEmpty()) {
-			errors.reject("program.name.max.length.exceeded", new String[] {programBasicDetailsDto.getName(), cropName}, "");
+		final Optional<ProgramDTO> programDTOOptional = this.programService.getProject(cropName, programBasicDetailsDto.getName());
+		if (programDTOOptional.isPresent()) {
+			errors.reject("program.name.already.exists", new String[] {programBasicDetailsDto.getName(), cropName}, "");
 			throw new ApiRequestValidationException(errors.getAllErrors());
 		}
 
