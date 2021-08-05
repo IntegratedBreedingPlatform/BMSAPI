14ff3618611241bf6be02c81d172fea1626d0071
Nahuel Soldevilla <nahuel@leafnode.io>
2021-08-05 16:00:58 -0300

    IBP-4815: Add pagination to file metadata list

--- src/main/java/org/ibp/api/rest/file/FileMetadataResource.java
+++ src/main/java/org/ibp/api/rest/file/FileMetadataResource.java
@@ -1,16 +1,24 @@
 package org.ibp.api.rest.file;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.generationcp.middleware.api.file.FileMetadataDTO;
+import org.generationcp.middleware.api.file.FileMetadataFilterRequest;
 import org.generationcp.middleware.api.file.FileMetadataService;
+import org.ibp.api.domain.common.PagedResult;
+import org.ibp.api.rest.common.PaginatedSearch;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
@@ -25,14 +33,23 @@ public class FileMetadataResource {
 	@Autowired
 	private FileMetadataService fileMetadataService;
 
-	@RequestMapping(value = "/filemetadata", method = RequestMethod.GET)
-	public ResponseEntity<List<FileMetadataDTO>> list(
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "page", dataType = "integer", paramType = "query",
+			value = "page number. Start at " + PagedResult.DEFAULT_PAGE_NUMBER),
+		@ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+			value = "Number of records per page.")
+	})
+	@RequestMapping(value = "/filemetadata/search", method = RequestMethod.POST)
+	public ResponseEntity<List<FileMetadataDTO>> search(
 		@PathVariable final String cropName,
-		@RequestParam final String observationUnitUUID,
 		@RequestParam(required = false) final String programUUID,
-		@RequestParam(required = false) final String variableName,
-		@RequestParam(required = false) final String fileName
+		@RequestBody final FileMetadataFilterRequest filterRequest,
+		@ApiIgnore @PageableDefault(page = PagedResult.DEFAULT_PAGE_NUMBER, size = PagedResult.DEFAULT_PAGE_SIZE) final Pageable pageable
 	) {
-		return new ResponseEntity<>(this.fileMetadataService.list(observationUnitUUID, programUUID, variableName, fileName), HttpStatus.OK);
+		return new PaginatedSearch().getPagedResult(
+			() -> this.fileMetadataService.countSearch(filterRequest, programUUID, pageable),
+			() -> this.fileMetadataService.search(filterRequest, programUUID, pageable),
+			pageable
+		);
 	}
 }
