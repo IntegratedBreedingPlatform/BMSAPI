b7cb9d79b0ae11613d83e8afd6f85a24e6ef4846
cuenyad <cuenya.diego@gmail.com>
2021-08-10 14:01:21 -0300

    Set x-frame-options to header response to prevent Clickjacking
    Issue: IBP-4806
    Review: none

--- src/main/java/org/ibp/BmsApiFilter.java
+++ src/main/java/org/ibp/BmsApiFilter.java
@@ -29,6 +29,7 @@ public class BmsApiFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token, Authorization, Content-type");
+		response.setHeader("x-frame-options", "SAMEORIGIN");
 
 		// Specific Cache control setting. An issue is caused by caching in IE9 and IE10. The GET requests to retrieve the variables,
 		// properties, methods or scales after one has been added are not executed again. IE will only execute the GET request again after
