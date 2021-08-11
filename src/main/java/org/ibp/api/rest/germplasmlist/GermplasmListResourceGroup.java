cb2aef30eacfba0bf957ca5dec3054472c98f21d
cuenyad <cuenya.diego@gmail.com>
2021-08-11 17:06:30 -0300

    Adding LISTS and GERMPLASM_LISTS permissions to allow creating Germplasm Lists.
    Issue: IBP-4840
    Review: none

--- src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
+++ src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
@@ -62,7 +62,7 @@ GermplasmListResourceGroup {
 
 	@ApiOperation(value = "Create a new Germplasm list")
 	@RequestMapping(value = "/crops/{crop}/germplasm-lists", method = RequestMethod.POST)
-	@PreAuthorize("hasAnyAuthority('ADMIN', 'GERMPLASM', 'MANAGE_GERMPLASM', 'IMPORT_GERMPLASM')")
+	@PreAuthorize("hasAnyAuthority('ADMIN', 'GERMPLASM', 'MANAGE_GERMPLASM', 'IMPORT_GERMPLASM', 'LISTS', 'GERMPLASM_LISTS')")
 	@ResponseBody
 	public ResponseEntity<GermplasmListGeneratorDTO> create(
 		@ApiParam(required = true) @PathVariable final String crop,
