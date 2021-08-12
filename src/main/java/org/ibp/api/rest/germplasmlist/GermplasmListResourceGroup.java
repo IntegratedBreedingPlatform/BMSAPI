68cb69317b0f63b47869f68a7c2c86a8e8273d44
cuenyad <cuenya.diego@gmail.com>
2021-08-12 09:22:43 -0300

    Adding comment.
    Issue: IBP-4840
    Review: none

--- src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
+++ src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
@@ -62,6 +62,7 @@ GermplasmListResourceGroup {
 
 	@ApiOperation(value = "Create a new Germplasm list")
 	@RequestMapping(value = "/crops/{crop}/germplasm-lists", method = RequestMethod.POST)
+	// TODO The Permissions will be change after implement IBP-4570 (New list manager)
 	@PreAuthorize("hasAnyAuthority('ADMIN', 'GERMPLASM', 'MANAGE_GERMPLASM', 'IMPORT_GERMPLASM', 'LISTS', 'GERMPLASM_LISTS')")
 	@ResponseBody
 	public ResponseEntity<GermplasmListGeneratorDTO> create(
