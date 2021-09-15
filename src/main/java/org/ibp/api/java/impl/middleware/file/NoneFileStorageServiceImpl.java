70ced3126686c91d6354965a8b5e9e75d80d2ab7
darla-leafnode <darla@leafnode.io>
2021-09-15 09:36:33 +0800

    Merge pull request #588 from IntegratedBreedingPlatform/IBP-5009-FixFileStorageFeatureAlwaysOn
    IBP-5009: NoneFileStorageServiceImpl.isConfigured should be always false

--- src/main/java/org/ibp/api/java/impl/middleware/file/NoneFileStorageServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/file/NoneFileStorageServiceImpl.java
@@ -4,27 +4,27 @@ import org.ibp.api.exception.ApiRuntime2Exception;
 import org.ibp.api.java.file.FileStorageService;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Map;
-
 public class NoneFileStorageServiceImpl implements FileStorageService {
+
+	public static final String FILE_STORAGE_NOT_CONFIGURED_ERROR_CODE = "file.storage.not.configured";
 
 	@Override
 	public void upload(final MultipartFile file, final String path) {
-		throw new ApiRuntime2Exception("", "file.storage.not.configured");
+		throw new ApiRuntime2Exception("", FILE_STORAGE_NOT_CONFIGURED_ERROR_CODE);
 	}
 
 	@Override
 	public byte[] getFile(final String path) {
-		throw new ApiRuntime2Exception("", "file.storage.not.configured");
+		throw new ApiRuntime2Exception("", FILE_STORAGE_NOT_CONFIGURED_ERROR_CODE);
 	}
 
 	@Override
 	public boolean isConfigured() {
-		return true;
+		return false;
 	}
 
 	@Override
 	public void deleteFile(final String path) {
-		throw new ApiRuntime2Exception("", "file.storage.not.configured");
+		throw new ApiRuntime2Exception("", FILE_STORAGE_NOT_CONFIGURED_ERROR_CODE);
 	}
 }
