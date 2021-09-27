96ebee05064df4993ad7c6e6e82cc378ceb506cc
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2021-09-27 14:04:20 +0800

    Added controller value
    IBP-5014 / IBP-5053

--- src/main/java/org/ibp/api/brapi/v2/variable/VariableResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v2/variable/VariableResourceBrapi.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Api(value = "BrAPI V2 Variable Services")
-@Controller
+@Controller(value = "VariableResourceBrapiV2")
 public class VariableResourceBrapi {
 
     @Autowired
