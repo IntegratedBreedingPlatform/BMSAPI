2282685e84c38208f14f7f902a0033c8784d59e8
Marc Ulysis De Ramos <marculysisderamos@gmail.com>
2021-09-27 18:05:45 +0800

    Observation Variables Search endpoints
    IBP-5014 / IBP-5053

--- src/main/java/org/ibp/api/brapi/v2/variable/VariableResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v2/variable/VariableResourceBrapi.java
@@ -3,20 +3,37 @@ package org.ibp.api.brapi.v2.variable;
 import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.generationcp.middleware.domain.ontology.VariableType;
 import org.generationcp.middleware.domain.search_request.brapi.v2.VariableSearchRequestDTO;
+import org.generationcp.middleware.exceptions.MiddlewareException;
 import org.generationcp.middleware.manager.api.SearchRequestService;
 import org.generationcp.middleware.service.api.BrapiView;
-import org.ibp.api.brapi.v1.common.SingleEntityResponse;
+import org.generationcp.middleware.service.api.study.VariableDTO;
+import org.ibp.api.brapi.v1.common.*;
+import org.ibp.api.domain.common.PagedResult;
 import org.ibp.api.domain.search.SearchDto;
+import org.ibp.api.java.ontology.VariableService;
+import org.ibp.api.rest.common.PaginatedSearch;
+import org.ibp.api.rest.common.SearchSpec;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Api(value = "BrAPI V2 Variable Services")
 @Controller(value = "VariableResourceBrapiV2")
 public class VariableResourceBrapi {
+
+    @Autowired
+    private VariableService variableService;
 
     @Autowired
     private SearchRequestService searchRequestService;
@@ -25,7 +42,7 @@ public class VariableResourceBrapi {
     @RequestMapping(value = "/{crop}/brapi/v2/search/variables", method = RequestMethod.POST)
     @ResponseBody
     @JsonView(BrapiView.BrapiV2.class)
-    public ResponseEntity<SingleEntityResponse<SearchDto>> postSearchVariables(
+    public ResponseEntity<SingleEntityResponse<SearchDto>> postSearchObservationVariables(
             @PathVariable final String crop,
             @RequestBody final VariableSearchRequestDTO variableSearchRequestDTO) {
         final SearchDto searchDto =
@@ -35,4 +52,96 @@ public class VariableResourceBrapi {
 
         return new ResponseEntity<>(singleVariableSearchRequestDTO, HttpStatus.OK);
     }
+
+    @ApiOperation(value = "Get the results of a Observation variables search request", notes = "Get the results of a Observation 'variables' search request")
+    @RequestMapping(value = "/{crop}/brapi/v2/search/variables/{searchResultsDbId}", method = RequestMethod.GET)
+    @ResponseBody
+    @JsonView(BrapiView.BrapiV2.class)
+    public ResponseEntity<EntityListResponse<VariableDTO>> getObservationVariableSearchResults(
+            @PathVariable final String crop,
+            @PathVariable final String searchResultsDbId,
+            @RequestParam(value = "page",
+                    required = false) final Integer currentPage,
+            @ApiParam(value = BrapiPagedResult.PAGE_SIZE_DESCRIPTION, required = false)
+            @RequestParam(value = "pageSize",
+                    required = false) final Integer pageSize) {
+
+        final VariableSearchRequestDTO requestDTO;
+        try {
+            requestDTO =
+                    (VariableSearchRequestDTO) this.searchRequestService
+                            .getSearchRequest(Integer.valueOf(searchResultsDbId), VariableSearchRequestDTO.class);
+        } catch (final NumberFormatException | MiddlewareException e) {
+            return new ResponseEntity<>(
+                    new EntityListResponse<VariableDTO>(new Result<>(new ArrayList<>())).withMessage("no search request found"),
+                    HttpStatus.NOT_FOUND);
+        }
+
+        return getSearchResults(crop, requestDTO, currentPage, pageSize);
+    }
+
+    @ApiOperation(value = "Call to retrieve a list of observationVariables available in the system.", notes = "Get the Observation Variables")
+    @RequestMapping(value = "/{crop}/brapi/v2/variables", method = RequestMethod.GET)
+    @JsonView(BrapiView.BrapiV2.class)
+    @ResponseBody
+    public ResponseEntity<EntityListResponse<VariableDTO>> getObservationVariables(@PathVariable final String crop,
+        @ApiParam(value = "Variable's unique ID")
+        @RequestParam(value = "observationVariableDbId", required = false) final String observationVariableDbId,
+        @ApiParam(value = "Variable's trait class (phenological, physiological, morphological, etc.)")
+        @RequestParam(value = "traitClass", required = false) final String traitClass,
+        @ApiParam(value = "The unique ID of a studies to filter on")
+        @RequestParam(value = "studyDbId", required = false) final String studyDbId,
+        @ApiParam(value = "An external reference ID. Could be a simple string or a URI. (use with externalReferenceSource parameter)")
+        @RequestParam(value = "externalReferenceID", required = false) final String externalReferenceID,
+        @ApiParam(value = "An identifier for the source system or database of an external reference (use with externalReferenceID parameter)")
+        @RequestParam(value = "externalReferenceSource", required = false) final String externalReferenceSource,
+        @ApiParam(value = BrapiPagedResult.CURRENT_PAGE_DESCRIPTION, required = false)
+        @RequestParam(value = "page", required = false) final Integer currentPage,
+        @ApiParam(value = BrapiPagedResult.PAGE_SIZE_DESCRIPTION, required = false)
+        @RequestParam(value = "pageSize", required = false) final Integer pageSize) {
+        final VariableSearchRequestDTO requestDTO = new VariableSearchRequestDTO();
+        requestDTO.setObservationVariableDbIds(Collections.singletonList(observationVariableDbId));
+        requestDTO.setTraitClasses(Collections.singletonList(traitClass));
+        requestDTO.setStudyDbId(Collections.singletonList(studyDbId));
+        requestDTO.setExternalReferenceIDs(Collections.singletonList(externalReferenceID));
+        requestDTO.setExternalReferenceSources(Collections.singletonList(externalReferenceSource));
+
+        return getSearchResults(crop, requestDTO, currentPage, pageSize);
+    }
+
+    private ResponseEntity<EntityListResponse<VariableDTO>> getSearchResults(String crop, VariableSearchRequestDTO requestDTO, Integer currentPage, Integer pageSize) {
+        final int finalPageNumber = currentPage == null ? BrapiPagedResult.DEFAULT_PAGE_NUMBER : currentPage;
+        final int finalPageSize = pageSize == null ? BrapiPagedResult.DEFAULT_PAGE_SIZE : pageSize;
+
+        final PageRequest pageRequest = new PageRequest(finalPageNumber, finalPageSize);
+
+        final PagedResult<VariableDTO> resultPage =
+                new PaginatedSearch().executeBrapiSearch(finalPageNumber, finalPageSize, new SearchSpec<VariableDTO>() {
+
+                    @Override
+                    public long getCount() {
+                        return VariableResourceBrapi.this.variableService.countAllVariables(Collections.unmodifiableList(
+                                Arrays.asList(VariableType.TRAIT.getId())));
+                    }
+
+                    @Override
+                    public List<VariableDTO> getResults(final PagedResult<VariableDTO> pagedResult) {
+                        final int pageNumber = pagedResult.getPageNumber() + 1;
+                        return VariableResourceBrapi.this.variableService
+                                .getAllVariables(crop, Collections.unmodifiableList(
+                                        Arrays.asList(VariableType.TRAIT.getId())), pagedResult.getPageSize(), pageNumber);
+                    }
+                });
+
+        final Result<VariableDTO> result = new Result<VariableDTO>().withData(resultPage.getPageResults());
+        final Pagination pagination = new Pagination().withPageNumber(resultPage.getPageNumber()).withPageSize(resultPage.getPageSize())
+                .withTotalCount(resultPage.getTotalResults()).withTotalPages(resultPage.getTotalPages());
+
+        final Metadata metadata = new Metadata().withPagination(pagination);
+
+        final EntityListResponse<VariableDTO> entityListResponse = new EntityListResponse<>(metadata, result);
+
+        return new ResponseEntity<>(entityListResponse, HttpStatus.OK);
+    }
+
 }
