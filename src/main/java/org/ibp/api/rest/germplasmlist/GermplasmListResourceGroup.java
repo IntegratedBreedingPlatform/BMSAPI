b2b5386a4204722f8676f58fe234a4015ab39cab
Leonel Kovacic <leonel@leafnode.io>
2021-09-28 11:29:34 -0300

    IBP-4901: created germplasmListDataSearchService

--- src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
+++ src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
@@ -229,7 +229,7 @@ GermplasmListResourceGroup {
 			value = "Sorting criteria in the format: property,asc|desc. ")
 	})
 	@ResponseBody
-	public ResponseEntity<List<GermplasmListSearchResponse>> getGermplasmLists(
+	public ResponseEntity<List<GermplasmListSearchResponse>> searchGermplasmLists(
 		@PathVariable final String cropName,
 		@ApiParam("The program UUID") @RequestParam(required = false) final String programUUID,
 		@RequestBody final GermplasmListSearchRequest request,
@@ -251,7 +251,7 @@ GermplasmListResourceGroup {
 			value = "Sorting criteria in the format: property,asc|desc. ")
 	})
 	@ResponseBody
-	public ResponseEntity<List<GermplasmListDataSearchResponse>> getGermplasmListData(
+	public ResponseEntity<List<GermplasmListDataSearchResponse>> searchGermplasmListData(
 		@PathVariable final String cropName,
 		@PathVariable final Integer listId,
 		@ApiParam("The program UUID") @RequestParam(required = false) final String programUUID,
