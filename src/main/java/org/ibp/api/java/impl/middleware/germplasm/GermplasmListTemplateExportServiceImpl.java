8b3c2bbe2a2fa96a0cc9ee15a5424be2523f58a2
cuenyad <cuenya.diego@gmail.com>
2021-10-21 15:14:26 -0300

    Adding ontology variable sheet and rebuild the style using ExcelCellStyleBuilder.
    Issue: IBP-5088
    Review: none

--- src/main/java/org/ibp/api/java/impl/middleware/germplasm/GermplasmListTemplateExportServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/germplasm/GermplasmListTemplateExportServiceImpl.java
@@ -2,22 +2,18 @@ package org.ibp.api.java.impl.middleware.germplasm;
 
 import com.google.common.io.Files;
 import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFFont;
-import org.apache.poi.hssf.usermodel.HSSFPalette;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.ss.usermodel.BorderStyle;
-import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.FillPatternType;
-import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.IndexedColors;
-import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.generationcp.middleware.domain.ontology.Variable;
+import org.generationcp.middleware.domain.ontology.VariableType;
+import org.generationcp.middleware.manager.ontology.api.OntologyVariableDataManager;
+import org.generationcp.middleware.manager.ontology.daoElements.VariableFilter;
 import org.ibp.api.exception.ResourceNotFoundException;
 import org.ibp.api.java.germplasm.GermplasmListTemplateExportService;
+import org.ibp.api.java.impl.middleware.germplasm.workbook.common.ExcelCellStyleBuilder;
+import org.ibp.api.java.impl.middleware.germplasm.workbook.generator.OntologyVariableSheetGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -28,10 +24,10 @@ import org.springframework.validation.MapBindingResult;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -42,28 +38,28 @@ public class GermplasmListTemplateExportServiceImpl implements GermplasmListTemp
 
 	private static final int COLUMN_WIDTH_PADDING = 6;
 	private static final int CHARACTER_WIDTH = 250;
-	private static final Map<String, ExcelCellStyle> IMPORT_HEADERS;
+	private static final Map<String, ExcelCellStyleBuilder.ExcelCellStyle> IMPORT_LIST_HEADERS;
 
 	static {
-		IMPORT_HEADERS = new LinkedHashMap<>();
-		IMPORT_HEADERS.put("export.germplasm.list.template.gid.column", ExcelCellStyle.HEADING_STYLE_YELLOW);
-		IMPORT_HEADERS.put("export.germplasm.list.template.guid.column", ExcelCellStyle.HEADING_STYLE_YELLOW);
-		IMPORT_HEADERS.put("export.germplasm.list.template.designation.column", ExcelCellStyle.HEADING_STYLE_YELLOW);
-		IMPORT_HEADERS.put("export.germplasm.list.template.entry.code.column", ExcelCellStyle.HEADING_STYLE_YELLOW);
+		IMPORT_LIST_HEADERS = new LinkedHashMap<>();
+		IMPORT_LIST_HEADERS.put("export.germplasm.list.template.gid.column", ExcelCellStyleBuilder.ExcelCellStyle.HEADING_STYLE_YELLOW);
+		IMPORT_LIST_HEADERS.put("export.germplasm.list.template.guid.column", ExcelCellStyleBuilder.ExcelCellStyle.HEADING_STYLE_YELLOW);
+		IMPORT_LIST_HEADERS.put("export.germplasm.list.template.designation.column", ExcelCellStyleBuilder.ExcelCellStyle.HEADING_STYLE_YELLOW);
+		IMPORT_LIST_HEADERS.put("export.germplasm.list.template.entry.code.column", ExcelCellStyleBuilder.ExcelCellStyle.HEADING_STYLE_YELLOW);
 	}
 
-	private Map<ExcelCellStyle, CellStyle> sheetStylesMap;
+	private ExcelCellStyleBuilder sheetStyles;
 
 	private HSSFWorkbook wb;
 
-
-	public enum ExcelCellStyle {
-		HEADING_STYLE_YELLOW,
-	}
-
-
 	@Autowired
 	protected ResourceBundleMessageSource messageSource;
+
+	@Autowired
+	protected OntologyVariableDataManager ontologyVariableDataManager;
+
+	@Autowired
+	protected OntologyVariableSheetGenerator ontologyVariableSheetGenerator;
 
 	@Override
 	public File export(final String cropName, final String programUUID) {
@@ -84,8 +80,16 @@ public class GermplasmListTemplateExportServiceImpl implements GermplasmListTemp
 		this.wb = new HSSFWorkbook();
 
 		final File file = new File(fileNamePath);
-		this.sheetStylesMap = this.createStyles();
+		this.sheetStyles = new ExcelCellStyleBuilder(this.wb);
 		this.writeObservationSheet();
+
+		final VariableFilter entryDetailFilter = new VariableFilter();
+
+		entryDetailFilter.setProgramUuid(programUUID);
+		entryDetailFilter.addVariableType(VariableType.ENTRY_DETAIL);
+		final List<Variable> entryDetailVariables = this.ontologyVariableDataManager.getWithFilter(entryDetailFilter);
+		this.ontologyVariableSheetGenerator.writeOntologyVariableSheet(this.wb, "export.germplasm.list.template.sheet.entry.detail",
+			entryDetailVariables);
 
 		try (final FileOutputStream fos = new FileOutputStream(file)) {
 			this.wb.write(fos);
@@ -100,70 +104,25 @@ public class GermplasmListTemplateExportServiceImpl implements GermplasmListTemp
 			this.wb.createSheet(this.getMessageSource().getMessage("export.germplasm.list.template.sheet.observation", null, locale));
 		observationSheet.setDefaultRowHeightInPoints(16);
 		this.writeObservationHeader(observationSheet);
-	}
 
-	private Map<ExcelCellStyle, CellStyle> createStyles() {
-		final Map<ExcelCellStyle, CellStyle> styles = new EnumMap<>(ExcelCellStyle.class);
-		this.setCustomColorAtIndex(IndexedColors.YELLOW, 255, 255, 204);
-
-		final Font headerFontObservation = this.buildFont("arial", 10, true);
-
-		final CellStyle headingStyleYellow = this.createStyleWithBorder();
-		headingStyleYellow.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
-		headingStyleYellow.setAlignment(HorizontalAlignment.CENTER);
-		headingStyleYellow.setFont(headerFontObservation);
-		styles.put(ExcelCellStyle.HEADING_STYLE_YELLOW, headingStyleYellow);
-
-		return styles;
-	}
-
-	private CellStyle createStyleWithBorder() {
-		final CellStyle cellStyle = this.createStyle();
-		cellStyle.setBorderTop(BorderStyle.THIN);
-		cellStyle.setBorderBottom(BorderStyle.THIN);
-		cellStyle.setBorderLeft(BorderStyle.THIN);
-		cellStyle.setBorderRight(BorderStyle.THIN);
-		return cellStyle;
-	}
-
-	private CellStyle createStyle() {
-		final CellStyle cellStyle = this.wb.createCellStyle();
-		cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-		return cellStyle;
 	}
 
 	private void writeObservationHeader(final HSSFSheet observationSheet) {
 		final Locale locale = LocaleContextHolder.getLocale();
 		final HSSFRow row = observationSheet.createRow(0);
 
-		final Map<String, ExcelCellStyle> headers = IMPORT_HEADERS;
-		final Iterator<Map.Entry<String, ExcelCellStyle>> iterator = headers.entrySet().iterator();
+		final Map<String, ExcelCellStyleBuilder.ExcelCellStyle> headers = IMPORT_LIST_HEADERS;
+		final Iterator<Map.Entry<String, ExcelCellStyleBuilder.ExcelCellStyle>> iterator = headers.entrySet().iterator();
 		int index = 0;
 		while (iterator.hasNext()) {
-			final Map.Entry<String, ExcelCellStyle> entry = iterator.next();
+			final Map.Entry<String, ExcelCellStyleBuilder.ExcelCellStyle> entry = iterator.next();
 			final HSSFCell cell = row.createCell(index, CellType.STRING);
-			cell.setCellStyle(this.sheetStylesMap.get(entry.getValue()));
+			cell.setCellStyle(this.sheetStyles.getCellStyle(entry.getValue()));
 			final String headerColumn = this.getMessageSource().getMessage(entry.getKey(), null, locale);
 			cell.setCellValue(headerColumn);
 			observationSheet.setColumnWidth(index, (headerColumn.length() + COLUMN_WIDTH_PADDING) * CHARACTER_WIDTH);
 			index++;
 		}
-	}
-
-	private void setCustomColorAtIndex(final IndexedColors indexedColor, final int red, final int green,
-		final int blue) {
-		final HSSFPalette customPalette = this.wb.getCustomPalette();
-		customPalette.setColorAtIndex(indexedColor.index, (byte) red, (byte) green, (byte) blue);
-	}
-
-	private HSSFFont buildFont(final String fontName, final int fontHeight, final boolean bold) {
-		final HSSFFont hSSFFont = this.wb.createFont();
-		hSSFFont.setColor(HSSFColor.HSSFColorPredefined.BLACK.getIndex());
-		hSSFFont.setFontName(fontName);
-		hSSFFont.setFontHeightInPoints((short) fontHeight);
-		hSSFFont.setBold(bold);
-		return hSSFFont;
 	}
 
 	public ResourceBundleMessageSource getMessageSource() {
