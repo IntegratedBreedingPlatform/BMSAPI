285d7abc8e184eccab928d649896ab884abd545f
Nahuel Soldevilla <nahuel@leafnode.io>
2021-12-06 13:24:15 -0300

    IBP-5170: Remove PreAuthorize for GET tree and GET variables services
    Relates to: IBP-5011, IBP-5145
    
    Some improvements can be made by making some apis accessible to
    logged-in users regardless of permissions. These are GET endpoints
    needed from different modules:
    
    validate/map variables for import list:
    - filter variables
    - get variable by id
    
    Save list:
    - get germplasm list tree
    - save tree-state
    
    For consistency, we remove PreAuthorize also from get /studies/tree

--- src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
+++ src/main/java/org/ibp/api/rest/germplasmlist/GermplasmListResourceGroup.java
@@ -78,7 +78,6 @@ public class GermplasmListResourceGroup {
 	private ReorderEntriesLock reorderEntriesLock;
 
 	@ApiOperation(value = "Get germplasm lists given a tree parent node folder", notes = "Get germplasm lists given a tree parent node folder")
-	@PreAuthorize("hasAnyAuthority('ADMIN', 'GERMPLASM', 'MANAGE_GERMPLASM', 'SEARCH_GERMPLASM')" + PermissionsEnum.HAS_INVENTORY_VIEW)
 	@RequestMapping(value = "/crops/{crop}/germplasm-lists/tree", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<List<TreeNode>> getGermplasmListByParentFolderId(
@@ -261,7 +260,6 @@ public class GermplasmListResourceGroup {
 	}
 
 	@ApiOperation(value = "Get tree of expanded germplasm list folders last used by user", notes = "Get tree of expanded germplasm list folders last used by user")
-	@PreAuthorize("hasAnyAuthority('ADMIN', 'GERMPLASM', 'MANAGE_GERMPLASM', 'SEARCH_GERMPLASM')" + PermissionsEnum.HAS_INVENTORY_VIEW)
 	@RequestMapping(value = "/crops/{crop}/germplasm-lists/tree-state", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<List<TreeNode>> getUserTreeState(
@@ -272,7 +270,6 @@ public class GermplasmListResourceGroup {
 	}
 
 	@ApiOperation(value = "Save hierarchy of germplasm list folders last used by user", notes = "Save hierarchy of germplasm list folders last used by user")
-	@PreAuthorize("hasAnyAuthority('ADMIN', 'GERMPLASM', 'MANAGE_GERMPLASM', 'SEARCH_GERMPLASM')" + PermissionsEnum.HAS_INVENTORY_VIEW)
 	@RequestMapping(value = "/crops/{crop}/germplasm-lists/tree-state", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<Void> saveUserTreeState(
