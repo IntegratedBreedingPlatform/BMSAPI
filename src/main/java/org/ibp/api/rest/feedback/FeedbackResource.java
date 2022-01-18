0b5bf1164e59c7e0aedc14b16a4eaaef4ba7b3bc
leonel@leafnode.io <leonelkovacic@gmail.com>
2022-01-18 12:32:57 -0300

    IBP-5378: swagger - ignoring several endpoints

--- src/main/java/org/ibp/api/rest/feedback/FeedbackResource.java
+++ src/main/java/org/ibp/api/rest/feedback/FeedbackResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/feedback")
@@ -17,12 +18,14 @@ public class FeedbackResource {
 	@Autowired
 	private FeedbackService feedbackService;
 
+	@ApiIgnore
 	@RequestMapping(value = "/{feature}/should-show", method = RequestMethod.GET)
 	public ResponseEntity<Boolean> shouldShowFeedback(@PathVariable final FeedbackFeature feature) {
 		final boolean shouldShowFeedback = this.feedbackService.shouldShowFeedback(feature);
 		return new ResponseEntity<>(shouldShowFeedback, HttpStatus.OK);
 	}
 
+	@ApiIgnore
 	@RequestMapping(value = "/{feature}/dont-show-again", method = RequestMethod.PUT)
 	public ResponseEntity<Void> dontShowAgain(@PathVariable final FeedbackFeature feature) {
 		this.feedbackService.dontShowAgain(feature);
