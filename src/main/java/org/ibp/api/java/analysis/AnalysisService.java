b3c7bc46cf754ff23771d4225f82f4e5d3cd8632
Aldrin Batac <aldrin.batac@leafnode.io>
2022-03-01 09:17:06 +0800

    IBP-5481 Initial commit. Add enpoint to create means dataset.

---
+++ src/main/java/org/ibp/api/java/analysis/AnalysisService.java
@@ -0,0 +1,9 @@
+package org.ibp.api.java.analysis;
+
+import org.generationcp.middleware.api.analysis.MeansRequestDto;
+
+public interface AnalysisService {
+
+	void createMeansDataset(MeansRequestDto meansInput);
+
+}
---
+++ src/main/java/org/ibp/api/rest/analysis/AnalysisResource.java
@@ -0,0 +1,34 @@
+package org.ibp.api.rest.analysis;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.generationcp.middleware.api.analysis.MeansRequestDto;
+import org.ibp.api.java.analysis.AnalysisService;
+import org.ibp.api.rest.dataset.DatasetDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Api(value = "Analysis Service")
+@Controller
+public class AnalysisResource {
+
+	@Autowired
+	private AnalysisService analysisService;
+
+	@ApiOperation(value = "Create means dataset", notes = "Create means dataset")
+	@RequestMapping(value = "/crops/{cropName}/single-site-analysis/means", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<DatasetDTO> createMeansDataset(@PathVariable final String cropName,
+		@RequestBody final MeansRequestDto meansRequestDto) {
+		this.analysisService.createMeansDataset(meansRequestDto);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+}
