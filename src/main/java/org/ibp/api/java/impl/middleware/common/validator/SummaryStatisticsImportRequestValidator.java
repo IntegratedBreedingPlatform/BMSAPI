92a0ca38c8860fa8c4d067a35c1fec7ed34fd93b
Aldrin Batac <aldrin.batac@leafnode.io>
2022-03-17 08:26:58 +0800

    IBP-5533 Add endpoint to create summary statistics dataset.

---
+++ src/main/java/org/ibp/api/java/impl/middleware/common/validator/SummaryStatisticsImportRequestValidator.java
@@ -0,0 +1,95 @@
+package org.ibp.api.java.impl.middleware.common.validator;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.map.CaseInsensitiveMap;
+import org.apache.commons.lang.StringUtils;
+import org.generationcp.middleware.domain.ontology.Variable;
+import org.generationcp.middleware.domain.ontology.VariableType;
+import org.generationcp.middleware.manager.ontology.api.OntologyVariableDataManager;
+import org.generationcp.middleware.manager.ontology.daoElements.VariableFilter;
+import org.generationcp.middleware.service.impl.analysis.MeansImportRequest;
+import org.generationcp.middleware.service.impl.analysis.SummaryStatisticsImportRequest;
+import org.ibp.api.exception.ApiRequestValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.MapBindingResult;
+import org.thymeleaf.util.MapUtils;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class SummaryStatisticsImportRequestValidator {
+
+	@Autowired
+	private OntologyVariableDataManager ontologyVariableDataManager;
+
+	public void validateEnvironmentNumberIsNotEmpty(final SummaryStatisticsImportRequest summaryStatisticsImportRequest) {
+		final BindingResult errors = new MapBindingResult(new HashMap<>(), MeansImportRequest.class.getName());
+		final boolean hasBlankEnvironmentNumbers =
+			summaryStatisticsImportRequest.getData().stream().anyMatch(md -> md.getEnvironmentNumber() == null);
+		if (hasBlankEnvironmentNumbers) {
+			errors.reject("summary.statistics.import.environment.numbers.required", "");
+			throw new ApiRequestValidationException(errors.getAllErrors());
+		}
+	}
+
+	public void validateSummaryDataIsNotEmpty(final SummaryStatisticsImportRequest summaryStatisticsImportRequest) {
+		final BindingResult errors = new MapBindingResult(new HashMap<>(), MeansImportRequest.class.getName());
+		if (CollectionUtils.isEmpty(summaryStatisticsImportRequest.getData())) {
+			errors.reject("summary.statistics.import.data.required", "");
+			throw new ApiRequestValidationException(errors.getAllErrors());
+		}
+	}
+
+	public void validateDataValuesIsNotEmpty(final SummaryStatisticsImportRequest summaryStatisticsImportRequest) {
+		final BindingResult errors = new MapBindingResult(new HashMap<>(), MeansImportRequest.class.getName());
+		if (summaryStatisticsImportRequest.getData().stream().anyMatch(d -> MapUtils.isEmpty(d.getValues()))) {
+			errors.reject("summary.statistics.import.data.values.required", "");
+			throw new ApiRequestValidationException(errors.getAllErrors());
+		}
+	}
+
+	public void validateAnalysisVariableNames(final SummaryStatisticsImportRequest summaryStatisticsImportRequest) {
+		final BindingResult errors = new MapBindingResult(new HashMap<>(), MeansImportRequest.class.getName());
+		if (CollectionUtils.isNotEmpty(summaryStatisticsImportRequest.getData())) {
+			final Set<String> analysisSummaryVariableNames =
+				summaryStatisticsImportRequest.getData().stream()
+					.map(o -> !MapUtils.isEmpty(o.getValues()) ? o.getValues().keySet() : new HashSet<String>()).flatMap(Set::stream)
+					.collect(Collectors.toSet());
+
+			final VariableFilter variableFilter = new VariableFilter();
+			analysisSummaryVariableNames.forEach(variableFilter::addName);
+			final Map<String, Variable> variablesMapByName =
+				new CaseInsensitiveMap(this.ontologyVariableDataManager.getWithFilter(variableFilter).stream()
+					.collect(Collectors.toMap(Variable::getName, Function.identity())));
+
+			final Set<String> nonExistingVariableNames =
+				analysisSummaryVariableNames.stream().filter(o -> !variablesMapByName.containsKey(o)).collect(Collectors.toSet());
+
+			if (CollectionUtils.isNotEmpty(nonExistingVariableNames)) {
+				errors.reject("summary.statistics.analysis.summary.variable.names.do.not.exist",
+					new Object[] {StringUtils.join(nonExistingVariableNames, ", ")}, "");
+			}
+
+			final List<Variable> invalidVariables =
+				variablesMapByName.values().stream().filter(o -> !o.getVariableTypes().contains(VariableType.ANALYSIS_SUMMARY))
+					.collect(Collectors.toList());
+			if (CollectionUtils.isNotEmpty(invalidVariables)) {
+				errors.reject("summary.statistics.import.variables.must.be.analysis.summary.type",
+					new Object[] {StringUtils.join(invalidVariables.stream().map(Variable::getName).collect(Collectors.toList()), ", ")},
+					"");
+			}
+		}
+		if (errors.hasErrors()) {
+			throw new ApiRequestValidationException(errors.getAllErrors());
+		}
+	}
+
+}
---
+++ src/test/java/org/ibp/api/java/impl/middleware/common/validator/SummaryStatisticsImportRequestValidatorTest.java
@@ -0,0 +1,154 @@
+package org.ibp.api.java.impl.middleware.common.validator;
+
+import org.generationcp.middleware.domain.ontology.Variable;
+import org.generationcp.middleware.domain.ontology.VariableType;
+import org.generationcp.middleware.manager.ontology.api.OntologyVariableDataManager;
+import org.generationcp.middleware.service.impl.analysis.SummaryStatisticsImportRequest;
+import org.ibp.api.exception.ApiRequestValidationException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SummaryStatisticsImportRequestValidatorTest {
+
+	public static final String EDIA_M_CM_Heritability = "EDIA_M_CM_Heritability";
+	public static final String EDIA_M_CM_PValue = "EDIA_M_CM_PValue";
+	public static final String PH_M_CM_Heritability = "PH_M_CM_Heritability";
+	public static final String PH_M_CM_PValue = "PH_M_CM_PValue";
+
+	@Mock
+	private OntologyVariableDataManager ontologyVariableDataManager;
+
+	@InjectMocks
+	private SummaryStatisticsImportRequestValidator summaryStatisticsImportRequestValidator;
+
+	@Test
+	public void testValidateMeansDataIsNotEmpty() {
+		final SummaryStatisticsImportRequest summaryStatisticsImportRequest = this.createSummaryStatisticsImportRequest();
+		summaryStatisticsImportRequest.setData(new ArrayList<>());
+
+		try {
+			this.summaryStatisticsImportRequestValidator.validateSummaryDataIsNotEmpty(summaryStatisticsImportRequest);
+			fail("Should throw an exception");
+		} catch (final ApiRequestValidationException e) {
+			assertEquals("summary.statistics.import.data.required", e.getErrors().get(0).getCode());
+		}
+
+	}
+
+	@Test
+	public void testValidateDataValuesIsNotEmpty() {
+		final SummaryStatisticsImportRequest SummaryStatisticsImportRequest = this.createSummaryStatisticsImportRequest();
+		SummaryStatisticsImportRequest.getData().get(0).setValues(null);
+		try {
+			this.summaryStatisticsImportRequestValidator.validateDataValuesIsNotEmpty(SummaryStatisticsImportRequest);
+			fail("Should throw an exception");
+		} catch (final ApiRequestValidationException e) {
+			assertEquals("summary.statistics.import.data.values.required", e.getErrors().get(0).getCode());
+		}
+	}
+
+	@Test
+	public void testValidateEnvironmentNumberIsNotEmpty_HasBlankEnvironmentId() {
+		final SummaryStatisticsImportRequest SummaryStatisticsImportRequest = this.createSummaryStatisticsImportRequest();
+		SummaryStatisticsImportRequest.getData().get(0).setEnvironmentNumber(null);
+
+		try {
+			this.summaryStatisticsImportRequestValidator.validateEnvironmentNumberIsNotEmpty(SummaryStatisticsImportRequest);
+			fail("Should throw an exception");
+		} catch (final ApiRequestValidationException e) {
+			assertEquals("summary.statistics.import.environment.numbers.required", e.getErrors().get(0).getCode());
+		}
+	}
+
+	@Test
+	public void testValidateAnalysisVariableNames_VariableNamesDoNotExist() {
+		when(this.ontologyVariableDataManager.getWithFilter(any())).thenReturn(new ArrayList<>());
+
+		final SummaryStatisticsImportRequest SummaryStatisticsImportRequest = this.createSummaryStatisticsImportRequest();
+
+		try {
+			this.summaryStatisticsImportRequestValidator.validateAnalysisVariableNames(SummaryStatisticsImportRequest);
+			fail("Should throw an exception");
+		} catch (final ApiRequestValidationException e) {
+			assertEquals("summary.statistics.analysis.summary.variable.names.do.not.exist", e.getErrors().get(0).getCode());
+			assertEquals(
+				new Object[] {EDIA_M_CM_Heritability + ", " + EDIA_M_CM_PValue + ", " + PH_M_CM_Heritability + ", " + PH_M_CM_PValue},
+				e.getErrors().get(0).getArguments());
+		}
+	}
+
+	@Test
+	public void testValidateAnalysisVariableNames_VariablesHaveInvalidVariableType() {
+		when(this.ontologyVariableDataManager.getWithFilter(any())).thenReturn(
+			Arrays.asList(this.createVariable(EDIA_M_CM_PValue, VariableType.TRAIT),
+				this.createVariable(PH_M_CM_PValue, VariableType.TRAIT),
+				this.createVariable(EDIA_M_CM_Heritability, VariableType.TRAIT),
+				this.createVariable(PH_M_CM_Heritability, VariableType.TRAIT)));
+
+		final SummaryStatisticsImportRequest SummaryStatisticsImportRequest = this.createSummaryStatisticsImportRequest();
+
+		try {
+			this.summaryStatisticsImportRequestValidator.validateAnalysisVariableNames(SummaryStatisticsImportRequest);
+			fail("Should throw an exception");
+		} catch (final ApiRequestValidationException e) {
+			assertEquals("summary.statistics.import.variables.must.be.analysis.summary.type", e.getErrors().get(0).getCode());
+			assertEquals(
+				new Object[] {EDIA_M_CM_Heritability + ", " + PH_M_CM_Heritability + ", " + EDIA_M_CM_PValue + ", " + PH_M_CM_PValue},
+				e.getErrors().get(0).getArguments());
+		}
+	}
+
+	private SummaryStatisticsImportRequest createSummaryStatisticsImportRequest() {
+
+		final Random random = new Random();
+		final SummaryStatisticsImportRequest summaryStatisticsImportRequest = new SummaryStatisticsImportRequest();
+
+		final SummaryStatisticsImportRequest.SummaryData summaryData1 = new SummaryStatisticsImportRequest.SummaryData();
+		summaryData1.setEnvironmentNumber(1);
+		final Map<String, Double> values1 = new HashMap<>();
+		values1.put(EDIA_M_CM_Heritability, random.nextDouble());
+		values1.put(EDIA_M_CM_PValue, random.nextDouble());
+		values1.put(PH_M_CM_Heritability, random.nextDouble());
+		values1.put(PH_M_CM_PValue, random.nextDouble());
+		summaryData1.setValues(values1);
+
+		final SummaryStatisticsImportRequest.SummaryData summaryData2 = new SummaryStatisticsImportRequest.SummaryData();
+		summaryData2.setEnvironmentNumber(2);
+		final Map<String, Double> values2 = new HashMap<>();
+		values2.put(EDIA_M_CM_Heritability, random.nextDouble());
+		values2.put(EDIA_M_CM_PValue, random.nextDouble());
+		values2.put(PH_M_CM_Heritability, random.nextDouble());
+		values2.put(PH_M_CM_PValue, random.nextDouble());
+		summaryData2.setValues(values2);
+
+		final List<SummaryStatisticsImportRequest.SummaryData> summaryDataList = new ArrayList<>();
+		summaryDataList.add(summaryData1);
+		summaryDataList.add(summaryData2);
+		summaryStatisticsImportRequest.setData(summaryDataList);
+		return summaryStatisticsImportRequest;
+	}
+
+	private Variable createVariable(final String name, final VariableType variableType) {
+		final Variable variable = new Variable();
+		variable.setName(name);
+		variable.addVariableType(variableType);
+		return variable;
+	}
+
+}
