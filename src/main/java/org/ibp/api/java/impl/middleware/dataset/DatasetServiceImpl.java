2a575d3a54c53f3beabf6684546bc1dfaf0f70f5
leonel@leafnode.io <leonelkovacic@gmail.com>
2022-03-22 16:47:11 -0300

    IBP-5395: prevent remove ENTRY_TYPE and ENTRY_NO

--- src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
+++ src/main/java/org/ibp/api/java/impl/middleware/dataset/DatasetServiceImpl.java
@@ -88,7 +88,7 @@ public class DatasetServiceImpl implements DatasetService {
 
 	private static final String LOCATION_ID_VARIABLE_NAME = "LOCATION";
 	private static final List<Integer> PROTECTED_VARIABLE_IDS =
-		Arrays.asList(TermId.TRIAL_INSTANCE_FACTOR.getId(), TermId.LOCATION_ID.getId());
+		Arrays.asList(TermId.TRIAL_INSTANCE_FACTOR.getId(), TermId.LOCATION_ID.getId(), TermId.ENTRY_NO.getId(), TermId.ENTRY_TYPE.getId());
 	public static final String MISSING_VALUE = "missing";
 	public static final String NOT_AVAILABLE_VALUE = "NA";
 	public static final String PARAM_NULL = "param.null";
