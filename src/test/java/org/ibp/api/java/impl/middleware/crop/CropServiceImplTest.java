b53221e845d165b6f2e3ae549747875d64e02bbc
leonel@leafnode.io <leonelkovacic@gmail.com>
2022-06-29 17:48:37 -0300

    IBP-5734: created CropService and move WorkbenchDataManager related methods to it

--- src/test/java/org/ibp/api/java/impl/middleware/crop/CropServiceImplTest.java
+++ src/test/java/org/ibp/api/java/impl/middleware/crop/CropServiceImplTest.java
@@ -1,30 +1,42 @@
 
 package org.ibp.api.java.impl.middleware.crop;
 
-import java.util.List;
-
-import org.generationcp.middleware.manager.api.WorkbenchDataManager;
+import com.google.common.collect.Lists;
+import org.generationcp.middleware.api.crop.CropService;
 import org.generationcp.middleware.pojos.workbench.CropType;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-import com.google.common.collect.Lists;
+import java.util.List;
 
 public class CropServiceImplTest {
+
+	@InjectMocks
+	private CropServiceImpl cropService;
+
+	@Mock
+	private CropService cropServiceMW;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
 
 	@Test
 	public void testGetInstalledCrops() {
 
-		CropServiceImpl cropService = new CropServiceImpl();
-		WorkbenchDataManager workbenchDataManager = Mockito.mock(WorkbenchDataManager.class);
 		List<CropType> cropTypeList = Lists.newArrayList(new CropType("wheat"), new CropType("maize"));
-		Mockito.when(workbenchDataManager.getInstalledCropDatabses()).thenReturn(cropTypeList);
-		cropService.setWorkbenchDataManager(workbenchDataManager);
+		Mockito.when(this.cropServiceMW.getInstalledCropDatabases()).thenReturn(cropTypeList);
 
-		List<String> installedCrops = cropService.getInstalledCrops();
+		List<String> installedCrops = this.cropService.getInstalledCrops();
 		Assert.assertEquals(cropTypeList.size(), installedCrops.size());
 		Assert.assertTrue(installedCrops.contains("maize"));
 		Assert.assertTrue(installedCrops.contains("wheat"));
 	}
+
 }
