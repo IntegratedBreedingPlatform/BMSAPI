97f527ea2f1dffba09f43d00896630f531f31315
Aldrin Batac <aldrin.batac@leafnode.io>
2022-08-04 08:17:30 +0800

    IBP-5773 Remove unnecessary permission.

--- src/main/java/org/ibp/api/rest/study/StudyInstanceResource.java
+++ src/main/java/org/ibp/api/rest/study/StudyInstanceResource.java
@@ -141,7 +141,7 @@ public class StudyInstanceResource {
 
 	@ApiOperation(value = "Delete georeference associated to an instance",
 		notes = "Delete georeference of associated to an instance")
-	@PreAuthorize("hasAnyAuthority('ADMIN','STUDIES','MANAGE_STUDIES', 'BROWSE_STUDIES')")
+	@PreAuthorize("hasAnyAuthority('ADMIN','STUDIES','MANAGE_STUDIES')")
 	@RequestMapping(value = "/{cropname}/programs/{programUUID}/studies/{studyId}/instances/{instanceId}/georeferences", method = RequestMethod.DELETE)
 	@ResponseBody
 	public ResponseEntity<Void> deleteInstanceGeoreferences(final @PathVariable String cropname, @PathVariable final String programUUID,
