9871009bf4960d6d4c3210a208b040c39cb38d17
mderamos <marc.ramos@leafnode.io>
2022-09-15 16:35:49 +0800

    Clean up
    IBP-5907

--- src/main/java/org/ibp/api/brapi/v2/ImageResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v2/ImageResourceBrapi.java
@@ -8,20 +8,18 @@ import org.generationcp.middleware.api.brapi.v1.image.ImageNewRequest;
 import org.generationcp.middleware.service.api.BrapiView;
 import org.ibp.api.brapi.v1.common.SingleEntityResponse;
 import org.ibp.api.java.file.FileMetadataService;
-import org.ibp.api.java.file.FileStorageService;
 import org.ibp.api.java.impl.middleware.file.validator.FileValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @Api("BrAPI v2 Image services")
-@RestController()
-@RequestMapping("/{cropName}/brapi/v2")
+@Controller
 public class ImageResourceBrapi {
 
 	@Autowired
@@ -31,7 +29,7 @@ public class ImageResourceBrapi {
 	private FileValidator fileValidator;
 
 	@ApiOperation("Create a new image meta data object")
-	@RequestMapping(value = "/images", method = RequestMethod.POST)
+	@RequestMapping(value = "/{crop}/brapi/v2/images", method = RequestMethod.POST)
 	@JsonView(BrapiView.BrapiV2.class)
 	public ResponseEntity<SingleEntityResponse<Image>> createImage(
 		@PathVariable final String cropName,
