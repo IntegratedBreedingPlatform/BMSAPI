9ff7a904f506a5188e1d9af2a6547e35061d55f2
leonel@leafnode.io <leonelkovacic@gmail.com>
2022-10-04 20:07:01 -0300

    IBP-5941: add new expression dataprocessor to context

---
+++ src/main/java/org/ibp/api/configuration/AdvanceConfiguration.java
@@ -0,0 +1,11 @@
+package org.ibp.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+// TODO: declare beans programatically
+@ImportResource("classpath:advance-context.xml")
+@Configuration
+public class AdvanceConfiguration {
+
+}
