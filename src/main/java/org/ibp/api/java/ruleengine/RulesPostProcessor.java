34202f11e6ea3b61af2d792042a27cdf06bbb0ea
leonel@leafnode.io <leonelkovacic@gmail.com>
2022-11-15 11:01:20 -0300

    IBP-6083: mark, rename and move ProcessCodeFacotry and ProcessCodeService

--- src/main/java/org/ibp/api/java/ruleengine/RulesPostProcessor.java
+++ src/main/java/org/ibp/api/java/ruleengine/RulesPostProcessor.java
@@ -6,7 +6,7 @@ import org.generationcp.middleware.ruleengine.RuleFactory;
 import org.generationcp.middleware.ruleengine.coding.expression.BaseCodingExpression;
 import org.generationcp.middleware.ruleengine.coding.expression.CodingExpressionFactory;
 import org.generationcp.middleware.ruleengine.naming.deprecated.expression.DeprecatedExpression;
-import org.generationcp.middleware.ruleengine.naming.impl.ProcessCodeFactory;
+import org.generationcp.middleware.ruleengine.naming.deprecated.impl.DeprecatedProcessCodeFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -29,7 +29,7 @@ public class RulesPostProcessor implements BeanPostProcessor {
 	private RuleFactory ruleFactory;
 
 	@Autowired
-	private ProcessCodeFactory processCodeFactory;
+	private DeprecatedProcessCodeFactory processCodeFactory;
 
 	@Autowired
 	private CodingExpressionFactory codingExpressionFactory;
