20fe8ff22195e2267db03462920969ff85b96584
cuenyad <cuenya.diego@gmail.com>
2022-11-16 17:51:40 -0300

    Implement Names and CombinedKey(fieldType + "_" + id)
    IBP-6056
    Review none

--- src/main/java/org/ibp/api/rest/labelprinting/StudyEntriesLabelPrinting.java
+++ src/main/java/org/ibp/api/rest/labelprinting/StudyEntriesLabelPrinting.java
@@ -3,12 +3,10 @@ package org.ibp.api.rest.labelprinting;
 import org.apache.commons.lang3.StringUtils;
 import org.generationcp.commons.util.FileNameGenerator;
 import org.generationcp.commons.util.FileUtils;
-import org.generationcp.middleware.api.brapi.v1.attribute.AttributeDTO;
 import org.generationcp.middleware.api.germplasm.GermplasmAttributeService;
 import org.generationcp.middleware.api.germplasm.GermplasmNameService;
 import org.generationcp.middleware.domain.etl.MeasurementVariable;
 import org.generationcp.middleware.domain.etl.StudyDetails;
-import org.generationcp.middleware.domain.germplasm.GermplasmNameDto;
 import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.domain.ontology.VariableType;
 import org.generationcp.middleware.domain.study.StudyEntrySearchDto;
@@ -18,6 +16,7 @@ import org.generationcp.middleware.service.api.study.StudyEntryPropertyData;
 import org.ibp.api.java.study.StudyEntryService;
 import org.ibp.api.rest.common.FileType;
 import org.ibp.api.rest.labelprinting.domain.Field;
+import org.ibp.api.rest.labelprinting.domain.FieldType;
 import org.ibp.api.rest.labelprinting.domain.LabelType;
 import org.ibp.api.rest.labelprinting.domain.LabelsData;
 import org.ibp.api.rest.labelprinting.domain.LabelsGeneratorInput;
@@ -47,7 +46,7 @@ import java.util.stream.Collectors;
 @Transactional
 public class StudyEntriesLabelPrinting extends LabelPrintingStrategy {
 
-	private static final int ATTRIBUTE_DISPLAY_MAX_LENGTH = 200;
+	private static final String UNDERSCORE = "_";
 
 	private static final int NAME_DISPLAY_MAX_LENGTH = 200;
 
@@ -87,7 +86,7 @@ public class StudyEntriesLabelPrinting extends LabelPrintingStrategy {
 	@Override
 	OriginResourceMetadata getOriginResourceMetadata(final LabelsInfoInput labelsInfoInput, final String programUUID) {
 		final StudyDetails study = this.studyDataManager.getStudyDetails(labelsInfoInput.getStudyId());
-		final String tempFileName = study.getStudyName().concat("_").concat(StudyEntriesLabelPrinting.ORIG_FINAL_NAME);
+		final String tempFileName = study.getStudyName().concat(StudyEntriesLabelPrinting.UNDERSCORE).concat(StudyEntriesLabelPrinting.ORIG_FINAL_NAME);
 		final String fileName = FileNameGenerator.generateFileName(tempFileName);
 		return new OriginResourceMetadata(FileUtils.cleanFileName(fileName), new HashMap<>());
 	}
@@ -100,33 +99,43 @@ public class StudyEntriesLabelPrinting extends LabelPrintingStrategy {
 		final String germplasmDetailsPropValue = this.getMessage("label.printing.germplasm.details");
 		final String entryDetailsPropValue = this.getMessage("label.printing.entry.details");
 		final String attributesPropValue = this.getMessage("label.printing.attributes.details");
+		final String namesPropValue = this.getMessage("label.printing.names.details");
 
 		final LabelType germplasmLabelType = new LabelType(germplasmDetailsPropValue, germplasmDetailsPropValue);
 		final LabelType entryDetailsLabelType = new LabelType(entryDetailsPropValue, entryDetailsPropValue);
 		final LabelType attributesType = new LabelType(attributesPropValue, attributesPropValue);
+		final LabelType namesType = new LabelType(namesPropValue, namesPropValue);
 
 		final List<Field> germplasmFields = new LinkedList<>();
 		final List<Field> entryDetailsFields = new LinkedList<>();
 		final List<Field> attributeFields = new LinkedList<>();
+		final List<Field> nameFields = new LinkedList<>();
 
 		germplasmLabelType.setFields(germplasmFields);
 		entryDetailsLabelType.setFields(entryDetailsFields);
 		attributesType.setFields(attributeFields);
+		namesType.setFields(nameFields);
 
 		labelTypes.add(germplasmLabelType);
 		labelTypes.add(entryDetailsLabelType);
 		labelTypes.add(attributesType);
+		labelTypes.add(namesType);
 
 		final List<MeasurementVariable> variables = this.studyEntryService.getEntryTableHeader(labelsInfoInput.getStudyId());
 
-		variables.forEach((Variable) -> {
-			final Field field = new Field(Variable);
-			if (VariableType.GERMPLASM_DESCRIPTOR.equals(Variable.getVariableType())) {
+		variables.forEach((variable) -> {
+			final Field field = new Field(variable);
+			field.setFieldType(FieldType.VARIABLE);
+			if (VariableType.GERMPLASM_DESCRIPTOR.equals(variable.getVariableType())) {
 				germplasmFields.add(field);
-			} else if (VariableType.ENTRY_DETAIL.equals(Variable.getVariableType())) {
+			} else if (VariableType.ENTRY_DETAIL.equals(variable.getVariableType())) {
 				entryDetailsFields.add(field);
-			} else if (VariableType.GERMPLASM_PASSPORT.equals(Variable.getVariableType()) || VariableType.GERMPLASM_ATTRIBUTE.equals(Variable.getVariableType())) {
+			} else if (VariableType.GERMPLASM_PASSPORT.equals(variable.getVariableType()) ||
+				VariableType.GERMPLASM_ATTRIBUTE.equals(variable.getVariableType())) {
 				attributeFields.add(field);
+			} else if (variable.getTermId() > 0 && null == variable.getVariableType()) {
+				field.setFieldType(FieldType.NAME);
+				nameFields.add(field);
 			}
 		});
 
@@ -140,39 +149,30 @@ public class StudyEntriesLabelPrinting extends LabelPrintingStrategy {
 		List<StudyEntryDto> studyEntryDtos = this.studyEntryService.getStudyEntries(labelsGeneratorInput.getStudyId(), filter, pageable);
 
 		// Data to be exported
-		final List<Map<Integer, String>> results = new LinkedList<>();
+		final List<Map<String, String>> results = new LinkedList<>();
 
-		//Get Germplasm names, attributes, entry details data
-		final Map<Integer, Map<Integer, String>> attributeValues = new HashMap<>();
-		final Map<Integer, Map<Integer, String>> nameValues = new HashMap<>();
-
-		final Set<Integer> keys = this.getSelectedFieldIds(labelsGeneratorInput);
-
-		final List<Integer> gids = studyEntryDtos.stream().map(StudyEntryDto::getGid).collect(Collectors.toList());
-		this.getAttributeValuesMap(attributeValues, gids);
-		this.getNameValuesMap(nameValues, gids);
+		final Set<String> combinedKeys = this.getSelectedFieldIds(labelsGeneratorInput);
 
 		final boolean isPdf = FileType.PDF.equals(labelsGeneratorInput.getFileType());
 
 		studyEntryDtos.forEach((studyEntry) -> {
-			final Map<Integer, String> row = new HashMap<>();
-			keys.forEach((key) -> {
+			final Map<String, String> row = new HashMap<>();
+			combinedKeys.forEach((combinedKey) -> {
+				final String[] field = combinedKey.split(StudyEntriesLabelPrinting.UNDERSCORE);
+				final Integer key = Integer.valueOf(field[1]);
 				if (TermId.CROSS.getId() == key) {
-					row.put(key,
+					row.put(combinedKey,
 						truncateValueIfPdf(isPdf, studyEntry.getCross(), StudyEntriesLabelPrinting.NAME_DISPLAY_MAX_LENGTH));
 				}else if (studyEntry.getProperties().containsKey(key)) {
 					final StudyEntryPropertyData data = studyEntry.getProperties().get(key);
-					row.put(key,
+					row.put(combinedKey,
 						truncateValueIfPdf(isPdf, data.getValue(), StudyEntriesLabelPrinting.NAME_DISPLAY_MAX_LENGTH));
-				} else {
-					this.getAttributeOrNameDataRowValue(isPdf, studyEntry, attributeValues, nameValues, key, row);
 				}
 
 			});
 			results.add(row);
 		});
-
-		return new LabelsData(TermId.GID.getId(), results);
+		return new LabelsData(FieldType.VARIABLE.getName() + UNDERSCORE + TermId.GID.getId(), results);
 	}
 
 	private String truncateValueIfPdf(final boolean isPdf, final String value, final int maxLength) {
@@ -180,47 +180,17 @@ public class StudyEntriesLabelPrinting extends LabelPrintingStrategy {
 			value.substring(0, maxLength) + "..." : value;
 	}
 
-	private void getAttributeOrNameDataRowValue(final boolean isPdf, final StudyEntryDto studyEntry, final Map<Integer, Map<Integer, String>> attributeValues,
-		final Map<Integer, Map<Integer, String>> nameValues, final int id, final Map<Integer, String> row) {
-
-		final Map<Integer, String> attributeMap = attributeValues.get(studyEntry.getGid());
-		final Map<Integer, String> nameMap = nameValues.get(studyEntry.getGid());
-
-		if (attributeMap != null && attributeMap.containsKey(id)) {
-			row.put(id, truncateValueIfPdf(isPdf, attributeMap.get(id), StudyEntriesLabelPrinting.ATTRIBUTE_DISPLAY_MAX_LENGTH));
-		} else if (nameMap != null && nameMap.containsKey(id)) {
-			row.put(id, truncateValueIfPdf(isPdf, nameMap.get(id), StudyEntriesLabelPrinting.NAME_DISPLAY_MAX_LENGTH));
-		}
-	}
-
-	Set<Integer> getSelectedFieldIds(final LabelsGeneratorInput labelsGeneratorInput) {
-		final Set<Integer> keys = labelsGeneratorInput.getFields().stream().flatMap(Collection::stream).collect(Collectors.toSet());
+	Set<String> getSelectedFieldIds(final LabelsGeneratorInput labelsGeneratorInput) {
+		final Set<String> keys = labelsGeneratorInput.getFields().stream().flatMap(Collection::stream).collect(Collectors.toSet());
 
 		if (labelsGeneratorInput.isBarcodeRequired()) {
 			if (labelsGeneratorInput.isAutomaticBarcode()) {
-				keys.add(TermId.GID.getId());
+				keys.add(FieldType.VARIABLE.getName() + UNDERSCORE + TermId.GID.getId());
 			} else {
 				keys.addAll(labelsGeneratorInput.getBarcodeFields());
 			}
 		}
 		return keys;
-	}
-
-	void getNameValuesMap(final Map<Integer, Map<Integer, String>> nameValues, final List<Integer> gids) {
-		final List<GermplasmNameDto> germplasmNames = this.germplasmNameService.getGermplasmNamesByGids(gids);
-		germplasmNames.forEach(name -> {
-			nameValues.putIfAbsent(name.getGid(), new HashMap<>());
-			nameValues.get(name.getGid()).put(name.getNameTypeId(), name.getName());
-		});
-	}
-
-	void getAttributeValuesMap(final Map<Integer, Map<Integer, String>> attributeValues, final List<Integer> gids) {
-		final Map<Integer, List<AttributeDTO>> attributesByGIDsMap = this.germplasmAttributeService.getAttributesByGIDsMap(gids);
-		for (final Map.Entry<Integer, List<AttributeDTO>> gidAttributes : attributesByGIDsMap.entrySet()) {
-			final Map<Integer, String> attributesMap = new HashMap<>();
-			gidAttributes.getValue().forEach(attributeDTO -> attributesMap.put(attributeDTO.getAttributeDbId(), attributeDTO.getValue()));
-			attributeValues.put(gidAttributes.getKey(), attributesMap);
-		}
 	}
 
 	@Override
