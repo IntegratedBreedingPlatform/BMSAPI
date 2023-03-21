17a7955b43ddd42f4006f9494f8eb144cdfcc62d
cuenyad <cuenya.diego@gmail.com>
2023-03-21 09:17:31 -0300

    Removing PreAuthorize and implementing ApiIgnore annotation.
    Issue: IBP-5924/IBP-6238
    Review: none

--- src/main/java/org/ibp/controller/VariableCacheController.java
+++ src/main/java/org/ibp/controller/VariableCacheController.java
@@ -15,19 +15,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Api(value = "Variable Cache Controller")
 @RestController
-@PreAuthorize("hasAnyAuthority('ADMIN', 'STUDIES', 'MANAGE_STUDIES', 'MS_STUDY_SETTINGS', 'MS_ADD_STUDY_SETTINGS_VARIABLES', "
-	+ " 'MS_GERMPLASM_AND_CHECKS', 'MS_ADD_ENTRY_DETAILS_VARIABLES', "
-	+ " 'MS_ENVIRONMENT','MS_ADD_ENVIRONMENTAL_CONDITIONS_VARIABLES', 'MS_ADD_ENVIRONMENT_DETAILS_VARIABLES', "
-	+ " 'MS_OBSERVATIONS','MS_ADD_OBSERVATION_TRAIT_VARIABLES', 'MS_ADD_OBSERVATION_SELECTION_VARIABLES', "
-	+ " 'CROP_MANAGEMENT', 'MANAGE_ONTOLOGIES')")
 public class VariableCacheController {
 
 	@Autowired
 	private VariableService variableService;
 
+	@ApiIgnore
 	@ResponseBody
 	@ApiOperation(value = "Delete Variables from VariableCache", notes = "Remove Variables from VariableCache by Ids")
 	@RequestMapping(value = "crops/{cropName}/variable-cache/{variablesIds}", method = RequestMethod.DELETE)
