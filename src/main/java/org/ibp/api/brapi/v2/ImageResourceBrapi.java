0fa727b825561feb0610c94b94b53a0a135b9d8e
Aldrin Batac <aldrin.batac@leafnode.io>
2023-08-11 10:42:45 +0800

    IBP-6400 POST v2 images body should be in List<ImageNewRequest>. Implement PUT v2 /images/{imageDbId}/imagecontent

--- src/main/java/org/ibp/api/brapi/v2/ImageResourceBrapi.java
+++ src/main/java/org/ibp/api/brapi/v2/ImageResourceBrapi.java
@@ -6,6 +6,8 @@ import io.swagger.annotations.ApiOperation;
 import org.generationcp.middleware.api.brapi.v1.image.Image;
 import org.generationcp.middleware.api.brapi.v1.image.ImageNewRequest;
 import org.generationcp.middleware.service.api.BrapiView;
+import org.ibp.api.brapi.v1.common.EntityListResponse;
+import org.ibp.api.brapi.v1.common.Result;
 import org.ibp.api.brapi.v1.common.SingleEntityResponse;
 import org.ibp.api.java.file.FileMetadataService;
 import org.ibp.api.java.impl.middleware.common.validator.ImageValidator;
@@ -21,7 +23,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Api("BrAPI v2 Image services")
 @Controller(value = "ImageResourceBrapiV2")
@@ -43,19 +47,43 @@ public class ImageResourceBrapi {
 	@PreAuthorize("hasAnyAuthority('ADMIN', 'STUDIES', 'MANAGE_STUDIES')")
 	@RequestMapping(value = "/{crop}/brapi/v2/images", method = RequestMethod.POST)
 	@JsonView(BrapiView.BrapiV2.class)
-	public ResponseEntity<SingleEntityResponse<Image>> createImage(
+	public ResponseEntity<EntityListResponse<Image>> createImage(
 		@PathVariable final String crop,
-		@RequestBody final ImageNewRequest body
+		@RequestBody final List<ImageNewRequest> body
 	) {
-		this.permissionValidator.validateProgramByObservationUnitDbId(crop, Arrays.asList(body.getObservationUnitDbId()), true);
 
 		this.fileValidator.validateFileStorage();
-		this.imageValidator.validateImage(body);
 
-		final Image result = this.fileMetadataService.createImage(body);
+		final List<Image> imageList = new ArrayList<>();
+		for (final ImageNewRequest imageNewRequest : body) {
+			this.permissionValidator.validateProgramByObservationUnitDbId(crop, Arrays.asList(imageNewRequest.getObservationUnitDbId()), true);
+			this.imageValidator.validateImage(imageNewRequest);
+			imageList.add(this.fileMetadataService.createImage(imageNewRequest));
+		}
+		final Result<Image> results = new Result<Image>().withData(imageList);
+		final EntityListResponse<Image> response = new EntityListResponse<>(results);
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
+	}
+
+	@ApiOperation("Update an image with the image file content")
+	@PreAuthorize("hasAnyAuthority('ADMIN', 'STUDIES', 'MANAGE_STUDIES')")
+	@RequestMapping(value = "/images/{imageDbId}/imagecontent",
+			produces = {"application/json"},
+			consumes = {"image/*"},
+			method = RequestMethod.PUT)
+	@JsonView(BrapiView.BrapiV2.class)
+	public ResponseEntity<SingleEntityResponse<Image>> updateImageContent(
+			@PathVariable final String cropName,
+			@PathVariable("imageDbId") final String imageDbId,
+			@RequestBody final byte[] imageContent
+	) {
+		this.fileValidator.validateFileStorage();
+
+		final Image result = this.fileMetadataService.updateImageContent(imageDbId, imageContent);
 		final SingleEntityResponse<Image> response = new SingleEntityResponse<>();
 		response.setResult(result);
 		return new ResponseEntity<>(response, HttpStatus.CREATED);
 	}
+
 
 }
